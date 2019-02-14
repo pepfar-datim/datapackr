26957255a9a2434314cbb57be5aa67d62eaa7f3b
Scott <scott.j.jackson@gmail.com>
2019-02-14 18:03:40 -0500

    Add dataset documentation

--- data-raw/produceConfigFile.R
+++ data-raw/produceConfigFile.R
@@ -191,8 +191,8 @@ mapIndicators <- function() {
         save(template_schema, file = "./data/template_schema.rda")
 
     ## Valid Data Pack Disaggs
-        validDPDisaggs <- validDPDisaggs()
-        save(validDPDisaggs, file = "./data/validDPDisaggs.rda")
+        valid_dp_disaggs <- validDPDisaggs()
+        saveRDS(valid_dp_disaggs, file = "./data/valid_dp_disaggs.rds")
 
     ## Prioritization Dictionary
         prioritizations <- prioritizationDict()
