8390319f57464b1ec5268d6f84ba5b3fac696427
Scott <scott.j.jackson@gmail.com>
2019-02-20 16:46:13 -0500

    Tag siteType and test site levels

--- R/getSiteList.R
+++ R/getSiteList.R
@@ -35,7 +35,7 @@ getSiteList <- function(country_uids,
         siteList <- 
           paste0(getOption("baseurl"), "api/",api_version(),
                  "/organisationUnits.json?paging=false&filter=ancestors.id:eq:",
-                   country_uid,
+                   country_id,
                  "&filter=organisationUnitGroups.id:in:[POHZmzofoVx,PvuaP6YALSA]",
                   "&fields=id,name,level,ancestors[id,name],organisationUnitGroups[id,name]") %>%
           httr::GET() %>%
@@ -44,13 +44,35 @@ getSiteList <- function(country_uids,
           do.call(rbind.data.frame, .)
         
       # Tag Site Type
-          sj <- siteList %>%
+        siteList %<>%
           dplyr::mutate(
-            site_type = dplyr::case_when(stringr::str_detect(as.character(organisationUnitGroups), "PvuaP6YALSA") ~ "Community",
-                                         stringr::str_detect(as.character(organisationUnitGroups), "POHZmzofoVx") ~ "Facility"))
+            site_type =
+              dplyr::case_when(
+                stringr::str_detect(as.character(organisationUnitGroups), "PvuaP6YALSA") ~ "Community",
+                stringr::str_detect(as.character(organisationUnitGroups), "POHZmzofoVx") ~ "Facility"))
         
       # Test Site Levels
-        levels <- NULL
+        levels <- datapackr::dataPackMap %>%
+          dplyr::filter(country_uid == country_id) %>%
+          dplyr::select(country,prioritization,planning,community,facility,mil_level)
+        
+        if (levels$community != (siteList %>%
+                                  dplyr::filter(site_type == "Community") %>%
+                                  dplyr::pull(level) %>%
+                                  unique())) {
+          warning("Oh my! How embarassing. There's a mismatch in Community level
+                  designations in DATIM. Better check it out.")
+        }
+        
+        if (levels$facility != (siteList %>%
+                                 dplyr::filter(site_type == "Facility") %>%
+                                 dplyr::pull(level) %>%
+                                 unique())) {
+          warning("Oh my! How embarassing. There's a mismatch in Facility level
+                  designations in DATIM. Better check it out.")
+        }
+         
+        
         
       
         
