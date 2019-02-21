46882992c0fcce49381ee1563ab01835b598b373
Scott <scott.j.jackson@gmail.com>
2019-02-20 20:51:14 -0500

    Finalize getSiteList function

--- R/getSiteList.R
+++ R/getSiteList.R
@@ -22,18 +22,21 @@ getSiteList <- function(country_uids,
   
   # Loop through country uids
     for (i in 1:country_uids) {
-      country_id <- country_uids[i]
+      cntry_uid <- country_uids[i]
+      cntry_name <- datapackr::dataPackMap %>%
+        dplyr::filter(country_uid == cntry_uid) %>%
+        dplyr::pull(country_name)
       
   # Is this country in a regional program?
       isRegion <- datapackr::dataPackMap %>%
-        dplyr::filter(country_uid == country_id) %>%
+        dplyr::filter(country_uid == cntry_uid) %>%
         dplyr::pull(is_region)
       
   # Pull Sites
       siteList <- 
-        paste0(getOption("baseurl"), "api/",api_version(),
+        paste0(getOption("baseurl"), "api/",datapackr::api_version(),
                "/organisationUnits.json?paging=false&filter=ancestors.id:eq:",
-                 country_id,
+               cntry_uid,
                "&filter=organisationUnitGroups.id:in:[POHZmzofoVx,PvuaP6YALSA]",
                 "&fields=id,name,level,ancestors[id,name],organisationUnitGroups[id,name]") %>%
         httr::GET() %>%
@@ -48,44 +51,78 @@ getSiteList <- function(country_uids,
             dplyr::case_when(
               stringr::str_detect(as.character(organisationUnitGroups), "PvuaP6YALSA") ~ "Community",
               stringr::str_detect(as.character(organisationUnitGroups), "POHZmzofoVx") ~ "Facility"))
+      
+  # Add _Military sites
+      if (include_mil) {
+        milNode <- datapackr::dataPackMap %>%
+          dplyr::filter(country_uid == cntry_uid) %>%
+          dplyr::mutate(
+            ancestors = list(list(name = NA_character_, id = NA_character_)),
+            organisationUnitGroups = list(list(name = NA_character_, id = NA_character_)),
+            site_type = "Military") %>%
+          dplyr::select(level = mil_level,
+                        name = mil_psnu,
+                        id = mil_psnu_uid,
+                        ancestors,
+                        organisationUnitGroups,
+                        site_type)
+        
+        siteList %<>%
+          dplyr::bind_rows(milNode)
+      }
         
   # Test Site Levels
       levels <- datapackr::dataPackMap %>%
-        dplyr::filter(country_uid == country_id) %>%
+        dplyr::filter(country_uid == cntry_uid) %>%
         dplyr::select(country,prioritization,planning,community,facility,mil_level)
       
       if (levels$community != (siteList %>%
                                 dplyr::filter(site_type == "Community") %>%
                                 dplyr::pull(level) %>%
                                 unique())) {
-        warning("Oh my! How embarassing. There's a mismatch in Community level
-                designations in DATIM. Better check it out.")
+        stop(paste0("Oh my! How embarassing. There's a mismatch in Community level designations in DATIM for ",
+                    cntry_name,
+                    ". Better check it out."))
       }
       
       if (levels$facility != (siteList %>%
                                dplyr::filter(site_type == "Facility") %>%
                                dplyr::pull(level) %>%
                                unique())) {
-        warning(
-          paste0("Oh my! How embarassing. There's a mismatch in Facility level designations in DATIM for ",
-                 (datapackr::dataPackMap %>%
-                   dplyr::filter(country_uid == country_id) %>%
-                   dplyr::pull(country_name)),
-                ". Better check it out."))
+        stop(paste0("Oh my! How embarassing. There's a mismatch in Facility level designations in DATIM for ",
+                    cntry_name,
+                    ". Better check it out."))
       }
 
   # Tag PSNUs
-      sj <- siteList %>%
+      siteList %<>%
         dplyr::mutate(
-          psnu = purrr::map_chr(ancestors,
-              function(x) magrittr::use_series(x, name) %>%
-              magrittr::extract(3)
-            )
+          psnu = dplyr::case_when(
+            site_type == "Military" ~ name,
+            level == levels$prioritization ~ name,
+            TRUE ~ purrr::map_chr(ancestors,
+                                  function(x) magrittr::use_series(x, name) %>%
+                                    magrittr::extract(levels$prioritization)
+                                 )
+            ),
+          psnu_uid = dplyr::case_when(
+            site_type == "Military" ~ id,
+            level == levels$prioritization ~ id,
+            TRUE ~ purrr::map_chr(ancestors,
+                                  function(x) magrittr::use_series(x, id) %>%
+                                    magrittr::extract(levels$prioritization)
+                                 )
+            ),
+          site_tool_label = paste0(
+            dplyr::case_when(isRegion ~ paste0(cntry_name," > "), TRUE ~ ""),
+            dplyr::if_else(site_type == "Military","",paste0(psnu, " > ")),
+            name,
+            " (#",site_type,")",
+            " (",id,")"
           )
-        
-        
-        
-    
-  }
+        )
+    }
+  
+  return(siteList)
   
 }
--- data-raw/run.R
+++ data-raw/run.R
@@ -1,16 +1,11 @@
 
-support_files_path <- "/Users/scott/Google Drive/PEPFAR/COP Targets/COP 19/3) Testing & Deployment/Support Files/"
-
 output_path <- "/Users/scott/Google Drive/PEPFAR/COP Targets/COP 19/5) Maintenance & Support/Draft Data Packs/Outputs"
-
-secrets <- "/Users/scott/.secrets/datim.json"
 
 ### NO TOUCHY #####
 library(tidyverse)
 library(magrittr)
 
 
-unPackData(
-    support_files_path,
-    output_path,
-    secrets)
+unPackData()
+
+
