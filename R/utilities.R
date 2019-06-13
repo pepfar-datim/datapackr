5b6b76ad0b631d1c6ce039b850d0619184677eed
Scott Jackson <scott.j.jackson@gmail.com>
2019-06-13 16:55:59 -0400

    Standard filepath testing; resolve pepfar-datim/COP-19-Target-Setting#392

--- R/utilities.R
+++ R/utilities.R
@@ -498,18 +498,34 @@ getCountries <- function(datapack_uid = NA) {
 #' @description 
 #' Given a file path, will test whether the file can be read.
 #' 
-#' @param path Filepath to test.
+#' @param path Filepath to test. Default is NA/
+#' @param type File type, either \code{standard} or \code{template}.
 #' 
 #' @return Logical. \code{TRUE} if file can be read, \code{FALSE} if not.
 #'
-can_read_file <- function(path) {
-  if (is.na(path)) {return(FALSE)}
-  file.access(path, 4) == 0
+canReadFile <- function(path, type = "standard") {
+  
+  #Check that the file path was supplied.
+  if (is.na(path)) {
+    return(FALSE)
+  }
+  
+  #If the file to read is a template file, check for write permissions.
+  else if (type == "template") {
+    file.access(path, 2) == 0
+  }
+  
+  #If the file to read is a standard file, check for at least read permissions.
+  else if (type == "standard") {
+    file.access(path, 4) == 0
+  }
+    
 }
 
 
 
 #' @export
+#' @importFrom tools file_ext
 #' @title Test an input file for read access and type. Prompt if issues.
 #' 
 #' @description 
@@ -519,24 +535,27 @@ can_read_file <- function(path) {
 #' 
 #' @param path Filepath to test and use.
 #' @param extension File extension to test for. (Do not include leading period.)
+#' @param type File type, whether \code{standard} or \code{template}.
 #' 
 #' @return Character vector containing valid filepath for further use.
 #' 
-handshake_file <- function(path = NA, extension) {
+handshakeFile <- function(path = NA, extension, type = "standard") {
   
-  if (!can_read_file(path) & interactive()) {
+  #If path has issues or NA, prompt user to select file from window.
+  if (!canReadFile(path, type = type) & interactive()) {
     interactive_print("Please choose a file.")
     path <- file.choose()
   }
 
-  msg <- "Checking the file exists..."
-  interactive_print(msg)
-
-  if (!can_read_file(path)) {
+  #Check the file can be read one more time.
+  interactive_print("Checking the file exists...")
+  if (!canReadFile(path, type = type)) {
     stop("File could not be read!")
   }
   
-  if (tools::file_ext(path) != tolower(extension)) {
+  #Check the file has correct extension
+  extension = stringr::str_remove(tolower(extension),"\\.")
+  if (file_ext(path) != extension) {
     stop(paste0("File is not the correct format! File must have extension .",
                 extension))
   } else {
