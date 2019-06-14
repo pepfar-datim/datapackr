3fff61bc6b37fe3a1d1da183cc74a5564bad497d
Scott Jackson <scott.j.jackson@gmail.com>
2019-06-13 21:03:24 -0400

    Update FASTForward.R

--- R/FASTForward.R
+++ R/FASTForward.R
@@ -1,27 +1,26 @@
-#' @importFrom magrittr %>% %<>%
+#' @export
 #' @title FASTforward(d)
 #'
 #' @description Takes the output of the \code{\link{unPackSheets}} function and
 #'     recompiles this to contain only data relevant for and in same structure
 #'     as the PEPFAR FAST Tool.
 #'
-#' @param d datapackr list object containing at least
-#'     \code{d$data$distributedMER}.
-#' @return A datapackr list object, \code{d}, storing at least 1 dataframe of
-#'    data structured for ingestion into the PEPFAR FAST Tool,
-#'    \code{d$data$FAST}.
+#' @param d Datapackr object
+#' 
+#' @return d
+#' 
 FASTforward <- function(d) {
   d$data$FAST <- d$data$distributedMER %>%
     dplyr::filter(
       # Detect HTS_TST & HTS_TST_POS cases
       stringr::str_detect(
-        indicatorCode,
+        indicator_code,
         "HTS_TST(.)+Age|(PMTCT|TB)_STAT\\.N(.)+(NewNeg|NewPos)$|VMMC_CIRC\\.(.)+(Negative|Positive)|HTS_INDEX"
       )
       # Detect OVC_SERV, TB_PREV, TX_CURR, TX_NEW, VMMC_CIRC cases
       |
         stringr::str_detect(
-          indicatorCode,
+          indicator_code,
           "OVC_SERV|TB_PREV\\.N|TX_CURR\\.|TX_NEW\\.N\\.Age|VMMC_CIRC\\."
         )
     ) %>%
@@ -31,7 +30,7 @@ FASTforward <- function(d) {
       ## Copy for HTS_TST
       ((.) %>%
          dplyr::filter(
-           stringr::str_detect(indicatorCode, "VMMC_CIRC(.)+(Negative|Positive)")
+           stringr::str_detect(indicator_code, "VMMC_CIRC(.)+(Negative|Positive)")
          ) %>%
          dplyr::mutate(indicator = "HTS_TST")
       ),
@@ -39,7 +38,7 @@ FASTforward <- function(d) {
       ((.) %>%
          dplyr::filter(
            stringr::str_detect(
-             indicatorCode,
+             indicator_code,
              "(VMMC_CIRC|HTS_TST)(.)+Positive|(HTS_INDEX|PMTCT_STAT|TB_STAT)(.)+NewPos$"
            )
          ) %>%
@@ -49,9 +48,9 @@ FASTforward <- function(d) {
     dplyr::mutate(
       indicator = dplyr::case_when(
         !is.na(indicator) ~ indicator,
-        stringr::str_detect(indicatorCode, "PMTCT_STAT|TB_STAT|HTS_INDEX") ~ "HTS_TST",
+        stringr::str_detect(indicator_code, "PMTCT_STAT|TB_STAT|HTS_INDEX") ~ "HTS_TST",
         TRUE ~ stringr::str_extract(
-          indicatorCode,
+          indicator_code,
           "OVC_SERV|HTS_TST|TB_PREV|TX_CURR|TX_NEW|VMMC_CIRC"
         )
       ),
