ff825b5bbf84f9f3237266972bb05e7e873c435b
Scott Jackson <scott.j.jackson@gmail.com>
2019-06-20 09:41:35 -0400

    In checkStructure, pull schema from d$info

--- R/checkStructure.R
+++ R/checkStructure.R
@@ -8,25 +8,16 @@
 #' @return d
 #' 
 checkStructure <- function(d) {
-  # Check structural integrity of Workbook sheets
+  
+  # Check structural integrity of Workbook sheets ####
   submission_sheets <-
     readxl::excel_sheets(d$keychain$submission_path) %>%
     tibble::enframe(name = NULL) %>%
     dplyr::select(sheet_name = value) %>%
     dplyr::mutate(submission_order = as.integer(1:(dplyr::n())))
   
-  # Check all sheets present and accounted for
-  if (d$info$tool == "Data Pack") {
-    schema <- datapackr::data_pack_schema
-  } else if (d$info$tool == "Site Tool") {
-    schema <- datapackr::site_tool_schema
-  }
-  #TODO Add once https://github.com/pepfar-datim/datapackr/issues/43 resolved
-  #else if (d$info$tool == "Mechanism Map") {
-  #   schema <- datapackr::mech_map_schema
-  # }
-  
-  d$tests$sheets_check <- schema %>%
+  # Check all sheets present and accounted for ####
+  d$tests$sheets_check <- d$info$schema %>%
     dplyr::select(sheet_name, template_order = sheet_num) %>%
     dplyr::distinct() %>%
     dplyr::left_join(submission_sheets, by = c("sheet_name")) %>%
@@ -34,7 +25,7 @@ checkStructure <- function(d) {
   
   #TODO: Decide whether to add all sheets into schema to check against
   
-  # Alert to missing Sheets
+  # TEST for missing Sheets ####
   info_msg <- "Checking for any missing tabs..."
   interactive_print(info_msg)
   
@@ -45,7 +36,7 @@ checkStructure <- function(d) {
     
     warning_msg <-
       paste0(
-        "MISSING SHEETS: Did you delete or rename these tabs?: ",
+        "MISSING SHEETS: Did you delete or rename these tabs? -> ",
         paste0(d$tests$missing_sheets, collapse = ", "),
         "")
     d$info$warning_msg <- append(d$info$warning_msg, warning_msg)
