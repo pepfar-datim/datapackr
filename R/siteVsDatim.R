d30ae0fffd499459256fd871dc9c28d5cb1ca6de
sam-bao <sgarman@baosystems.com>
2019-08-19 13:54:16 +0200

    Update siteVsDatim.R

--- R/siteVsDatim.R
+++ R/siteVsDatim.R
@@ -1,3 +1,10 @@
+DHISLogin("/users/sam/.secrets/triage.json")
+base_url <- getOption("baseurl")
+d <- datapackr::unPackSiteToolData("/Users/sam/Documents/cop_19_data/final_site_tool/17_JHHSiteTool_Rwanda_20190407_submission.xlsx")
+
+
+
+
 # Retry API is part of data pack commons but I don't currently export it.
 #' @title RetryAPI(api_url, content_type, max_attempts)
 #' 
@@ -24,47 +31,107 @@ RetryAPI <- function(api_url, content_type, max_attempts = 10){
   stop(paste("Failed to obtain valid response in RetryAPI for:", api_url))
 }
 
-SiteVsDatim <- function(d, org_unit_uid = NULL, period, base_url = getOption("baseurl")){
+
+
+#' @export
+#' @title GetDataValueSet
+#' 
+#' @description Used to read DHIS 2 data using the data value set endpoint
+#' @param keys character vector - data value set parameter keys (e.g. "dataSet", "period")
+#' @param values character vector - values marching the key from keys (e.g. "Abcde123456", "2019Q1"
+#' @return  tibble with the data requested
+#'
+GetDataValueSet <- function(keys, values, 
+                            base_url = getOption("baseurl"), 
+                            api_version = "30"){
   
-  if (is.null(org_unit_uid)) {
-    org_unit_uid <- d$info$datapack_uid
+  #  api_call <- "https://triage.datim.org/api/30/dataValueSets.csv?dataSet=nIHNMxuPUORX&dataSet=sBv1dj90IX6&dataSet=C2G7IyPPrvD&dataSet=HiJieecLXxNX&period=2019Oct&orgUnit=XtxUYCsDWrR&children=true&categoryOptionComboIdScheme=code&includeDeleted=false"
+  
+# concatenate and format the keys and values provided for the api call 
+  parameters <- stringr::str_c(keys, 
+                               values, 
+                               sep = "=", 
+                               collapse = "&")
+  api_call <- glue::glue("{base_url}api/{api_version}/dataValueSets.csv?{parameters}")
+  # note the deleted column comes back empty (missing a comma in fact) which results in a warning
+  # we can disregard that warning
+  #  row col   expected     actual         file
+  #  1  -- 11 columns 10 columns literal data
+  
+  RetryAPI(api_call, "application/csv") %>%   
+    httr::content(., "text") %>% 
+    {suppressWarnings(readr::read_csv(., 
+                                      col_names = TRUE, 
+                                      col_types = readr::cols(.default = "c", 
+                                                              value = "d", 
+                                                              lastupdated = "T")))} %>%
+    dplyr::rename(data_element = dataelement, 
+                  org_unit = orgunit,
+                  category_option_combo = categoryoptioncombo,
+                  attribute_option_combo = attributeoptioncombo,
+                  stored_by = storedby,
+                  last_updated = lastupdated)
   }
+
+
+
+SiteVsDatim <- function(site_data, 
+                        org_unit_uid, 
+                        period, 
+                        base_url = getOption("baseurl")){
   
   if(period == "2019Oct"){
-    print("put data sets here")
+    parameters <- tibble::tribble(~key, ~value,
+                                  "dataSet", "nIHNMxuPUORX",
+                                  "dataSet", "sBv1dj90IX6",
+                                  "dataSet", "C2G7IyPPrvD",
+                                  "dataSet", "HiJieecLXxNX",
+                                  "period", "2019Oct",
+                                  "orgUnit", org_unit_uid,
+                                  "children", "true",
+                                  "categoryOptionComboIdScheme", "code",
+                                  "includeDeleted", "false")
+    }
+  
+# rename columns to fit standards
+# aggregate duplicate rows from site tool data
+  site_data <- site_data %>%
+    dplyr::rename(tool_value = value, data_element_uid = dataElement, 
+                  org_unit_uid = orgUnit,
+                  category_option_combo_uid = categoryOptionCombo,
+                  attribute_option_combo_code = attributeOptionCombo) %>% 
+    dplyr::group_by(data_element_uid, period, org_unit_uid,
+                    category_option_combo_uid, attribute_option_combo_code) %>%
+    dplyr::summarise(tool_value=round(sum(as.numeric(tool_value))))
+    
+# get data from datim
+# rename to standard names
+  datim_data <- GetDataValueSet(parameters$key, parameters$value) %>% 
+    dplyr::rename(datim_value = value, data_element_uid = data_element, 
+                  org_unit_uid = org_unit,
+                  category_option_combo_uid = category_option_combo,
+                  attribute_option_combo_code = attribute_option_combo)
+
+  data <- dplyr::full_join(site_data, datim_data)
+    
+# pull dedups into their own object
+  data_dedups <- dplyr::filter(data, 
+                                 attribute_option_combo_code == "00000" | 
+                                   attribute_option_combo_code == "00001")
+  data <- dplyr::filter(data, 
+                          attribute_option_combo_code != "00000" & 
+                            attribute_option_combo_code != "00001")
+    
+# Find the cases with different values 
+  data_different_value <- dplyr::filter(data, tool_value != datim_value)
+  data_datim_only <- dplyr::filter(data, is.na(tool_value))
+  data_site_tool_only <- dplyr::filter(data, is.na(datim_value))
+    
+  list(data_dedups = data_dedups,
+       data_different_value = data_different_value,
+       data_datim_only = data_datim_only,
+       data_site_tool_only = data_site_tool_only
+       )
   }
-  
-  site_data <- d$datim$site_data %>% 
-    dplyr::group_by(dataElement,period,orgUnit,categoryOptionCombo,attributeOptionCombo) %>% 
-    dplyr::summarise(value=round(sum(as.numeric(value)))) %>% 
-    dplyr::rename( "tool_value" = "value")
 
-  api_call <- "https://triage.datim.org/api/30/dataValueSets.csv?dataSet=nIHNMxuPUORX&dataSet=sBv1dj90IX6&dataSet=C2G7IyPPrvD&dataSet=HiJieecLXxNX&period=2019Oct&orgUnit=XtxUYCsDWrR&children=true&categoryOptionComboIdScheme=code&includeDeleted=false"
-  
-  
-  
-# note the deleted column comes back empty (missing a comma in fact) which results in a warning
-# we can disregard that warning
-#  row col   expected     actual         file
-#  1  -- 11 columns 10 columns literal data
-  
-  datim_data <-  RetryAPI(api_call,"application/csv") 
-    datim_data <- datim_data %>%   httr::content(., "text") %>% 
-    readr::read_csv(col_names = TRUE, col_types = readr::cols(.default = "c", 
-                                                              value = "d", 
-                                                              lastupdated = "T")) %>%
-      dplyr::rename(dataElement = dataelement, 
-                    orgUnit = orgunit,
-                    categoryOptionCombo = categoryoptioncombo,
-                    attributeOptionCombo = attributeoptioncombo,
-                    datim_value = value)
-    
-    data=dplyr::full_join(site_data,datim_data)
-    data_dedups <- dplyr::filter(data, attributeOptionCombo == "00000" | attributeOptionCombo == "00001")
-    data <- dplyr::filter(data, attributeOptionCombo != "00000" & attributeOptionCombo != "00001")
-    data_different_value <- dplyr::filter(data, tool_value != datim_value)
-    data_datim_only <- dplyr::filter(data, is.na(tool_value))
-    data_site_tool_only <- dplyr::filter(data, is.na(datim_value))
-    
-
-}
+temp <- SiteVsDatim(d$datim$site_data, d$info$datapack_uid, "2019Oct")
