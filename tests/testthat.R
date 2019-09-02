80e1135ebdfe82ddc882b6a1a4d1d56b98e6dce4
sam-bao <sgarman@baosystems.com>
2019-09-02 14:47:03 +0200

    GetDataValueSets function initial committ

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(datapackr)
+
+test_check("datapackr")
---
+++ tests/testthat/test-GetDataValueSet.R
@@ -0,0 +1,24 @@
+context("Test GetDataValueSets")
+
+test_that("GetDataValueSets", {
+  datapackcommons::DHISLogin_Play("2.30")
+  data <- getDataValueSets(c("dataElementGroup", "orgUnit", "startDate", "endDate", "children", "limit"), 
+                  c("qfxEYY9xAl6", "O6uvpzGd5pu", "2013-01-01", "2090-01-01","true","25"), 
+                  "https://play.dhis2.org/2.30/")
+  testthat::expect_named(data, c("data_element",
+                         "period",
+                         "org_unit",
+                         "category_option_combo", 
+                         "attribute_option_combo",
+                         "value",                 
+                         "stored_by",
+                         "last_updated",
+                         "comment",
+                         "followup",
+                         "deleted"))
+  testthat::expect_equal(NROW(data), 25)
+  testthat::expect_error(getDataValueSets(c("limit"), 
+                                         c("25"), 
+                                         "https://play.dhis2.org/2.30/", 
+                                         max_attempts = 1))
+  })
