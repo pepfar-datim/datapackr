b7fe0720cb367ed9f4b7e3acc06420840c15e0e5
sam-bao <sgarman@baosystems.com>
2019-10-21 14:30:59 +0200

    Removing files that will be part of a different pull request

--- R/runShinyApps.R
+++
@@ -1,8 +0,0 @@
-#' @export
-runApp_CompareSiteVsDatim <- function(){
-  appDir <- system.file("siteVsDatimApp", package = "datapackr")
-  if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `mypackage`.", call. = FALSE)
-    }
-  shiny::runApp(appDir, display.mode = "normal")
-  }
