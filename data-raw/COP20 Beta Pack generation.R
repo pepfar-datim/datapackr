6330bfc999a61dec515c232f0e5659973934e2df
Scott Jackson <scott.j.jackson@gmail.com>
2019-12-01 21:12:22 -0500

    Prepare for batch Beta Pack processing

--- data-raw/COP20 Beta Pack generation.R
+++ data-raw/COP20 Beta Pack generation.R
@@ -5,28 +5,36 @@ secrets <- "/Users/scott/.secrets/datim.json"
 
 loginToDATIM(secrets)
 
-datapack_name <- "Uganda"
-country_uids <- c("FETQ6OmnsKB")
-                #Uganda         #Tanzania       #Ghana
-
-template_path <- NA
+template_path <- "./data-raw/COP20_Data_Pack_Template_vFINAL.xlsx"
 
 cop_year = 2020
 
-output_folder <- "/Users/scott/Google Drive/PEPFAR/COP Targets/COP 20/2) Development"
+output_folder <- "/Users/scott/Google Drive/PEPFAR/COP Targets/COP 20/3) Testing & Deployment/Beta Packs/Testing"
 
 model_data_path <- "/Users/scott/Google Drive/PEPFAR/COP Targets/COP 20/3) Testing & Deployment/SAMPLE_model_data_pack_input_jason_20_20191127_4.rds"
 
 model_data <- readRDS(model_data_path) %>%
   flattenDataPackModel_19()
 
+batch <- tibble::tribble(
+  ~datapack_name, ~country_uids,
+  "India", "skj3e4YSiJY",
+  "Thailand", "Gv5ApcpDrIB",
+  "Nigeria", "PqlFzhuPcF1",
+  "South Africa", "cDGPF739ZZr",
+  "Zimbabwe", "a71G4Gtcttv",
+  "Tanzania", "mdXu6iCbn2G",
+  "Uganda", "FETQ6OmnsKB",
+  "Mozambique", "h11OyvlPxpJ",
+  "Ethiopia", "IH1kchw86uA"
+)
 
-# devtools::install_github(repo = "https://github.com/pepfar-datim/data-pack-commons.git", ref = "Prod")
-# library(datapackcommons)
+for (i in 1:NROW(batch)) {
+  packDataPack(model_data = model_data,
+               datapack_name = batch[i,1],
+               country_uids = batch[i,2],
+               template_path = template_path,
+               cop_year = cop_year,
+               output_folder = output_folder)
+}
 
-packDataPack(model_data,
-             datapack_name,
-             country_uids,
-             template_path,
-             cop_year,
-             output_folder)
