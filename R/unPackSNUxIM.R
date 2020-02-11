347f3708046ba7e9e677983b3cabb72af57ac25b
Scott Jackson <scott.j.jackson@gmail.com>
2020-02-11 02:56:32 -0800

    Add test for invalid column headers

--- R/unPackSNUxIM.R
+++ R/unPackSNUxIM.R
@@ -33,27 +33,30 @@ unPackSNUxIM <- function(d) {
   # Run structural checks ####
   d <- checkColStructure(d, "PSNUxIM")
   
-  # Keep only columns we need
+  # Keep only columns we need ####
   toKeep <- d$info$schema %>%
     dplyr::filter(sheet_name == sheet_name
                   & !indicator_code %in% c("Rollup", "sheet_num", "DataPackTarget", "ID")
-  # Filter by what's in submission to avoid unknown column warning messages
+  # Filter by what's in submission to avoid unknown column warning messages ####
                   & indicator_code %in% colnames(d$data$SNUxIM)) %>%
     dplyr::pull(indicator_code) %>% 
     unique(.)
   
-  sj <- d$data$SNUxIM %>%
+  d$data$SNUxIM %<>%
     dplyr::select(
       PSNU,
       indicator_code,
       dplyr::one_of(toKeep),
       dplyr::matches("Dedupe|(\\d){4,6}")) %>%
-  # We don't need columns or rows with all NA targets -- Drop them.
+  
+  # We don't need columns or rows with all NA targets -- Drop them. ####
     #dplyr::select_if(~!all(is.na(.))) %>%
     dplyr::filter(
       rowSums(!is.na(dplyr::select(., dplyr::matches("Dedupe|(\\d){4,6}")))) != 0
       ) %>%
-  # # Align PMTCT_EID Age bands with rest of Data Pack (TODO: Fix in Data Pack, not here)
+  
+    
+    # # Align PMTCT_EID Age bands with rest of Data Pack (TODO: Fix in Data Pack, not here)
   #  dplyr::mutate(
   #    Age = dplyr::case_when(
   #      stringr::str_detect(indicator_code, "PMTCT_EID(.)+2to12mo") ~ "02 - 12 months",
@@ -66,21 +69,55 @@ unPackSNUxIM <- function(d) {
   #      stringr::str_detect(indicator_code, "HTS_SELF(.)+Unassisted") ~ "Male|Female",
   #      TRUE ~ Sex)) %>%
   # tidyr::separate_rows(Sex, sep = "\\|") %>%
-  # Create distribution matrix 
+  
+  # Gather for joining ####
     tidyr::gather(
-      key = "mechanism_code",
+      key = "mechCode_supportType",
       value = "value",
       -PSNU, -indicator_code, -Age, -Sex, -KeyPop,
       na.rm = TRUE) %>%
+  
+  # Drop zeros ####
     dplyr::mutate(value = as.numeric(value)) %>%
-    dplyr::filter(value != 0) %>%
-    tidyr::separate(mechanism_code, into=c("mechanism_code","support_type"), sep = "_") %>% 
+    dplyr::filter(value != 0)
+    
+  # TEST Column headers for appropriate structure ####
+    mech_headers_check <- d$data$SNUxIM %>%
+      dplyr::filter(!stringr::str_detect(mechCode_supportType, "Dedupe|(\\d{4,6})_(DSD|TA)"))
+    
+    d[["tests"]][["mech_headers_check"]][[as.character(sheet)]] <- mech_headers_check
+    
+    if (NROW(mech_headers_check) > 0) {
+      invalid_mech_headers <- mech_headers_check %>%
+        dplyr::pull(mechCode_supportType) %>%
+        unique()
+      
+      d[["tests"]][["invalid_mech_headers"]][[as.character(sheet)]] <- invalid_mech_headers
+      
+      warning_msg <-
+        paste0(
+          "WARNING! In tab ",
+          sheet,
+          ", INVALID COLUMN HEADERS: The following column headers are invalid.
+          Please use only the form 12345_DSD. ->  \n\t* ",
+          paste(invalid_mech_headers, collapse = "\n\t* "),
+          "\n")
+      
+      d$info$warning_msg <- append(d$info$warning_msg, warning_msg)
+      
+    }
+  
+  # Get mech codes and support types ####
+  d$data$SNUxIM %<>%
     dplyr::mutate(
-      psnuid = stringr::str_extract(PSNU, "(?<=\\[)([A-Za-z][A-Za-z0-9]{10})(?<!\\])"),
-      mechanism_code = stringr::str_extract(mechanism_code, "(\\d{4,6})|Dedupe"),
-      mechanism_code = stringr::str_replace(mechanism_code, "Dedupe", "99999")) %>%
+      mechanism_code = stringr::str_extract(mechCode_supportType, "(\\d{4,6})|Dedupe"),
+      mechanism_code = stringr::str_replace(mechanism_code, "Dedupe", "99999"),
+      support_type = stringr::str_extract(mechCode_supportType, "(?<=_)DSD|TA"),
+    
+  # Get other metadata needed for joining with other targets data
+      psnuid = stringr::str_extract(PSNU, "(?<=\\[)([A-Za-z][A-Za-z0-9]{10})(?<!\\])")) %>%
     dplyr::select(PSNU, psnuid,  indicator_code, Age, Sex,
-                  KeyPop, mechanism_code, distribution, SNUxIM_value = value)
+                  KeyPop, mechanism_code, support_type, SNUxIM_value = value)
     
   return(d)
 }
