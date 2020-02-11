eb9c2a4b716308a169de30a6f4938e06c1d99cfd
Scott Jackson <scott.j.jackson@gmail.com>
2020-02-11 01:36:07 -0800

    Resolves pepfar-datim/COP-Target-Setting#608

--- R/unPackSNUxIM.R
+++ R/unPackSNUxIM.R
@@ -31,7 +31,7 @@ unPackSNUxIM <- function(d) {
   if (NROW(d$data$SNUxIM) == 0) {return(d)}
   
   # Run structural checks ####
-  #d <- checkColStructure(d, "SNUxIM")
+  d <- checkColStructure(d, "PSNUxIM")
   
   # Keep only columns we need
   toKeep <- d$info$schema %>%
@@ -42,41 +42,47 @@ unPackSNUxIM <- function(d) {
     dplyr::pull(indicator_code) %>% 
     unique(.)
   
-  d$data$SNUxIM %<>%
+  sj <- d$data$SNUxIM %>%
     dplyr::select(
       PSNU,
       indicator_code,
       dplyr::one_of(toKeep),
       dplyr::matches("Dedupe|(\\d){4,6}")) %>%
   # We don't need columns or rows with all NA targets -- Drop them.
-    dplyr::select_if(~!all(is.na(.))) %>%
+    #dplyr::select_if(~!all(is.na(.))) %>%
     dplyr::filter(
       rowSums(!is.na(dplyr::select(., dplyr::matches("Dedupe|(\\d){4,6}")))) != 0
       ) %>%
-  #TODO: Has this been fixed, or is it still relevant?
+  # Recalculate Dedupe
+    dplyr::mutate_at(dplyr::vars(dplyr::matches("Dedupe|(\\d){4,6}")), as.numeric) %>%
+    dplyr::mutate(
+      mechanisms = rowSums(dplyr::select(., dplyr::matches("(\\d){4,6}")), na.rm = TRUE),
+      Dedupe = dplyr::if_else(mechanisms > 1, 1 - mechanisms, 0)
+    ) %>%
+    dplyr::filter(mechanisms != 0) %>%
+    dplyr::select(-mechanisms) %>%
   # # Align PMTCT_EID Age bands with rest of Data Pack (TODO: Fix in Data Pack, not here)
-  #   dplyr::mutate(
-  #     CoarseAge = dplyr::case_when(
-  #       stringr::str_detect(indicator_code, "PMTCT_EID(.)+2to12mo") ~ "02 - 12 months",
-  #       stringr::str_detect(indicator_code, "PMTCT_EID(.)+2mo") ~ "<= 02 months",
-  #       TRUE ~ CoarseAge),
-  #     Sex = dplyr::case_when(
+  #  dplyr::mutate(
+  #    Age = dplyr::case_when(
+  #      stringr::str_detect(indicator_code, "PMTCT_EID(.)+2to12mo") ~ "02 - 12 months",
+  #      stringr::str_detect(indicator_code, "PMTCT_EID(.)+2mo") ~ "<= 02 months",
+  #      TRUE ~ Age),
+  #    Sex = dplyr::case_when(
   # # Drop Unknown Sex from PMTCT_EID (TODO: Fix in Data Pack, not here)
-  #       stringr::str_detect(indicator_code, "PMTCT_EID") ~ NA_character_,
+  #      stringr::str_detect(indicator_code, "PMTCT_EID") ~ NA_character_,
   # # Fix issues with HTS_SELF (duplicate and split by Male/Female) (TODO: Fix in Data Pack, not here)
-  #       stringr::str_detect(indicator_code, "HTS_SELF(.)+Unassisted") ~ "Male|Female",
-  #       TRUE ~ Sex)) %>%
-  #  tidyr::separate_rows(Sex, sep = "\\|") %>%
+  #      stringr::str_detect(indicator_code, "HTS_SELF(.)+Unassisted") ~ "Male|Female",
+  #      TRUE ~ Sex)) %>%
+  # tidyr::separate_rows(Sex, sep = "\\|") %>%
   # Create distribution matrix 
     tidyr::gather(
       key = "mechanism_code",
       value = "value",
       -PSNU, -indicator_code, -Age, -Sex, -KeyPop,
       na.rm = TRUE) %>%
-    dplyr::mutate(value = as.numeric(value)) %>%
-    dplyr::filter(value != 0) %>%
     dplyr::group_by(PSNU, indicator_code, Age, Sex, KeyPop) %>%
-    dplyr::mutate(distribution = value / sum(value)) %>%
+    dplyr::mutate(
+      distribution = value / sum(value)) %>%
     dplyr::ungroup() %>%
     tidyr::separate(mechanism_code, into=c("mechanism_code","support_type"), sep = "_") %>% 
     dplyr::mutate(
