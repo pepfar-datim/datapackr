9242ac795ef739f9008b16a15305b84b9d493992
Scott Jackson <31488407+jacksonsj@users.noreply.github.com>
2020-02-18 18:49:51 -0500

    Updates to Excel Template to close #635, #650, #651 (#112)
    * Allow processing of brand new PSNUxIMs; Fix issues with regional OU processing
    
    * Create PSNUxIM DataPackTarget references dynamically
    
    * Resolves pepfar-datim/COP-Target-Setting#635
    
    * Changes to Excel template
    
    - Resolves pepfar-datim/COP-Target-Setting#635
    - Resolves pepfar-datim/COP-Target-Setting#650
    - Resolves pepfar-datim/COP-Target-Setting#651
    
    * Update schema naming in /data to allow correct package referencing
    
    Co-authored-by: Sam Garman <36417685+sam-bao@users.noreply.github.com>

--- data-raw/update_cop20_datapack_template.R
+++ data-raw/update_cop20_datapack_template.R
@@ -1,7 +1,7 @@
 ## If you've made any edits to the Excel template, rebuild package first to
 ## capture these, then run the below.
 
-secrets <- "/Users/scott/.secrets/test-mer2.json"
+secrets <- "/Users/scott/.secrets/datim.json"
 
 loginToDATIM(secrets)
 
@@ -14,6 +14,14 @@ cop20_data_pack_schema <-
     filepath = datapack_template_filepath,
     skip = skip_tabs(tool = "Data Pack Template", cop_year = 2020),
     cop_year = 2020)
+
+comparison <- compareDF::compare_df(df_new = cop20_data_pack_schema,
+                                    df_old = datapackr::cop20_data_pack_schema,
+                                    group_col = c("sheet_num", "sheet_name", "col"))
+
+comparison_df <- comparison$comparison_df
+
+View(comparison_df)
 
 save(cop20_data_pack_schema,
      file = "./data/cop20_data_pack_schema.rda",
