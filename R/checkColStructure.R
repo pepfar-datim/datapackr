c3146aee48ab567e0d4f688e4c32d8ab19f01e46
Scott Jackson <scott.j.jackson@gmail.com>
2020-02-24 13:44:48 +0200

    Fix issues with DataPackTarget col references RE: d$tests$col_check

--- R/checkColStructure.R
+++ R/checkColStructure.R
@@ -31,6 +31,9 @@ checkColStructure <- function(d, sheet) {
     dplyr::left_join(submission_cols, by = c("indicator_code" = "indicator_code")) %>%
     dplyr::mutate(order_check = template_order == submission_order)
   
+  d[["info"]][["col_check"]][[as.character(sheet)]] <- character()
+  d[["info"]][["col_check"]][[as.character(sheet)]] <- col_check
+  
   # Alert to missing cols ####
   if (any(is.na(col_check$submission_order))) {
     
--- R/packSNUxIM.R
+++ R/packSNUxIM.R
@@ -160,7 +160,7 @@ packSNUxIM <- function(d) {
           return(col_letter)
         }
         
-        col_letters <- lapply(d$tests$col_check, get_ID_col)
+        col_letters <- lapply(d$info$col_check, get_ID_col)
         
         compile_formula_ref <- function(sheet_name) {
           if (!sheet_name %in% c("Epi Cascade I", "Epi PMTCT", "Prioritization", "PSNUxIM")) {
@@ -169,12 +169,12 @@ packSNUxIM <- function(d) {
           }
         }
         
-        OVC_HIVSTAT_col_letter <- d$tests$col_check$OVC %>%
+        OVC_HIVSTAT_col_letter <- d$info$col_check$OVC %>%
           dplyr::filter(indicator_code == "PSNU") %>%
           dplyr::pull(submission_order) %>%
           openxlsx::int2col()
         
-        formula_refs_compiled <- lapply(names(d$tests$col_check), compile_formula_ref) %>%
+        formula_refs_compiled <- lapply(names(d$info$col_check), compile_formula_ref) %>%
           unlist() %>%
           paste(collapse = "") %>%
           paste0(., "OVC!$",OVC_HIVSTAT_col_letter,":$",OVC_HIVSTAT_col_letter)
