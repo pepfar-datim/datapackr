f195a7e91460fbc0322241e4d4e35e117d8a03ff
Scott Jackson <scott.j.jackson@gmail.com>
2020-03-20 12:05:16 -0700

    adornMechanisms - Code style cleanup

--- R/adornMechanisms.R
+++ R/adornMechanisms.R
@@ -12,10 +12,8 @@
 #' 
 #' @return Modified d object with d$data$analtyics
 #' 
-#'
-  getMechanismView<-function() {
-  
-  empty_mechs_view<-tibble::tibble(
+getMechanismView<-function() {
+  empty_mechs_view <- tibble::tibble(
     "mechanism_desc" = character() ,
     "mechanism_code"= character(),
     "attributeOptionCombo" = character(),
@@ -24,11 +22,11 @@
     "agency" = character(),
     "ou" = character(),
     "startdate" = character(),
-    "enddate" = character())
+    "enddate" = character()
+    )
   
   getMechanismViewFromDATIM <- function() {
-    
-    if ( !isLoggedIn() ) {
+    if (!isLoggedIn()) {
       warning("You are not logged in but have requested a mechanism view.")
       return(empty_mechs_view)
     } else {
@@ -44,27 +42,32 @@
           partner_desc = partner,
           partner_id = primeid
         )
-    } }
+    }
+  }
   
   support_files_directory<-getOption("support_files_directory")
   
-  if ( is.null(support_files_directory) ) {
-    mechs<-getMechanismViewFromDATIM() 
+  if (is.null(support_files_directory)) {
+    mechs <- getMechanismViewFromDATIM() 
   } else {
       
     cached_mechs_path <- paste0(support_files_directory,"mechs.rds")
     
-    if ( file.access(cached_mechs_path,4)  == 0) {
-      mechs<-readRDS(cached_mechs_path)
-    } else {
-      mechs<-getMechanismViewFromDATIM() }
+    if (file.access(cached_mechs_path, 4) == 0) {
+      mechs <- readRDS(cached_mechs_path)
+    } else {mechs <- getMechanismViewFromDATIM()}
+    
   }
   
-  structure_ok<-dplyr::setequal(names(empty_mechs_view),names(mechs))
+  structure_ok <- dplyr::setequal(names(empty_mechs_view), names(mechs))
+  
   if (!structure_ok) {warning("Mechanism structure is not correct.")}
   
   mechs
+  
 }
+
+  
 #' @export
 #' @importFrom magrittr %>% %<>%
 #' @title adornMechanisms(d)
@@ -76,21 +79,21 @@
 #' 
 #' @return Modified d object with d$data$analtyics
 #' 
-#' 
 adornMechanisms <- function(d) {
   
-  mechs<-getMechanismView() %>% 
-    dplyr::select(-ou,-startdate,-enddate)
+  mechs <- getMechanismView() %>% 
+    dplyr::select(-ou, -startdate, -enddate)
   
-  d$data$analytics <- dplyr::left_join( d$data$analytics , mechs, by = "mechanism_code") %>% 
-    dplyr::mutate(mechanism_desc = dplyr::case_when(mechanism_code == "99999" ~ 'Dedupe approximation',
-                                                    TRUE ~ mechanism_desc),
-                  partner_desc = dplyr::case_when(mechanism_code == "99999" ~ 'Dedupe approximation',
-                                                  TRUE ~ partner_desc),
-                  partner_id = dplyr::case_when(mechanism_code == "99999" ~ '99999',
-                                                TRUE ~ partner_id),
-                  agency = dplyr::case_when(mechanism_code == "99999" ~ 'Dedupe approximation',
-                                            TRUE ~ agency))
+  d$data$analytics <- dplyr::left_join(d$data$analytics, mechs, by = "mechanism_code") %>% 
+    dplyr::mutate(
+      mechanism_desc = dplyr::case_when(mechanism_code == "99999" ~ 'Dedupe approximation',
+                                        TRUE ~ mechanism_desc),
+      partner_desc = dplyr::case_when(mechanism_code == "99999" ~ 'Dedupe approximation',
+                                      TRUE ~ partner_desc),
+      partner_id = dplyr::case_when(mechanism_code == "99999" ~ '99999',
+                                    TRUE ~ partner_id),
+      agency = dplyr::case_when(mechanism_code == "99999" ~ 'Dedupe approximation',
+                                TRUE ~ agency))
   
   d
 }
