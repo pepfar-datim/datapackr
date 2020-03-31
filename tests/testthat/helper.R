d51af7e40f87d4b0fe51ed0b2d5e02e6ad1b5538
Jason P. Pickering <jason.p.pickering@gmail.com>
2020-03-31 10:01:39 +0200

    Add unit tests for handshake

---
+++ tests/testthat/helper.R
@@ -0,0 +1,4 @@
+test_sheet <-
+  function(fname)
+    rprojroot::find_package_root_file("inst/extdata",fname)
+
---
+++ tests/testthat/test-handshake.R
@@ -0,0 +1,16 @@
+context("test-template")
+
+
+test_that("Can handshake template", {
+  d<-handshakeFile(test_sheet('COP20_Data_Pack_Template_vFINAL.xlsx'),
+                              'Data Pack Template')
+  expect_true(file.exists(d))
+} )
+
+test_that("Can error on bad type", {
+  
+  expect_error(handshakeFile(test_sheet('COP20_Data_Pack_Template_vFINAL.xlsx'),
+                   'Foo Template'),'Please specify correct file type: Data Pack, Data Pack Template, Site Tool,
+      Site Tool Template, Mechanism Map, Data Pack Template, or Site Filter.')
+
+} )
