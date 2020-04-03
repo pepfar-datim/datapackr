f7cd10ec8c8614ef7c40c5b647c6085f250bc2e4
Jason Pickering <jason.p.pickering@gmail.com>
2020-04-03 11:39:02 +0200

    Add test for invalid country UIDs

--- tests/testthat/test-get-datapack-name.R
+++ tests/testthat/test-get-datapack-name.R
@@ -23,6 +23,19 @@ test_that("Can read a Datapack Name and UIDs", {
   unlink(template_copy)
 } )
 
+test_that("Can error on an invalid regional DataPack UID", {
+  template_copy=paste0(tempfile(),".xlsx")
+  file.copy(from = test_sheet('COP20_Data_Pack_Template_vFINAL.xlsx'), to=template_copy)
+  wb = openxlsx::loadWorkbook(template_copy)
+  
+  home_address<-cellranger::as.cell_addr(countryUIDs_homeCell(), strict = FALSE)
+  openxlsx::writeData(wb = wb,sheet="Home", 
+                      x="Asia_Regional_Data_Pack",
+                      xy = c(home_address$col,home_address$row))
+  openxlsx::saveWorkbook(wb = wb,file = template_copy,overwrite = TRUE)
+  expect_error(unPackCountryUIDs(submission_path = template_copy))
+  unlink(template_copy)
+} )
 
 
 
