795902904c19850c7ac70fa896066499ac79a307
Jason Pickering <jason.p.pickering@gmail.com>
2020-04-03 11:32:00 +0200

    Add more tests for getting country uids

--- tests/testthat/test-get-datapack-name.R
+++ tests/testthat/test-get-datapack-name.R
@@ -1,7 +1,7 @@
 context("test-get-datapack-name")
 
 
-test_that("Can read a Datapack Name", {
+test_that("Can read a Datapack Name and UIDs", {
   template_copy=paste0(tempfile(),".xlsx")
   file.copy(from = test_sheet('COP20_Data_Pack_Template_vFINAL.xlsx'), to=template_copy)
   wb = openxlsx::loadWorkbook(template_copy)
@@ -9,9 +9,20 @@ test_that("Can read a Datapack Name", {
   openxlsx::writeData(wb = wb,sheet="Home", 
                       x="Demoland",
                       xy = c(home_address$col,home_address$row))
+  
+  home_address<-cellranger::as.cell_addr(countryUIDs_homeCell(), strict = FALSE)
+  openxlsx::writeData(wb = wb,sheet="Home", 
+                      x="abc12345678",
+                      xy = c(home_address$col,home_address$row))
+  
   openxlsx::saveWorkbook(wb = wb,file = template_copy,overwrite = TRUE)
   foo<-unPackDataPackName(submission_path = template_copy)
   expect_equal(foo,"Demoland")
+  foo<-unPackCountryUIDs(submission_path = template_copy)
+  expect_equal(foo,"abc12345678")
   unlink(template_copy)
 } )
+
+
+
 
