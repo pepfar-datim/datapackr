abe3314f5527341c936835ee9425dda12efc146f
Scott Jackson <scott.j.jackson@gmail.com>
2020-08-05 13:55:37 -0700

    Resolves pepfar-datim/COP-Target-Setting#849

--- R/unPackSchema.R
+++ R/unPackSchema.R
@@ -9,16 +9,20 @@
 #'
 #' @param filepath Local filepath for a Data Pack template (XLSX).
 #' @param skip Character vector of Sheet Names to label for skipping in schema.
+#' @param type Type of tool to unpack.
 #' @param cop_year Specifies COP year for dating as well as selection of
 #' templates.
 #'
 #' @return Data Pack schema.
 #'
-unPackSchema_datapack <- function(filepath = NULL, skip = NULL, cop_year = getCurrentCOPYear()) {
+unPackSchema_datapack <- function(filepath = NULL,
+                                  skip = NULL,
+                                  type = "Data Pack Template",
+                                  cop_year = getCurrentCOPYear()) {
 
   # Check the filepath is valid. If NA, request via window. ####
   filepath <- handshakeFile(path = filepath,
-                            tool = "Data Pack Template")
+                            tool = type)
 
   schema <- tidyxl::xlsx_cells(path = filepath, include_blank_cells = FALSE) %>%
     dplyr::select(sheet_name = sheet, col, row, character, formula, numeric)
@@ -27,13 +31,13 @@ unPackSchema_datapack <- function(filepath = NULL, skip = NULL, cop_year = getCu
   data.table::setDT(schema)[,sheet_num:=.GRP, by = c("sheet_name")]
 
   # Skip detail on listed sheets. ####
-  if (is.null(skip)) {skip = skip_tabs(tool = "Data Pack", cop_year = cop_year)}
+  if (is.null(skip)) {skip = skip_tabs(tool = type, cop_year = cop_year)}
   sheets <- tidyxl::xlsx_sheet_names(filepath)
   verbose_sheets <- sheets[!sheets %in% skip]
 
   schema %<>%
     dplyr::filter(sheet_name %in% verbose_sheets,
-                  row %in% c(5:(headerRow("Data Pack Template", cop_year)+1))) %>%
+                  row %in% c(5:(headerRow(type, cop_year)+1))) %>%
 
   # Gather and Spread to get formula, value, and indicator_code in separate cols ####
     tidyr::gather(key,value,-sheet_num,-sheet_name,-col,-row) %>%
@@ -56,48 +60,75 @@ unPackSchema_datapack <- function(filepath = NULL, skip = NULL, cop_year = getCu
 
   # When formula is empty, pull from value (Assumed value) ####
     dplyr::mutate(formula = dplyr::if_else(is.na(formula), value, formula))
+    
+  # For OPU Data Packs, delete everything in metadata rows/cols
+  if (type == "OPU Data Pack Template") {
+    schema %<>%
+      dplyr::mutate_at(
+        dplyr::vars(
+          c("dataelement_dsd","dataelement_ta","categoryoption_specified",
+            "valid_ages","valid_sexes","valid_kps")),
+            ~ (. = NA_character_))
+  }
 
   # Translate valid disaggs ####
-  valid_COs <- getValidCategoryOptions(cop_year = cop_year)
-
-  disaggs <- valid_COs %>%
-    dplyr::select(name = datapack_disagg, id, group = datapack_schema_group) %>%
-    dplyr::filter(group != "") %>%
-    tidyr::separate_rows(group, sep = ",") %>%
-    dplyr::arrange(group, name) %>%
-    dplyr::group_by(group) %>%
-    tidyr::nest(options = c(name,id)) %>%
-    tibble::deframe()
-
   empty <- list(tibble::tribble(
     ~name, ~id,
     NA_character_, NA_character_))
-
+  
+  if (type == "OPU Data Pack Template") {
+    disaggs <- list(tibble::tribble(
+      ~name, ~id,
+      NA_character_, NA_character_))
+    
+    schema %<>%
+      dplyr::mutate(
+        valid_ages.options = empty,
+        valid_sexes.options = empty,
+        valid_kps.options = empty
+      )
+    
+    
+  } else {
+    valid_COs <- getValidCategoryOptions(cop_year = cop_year)
+  
+    disaggs <- valid_COs %>%
+      dplyr::select(name = datapack_disagg, id, group = datapack_schema_group) %>%
+      dplyr::filter(group != "") %>%
+      tidyr::separate_rows(group, sep = ",") %>%
+      dplyr::arrange(group, name) %>%
+      dplyr::group_by(group) %>%
+      tidyr::nest(options = c(name,id)) %>%
+      tibble::deframe()
+  
+    schema %<>%
+      dplyr::mutate(
+        valid_ages.options = dplyr::case_when(
+          valid_ages == "5 yr" ~ list(disaggs$`5yr`),
+          valid_ages == "5 yr, 25-49" ~ list(disaggs$`25-49`),
+          valid_ages == "15s" ~ list(disaggs$coarse),
+          valid_ages == "5 yr, 1+" ~ list(disaggs$`01+`),
+          valid_ages == "5 yr, 15+" ~ list(disaggs$`15+`),
+          valid_ages == "5 yr, 10+" ~ list(disaggs$`10+`),
+          valid_ages == "5 yr, <01-18+" ~ list(disaggs$ovc_serv),
+          valid_ages == "5 yr, <01-17" ~ list(disaggs$ovc_hiv_stat),
+          valid_ages == "01-04" ~ list(disaggs$`01-04`),
+          valid_ages == "<01" ~ list(disaggs$`<01`),
+          TRUE ~ empty),
+        valid_sexes.options = dplyr::case_when(
+          valid_sexes == "M/F" ~ list(disaggs$`M/F`),
+          valid_sexes == "M" ~ list(disaggs$`M/F`[disaggs$`M/F`$name == "Male",]),
+          valid_sexes == "F" ~ list(disaggs$`M/F`[disaggs$`M/F`$name == "Female",]),
+          TRUE ~ empty),
+        valid_kps.options = dplyr::case_when(
+          valid_kps == "coarseKPs" ~ list(disaggs$coarseKPs),
+          valid_kps == "fineKPs" ~ list(disaggs$fineKPs),
+          valid_kps == "pwidKPs" ~ list(disaggs$pwidKPs),
+          TRUE ~ empty)
+        )
+  }
+  
   schema %<>%
-    dplyr::mutate(
-      valid_ages.options = dplyr::case_when(
-        valid_ages == "5 yr" ~ list(disaggs$`5yr`),
-        valid_ages == "5 yr, 25-49" ~ list(disaggs$`25-49`),
-        valid_ages == "15s" ~ list(disaggs$coarse),
-        valid_ages == "5 yr, 1+" ~ list(disaggs$`01+`),
-        valid_ages == "5 yr, 15+" ~ list(disaggs$`15+`),
-        valid_ages == "5 yr, 10+" ~ list(disaggs$`10+`),
-        valid_ages == "5 yr, <01-18+" ~ list(disaggs$ovc_serv),
-        valid_ages == "5 yr, <01-17" ~ list(disaggs$ovc_hiv_stat),
-        valid_ages == "01-04" ~ list(disaggs$`01-04`),
-        valid_ages == "<01" ~ list(disaggs$`<01`),
-        TRUE ~ empty),
-      valid_sexes.options = dplyr::case_when(
-        valid_sexes == "M/F" ~ list(disaggs$`M/F`),
-        valid_sexes == "M" ~ list(disaggs$`M/F`[disaggs$`M/F`$name == "Male",]),
-        valid_sexes == "F" ~ list(disaggs$`M/F`[disaggs$`M/F`$name == "Female",]),
-        TRUE ~ empty),
-      valid_kps.options = dplyr::case_when(
-        valid_kps == "coarseKPs" ~ list(disaggs$coarseKPs),
-        valid_kps == "fineKPs" ~ list(disaggs$fineKPs),
-        valid_kps == "pwidKPs" ~ list(disaggs$pwidKPs),
-        TRUE ~ empty)
-      ) %>%
     dplyr::select(sheet_num, sheet_name, col, indicator_code,
                   dataset, col_type, value_type,
                   dataelement_dsd, dataelement_ta, categoryoption_specified,
---
+++ data-raw/update_cop20OPU_datapack_schema.R
@@ -0,0 +1,30 @@
+## If you've made any edits to the Excel template, rebuild package first to
+## capture these, then run the below.
+
+secrets <- "/Users/scott/.secrets/triage.json"
+
+datapackr::loginToDATIM(secrets)
+
+datapack_template_filepath <- system.file("extdata",
+                                          "COP20_OPU_Data_Pack_Template.xlsx",
+                                          package = "datapackr",
+                                          mustWork = TRUE)
+cop20OPU_data_pack_schema <-
+  unPackSchema_datapack(
+    filepath = datapack_template_filepath,
+    skip = skip_tabs(tool = "OPU Data Pack Template", cop_year = 2020),
+    cop_year = 2020)
+
+comparison <- compareDF::compare_df(df_new = cop20_data_pack_schema,
+                                    df_old = datapackr::cop20_data_pack_schema,
+                                    group_col = c("sheet_num", "sheet_name", "col"))
+
+comparison_df <- comparison$comparison_df
+
+View(comparison_df)
+
+save(cop20OPU_data_pack_schema,
+     file = "./data/cop20OPU_data_pack_schema.rda",
+     compress = "xz")
+
+## Rebuld package again.
