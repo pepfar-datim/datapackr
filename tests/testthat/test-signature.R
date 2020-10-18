fe0d7d5bc623bde2dce083f6111bc74fc8513e14
Scott Jackson <scott.j.jackson@gmail.com>
2020-10-17 17:26:05 -0700

    Fix test-signature to use correct root directory

--- tests/testthat/test-signature.R
+++ tests/testthat/test-signature.R
@@ -2,12 +2,12 @@ context("test-signature")
 
 
 test_that("Can generate a signature", {
-  d<-datapackr:::createKeychainInfo(submission_path = test_sheet('COP20_Data_Pack_Template_vFINAL.xlsx'),
+  d <- datapackr:::createKeychainInfo(submission_path = test_sheet('COP20_Data_Pack_Template_vFINAL.xlsx'),
                         tool = "Data Pack Template",
                         country_uids = NULL,
                         cop_year = NULL) 
-  expect_setequal(names(d), c("info", "keychain"))
-   expect_setequal(
+  testthat::expect_setequal(names(d), c("info", "keychain"))
+  testthat::expect_setequal(
      names(d$info),
      c(
        "datapack_name",
@@ -23,7 +23,7 @@ test_that("Can generate a signature", {
    expect_null(d$info$warning_msg)
    expect_false(d$info$has_error)
    expect_false(d$info$newSNUxIM)
-   expect_equal(d$info$cop_year,getCurrentCOPYear())
+   #expect_equal(d$info$cop_year,getCurrentCOPYear())
    #Since we are using a specific version of the template for these tests
    expect_equal(d$info$cop_year,2020)
   
