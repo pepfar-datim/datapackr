ab0fee3c54bcbadbca5c8344e6fc37878b3c238f
maxwellchandler <mchandler@baosystems.com>
2020-11-05 14:03:02 -0700

    refactor pullhtsmodality

--- R/pullHTSModality.R
+++ R/pullHTSModality.R
@@ -9,27 +9,32 @@
 #' 
 #' @return Dataframe of HTS modalities mapped to dataElements
 #' 
+
 getHTSModality <- function(cop_year = getCurrentCOPYear(), dataElements = NULL) {
-  if (cop_year == 2020) {
-    groupSet = "ra9ZqrTtSQn"
+
+      if (cop_year == 2020) {
+    groupSet <- "ra9ZqrTtSQn"
   } else if (cop_year == 2019) {
-    groupSet = "Jm6OwL9IqEa"
+    groupSet <- "Jm6OwL9IqEa"
   } else if (cop_year == 2018) {
-    groupSet = "CKTkg8dLlr7"}
-  
-  modality_map <- api_call(paste0("dataElementGroupSets/",groupSet)) %>%
-    api_fields("dataElementGroups[name,dataElements[id]]") %>%
-    api_get() %>%
-    tidyr::unnest(cols = dataElements) %>%
+    groupSet <- "CKTkg8dLlr7"}
+
+  modality_map <- datimutils::getDataElementGroupSets(groupSet,
+                                    by = "id",
+                                    fields = "dataElementGroups[name,dataElements[id]]")
+
+  modality_map <- modality_map$dataElementGroups[[1]]
+
+  modality_map <- modality_map %>% tidyr::unnest(cols = dataElements) %>%
     dplyr::distinct() %>%
     dplyr::select(dataElement = id,
                   hts_modality = name ) %>%
     dplyr::mutate(hts_modality = stringr::str_remove(hts_modality,"FY\\d{2}R/FY\\d{2}T"))
-  
+
   if (!is.null(dataElements)) {
     modality_map %<>%
       dplyr::filter(dataElement %in% dataElements)
   }
-  
-  return(modality_map)   
+
+  return(modality_map)
 }
---
+++ tests/testthat/test-pullHTSModality.R
@@ -0,0 +1,14 @@
+library(httptest)
+
+with_mock_api({
+
+test_that("test getHTSModality: ", {
+
+data <- getHTSModality(cop_year = 2020)
+
+testthat::expect_s3_class(data, "data.frame")
+testthat::expect_equal(NROW(data), 88)
+testthat::expect_named(data, c("dataElement", "hts_modality"))
+rm(data) })
+
+})
