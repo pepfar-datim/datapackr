207158b815a63f3d54e001db626d729cfc3e45f1
Scott Jackson <scott.j.jackson@gmail.com>
2020-12-21 10:13:05 -0700

    Add for newest data model

--- data-raw/COP21_Data_Pack_generation_script.R
+++ data-raw/COP21_Data_Pack_generation_script.R
@@ -7,7 +7,7 @@ datapackr::loginToDATIM(secrets)
 
 output_folder <- "/Users/scott/Google Drive/PEPFAR/COP Targets/COP 21/3) Testing & Deployment/Data Packs"
 
-model_data_path <- "/Users/scott/Google Drive/PEPFAR/COP Targets/COP 21/3) Testing & Deployment/model_data_pack_input_21_20201202_1_flat.rds"
+model_data_path <- "/Users/scott/Google Drive/PEPFAR/COP Targets/COP 21/3) Testing & Deployment/model_data_pack_input_21_20201216_1_flat.rds"
 
 model_data <- readRDS(model_data_path)
 
