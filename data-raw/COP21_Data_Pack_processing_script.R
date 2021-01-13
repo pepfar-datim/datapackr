9ad3ea8452ee718cf3f5c94c5258022217a2d5be
Scott Jackson <scott.j.jackson@gmail.com>
2021-01-13 07:08:10 -0800

    Add new PSNUxIM model file

--- data-raw/COP21_Data_Pack_processing_script.R
+++ data-raw/COP21_Data_Pack_processing_script.R
@@ -1,7 +1,7 @@
 
 datapackr::loginToDATIM("~/.secrets/datim.json")
 
-snuxim_model_data_path <- "/Users/scott/Google Drive/PEPFAR/COP Targets/COP 21/3) Testing & Deployment/PSNUxIM_20201229_1.rds"
+snuxim_model_data_path <- "/Users/scott/Google Drive/PEPFAR/COP Targets/COP 21/3) Testing & Deployment/PSNUxIM_20210113_1.rds"
 output_folder <- "/Users/scott/Google Drive/PEPFAR/COP Targets/COP 21/3) Testing & Deployment"
 #model_data_path <- "/Users/scott/Google Drive/PEPFAR/COP Targets/COP 21/3) Testing & Deployment/model_data_pack_input_20_20200220_1_flat.rds"
 
