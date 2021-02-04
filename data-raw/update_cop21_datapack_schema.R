24618929494f794814fa31203719c3ccabfb0939
Scott Jackson <scott.j.jackson@gmail.com>
2021-02-03 18:22:23 -0800

    Add FY to schema
    - Correct from .T to .T_1 for KP_ESTIMATES
    - Add FY to schema

--- data-raw/update_cop21_datapack_schema.R
+++ data-raw/update_cop21_datapack_schema.R
@@ -1,9 +1,7 @@
 ## If you've made any edits to the Excel template, rebuild package first to
 ## capture these, then run the below.
 
-secrets <- "/Users/scott/.secrets/cop-test.json"
-
-datapackr::loginToDATIM(secrets)
+datapackr::loginToDATIM("/Users/scott/.secrets/datim.json")
 
 datapack_template_filepath <- system.file("extdata",
                                           "COP21_Data_Pack_Template.xlsx",
