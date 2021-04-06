b47b89a1244601774add74b5565076eef430e1ce
maxwellchandler <mchandler@baosystems.com>
2021-04-06 14:33:13 -0600

    removed code that generates excel sheet and just use sheet

--- tests/testthat/test-adorn-mechanisms.R
+++ tests/testthat/test-adorn-mechanisms.R
@@ -1,27 +1,6 @@
 
 context("Get a mechanism view when logged into DATIM")
 
-# 
-#   test_that("We can get a warning when not logged in", {
-#     datimutils::loginToDATIM(config_path = secrets)
-#     expect_true(exists("d2_default_session"))
-#     #Kill the handle to simulate a dead one
-#     #TODO: Not clear why the method expects a blank URL....
-#     d2_default_session$base_url<-NULL
-#     test_mech_list<-expect_warning(getMechanismViewFromDATIM(d2_session = d2_default_session))
-#     expect_type(test_mech_list,"list") 
-#     #Should return an empty mechs list
-#     expect_equal(NROW(test_mech_list),0)
-#     mechs_names<-c("mechanism_desc",
-#                    "mechanism_code",
-#                    "attributeOptionCombo",
-#                    "partner_desc","partner_id",
-#                    "agency","ou",
-#                    "startdate","enddate")  
-#     expect_true(setequal(mechs_names,names(test_mech_list)))
-#   })
-
-
 with_mock_api({
   test_that("We can get a mechanism list when logged in", {
     datimutils::loginToDATIM(config_path = secrets)
--- tests/testthat/test-signature.R
+++ tests/testthat/test-signature.R
@@ -1,23 +1,25 @@
 context("test-signature")
 
 test_that("Can generate a signature", {
-   wb <- xlsx::loadWorkbook("sheets/COP20_Data_Pack_Template_vFINAL.xlsx")
-   df <- data.frame("a" = "e5s0nMiYRt2","b" =1, "c" = 1,"d" = 1,"e" = 1,"f" = 1,"g" = 1,"h" = 1,"i" = 1,"j" = 1,"k" = 1,"l" = '1')
-   xlsx::addDataFrame(df, xlsx::getSheets(wb)$Prioritization, startRow = 15, col.names = F, row.names = F)
-   xlsx::saveWorkbook(wb, "sheets/COP20_Data_Pack_Template_v2.xlsx")
-   while(!(file.exists("sheets/COP20_Data_Pack_Template_v2.xlsx"))){
-      print("waiting")
-      Sys.sleep(5)
-   }
-   wb <- xlsx::loadWorkbook("sheets/COP20_Data_Pack_Template_v2.xlsx")
-   df <- data.frame("a" = "IH1kchw86uA")
-   xlsx::addDataFrame(df, xlsx::getSheets(wb)$Home, startRow = 25, startColumn = 2, col.names = F, row.names = F)
-   xlsx::saveWorkbook(wb, "sheets/COP20_Data_Pack_Template_v2.xlsx")
-   while(!(file.exists("sheets/COP20_Data_Pack_Template_v2.xlsx"))){
-      print("waiting")
-      Sys.sleep(5)
-   }
-  d <- datapackr:::createKeychainInfo(submission_path = 'sheets/COP20_Data_Pack_Template_v2.xlsx',
+   
+   #demostration of why using openxlsx will not work, tests will fail, end file is corrupted
+   # wb <- openxlsx::loadWorkbook("sheets/COP20_Data_Pack_Template_vFINAL.xlsx")
+   # df <- data.frame("a" = "e5s0nMiYRt2","b" =1, "c" = 1,"d" = 1,"e" = 1,"f" = 1,"g" = 1,"h" = 1,"i" = 1,"j" = 1,"k" = 1,"l" = '1')
+   # openxlsx::writeData(wb, sheet = "Prioritization", x = df,  startRow = 15, colNames = F, rowNames = F)
+   # df <- data.frame("a" = "IH1kchw86uA")
+   # openxlsx::writeData(wb, sheet = "Home", x = df,  startRow = 25, startCol = 2, colNames = F, rowNames = F)
+   # openxlsx::saveWorkbook(wb, "sheets/COP20_Data_Pack_Template_v2.xlsx", overwrite = T)
+   # 
+   
+   #the correct way using xlsx
+   # wb <- xlsx::loadWorkbook("sheets/COP20_Data_Pack_Template_vFINAL.xlsx")
+   # df <- data.frame("a" = "e5s0nMiYRt2","b" =1, "c" = 1,"d" = 1,"e" = 1,"f" = 1,"g" = 1,"h" = 1,"i" = 1,"j" = 1,"k" = 1,"l" = '1')
+   # xlsx::addDataFrame(df, xlsx::getSheets(wb)$Prioritization, startRow = 15, col.names = F, row.names = F)
+   # df <- data.frame("a" = "IH1kchw86uA")
+   # xlsx::addDataFrame(df, xlsx::getSheets(wb)$Home, startRow = 25, startColumn = 2, col.names = F, row.names = F)
+   # xlsx::saveWorkbook(wb, "sheets/COP20_Data_Pack_Template_v2.xlsx")
+   
+   d <- datapackr:::createKeychainInfo(submission_path = 'sheets/COP20_Data_Pack_Template_v2.xlsx',
                         tool = "Data Pack",
                         country_uids = NULL,
                         cop_year = NULL) 
