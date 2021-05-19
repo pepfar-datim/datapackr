ad6edca59bcf4282db102624ac07749c3f2e0690
Jason P. Pickering <jason.p.pickering@gmail.com>
2021-05-19 11:43:34 +0200

    Fix code list tests

---
+++ tests/testthat/test-pull-code-list.R
@@ -0,0 +1,17 @@
+with_mock_api({
+  test_that("We can fetch a code list from DATIM", {
+    datimutils::loginToDATIM(config_path = test_config("test-config.json"))
+    expect_true(exists("d2_default_session"))
+    test_dataset<-pullDATIMCodeList("YfZot37BbTm")
+    expect_type(test_dataset,"list")
+    test_dataset_names<-c("dataset",
+                   "dataelement",
+                   "shortname",
+                   "code",
+                   "dataelementuid",
+                   "dataelementdesc",
+                   "categoryoptioncombo",
+                   "categoryoptioncombocode","categoryoptioncombouid")
+    expect_true(setequal(test_dataset_names,names(test_dataset)))
+  })
+})
