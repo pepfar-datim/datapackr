514c74f48bf680c1efc13850cdb8d9eacd652df6
Jason P. Pickering <jason.p.pickering@gmail.com>
2021-05-20 09:33:54 +0200

    Add test for getCurrentCopYear

--- tests/testthat/test-rounding.R
+++ tests/testthat/test-rounding.R
@@ -1,4 +1,4 @@
-context("rounding")
+context("Utility functions")
 
 test_that("can properly round numbers", {
   expect_equal(round_trunc(0),0L)
@@ -10,3 +10,8 @@ test_that("can properly round numbers", {
   expect_equal(round_trunc(-2.5),-3L)
   expect_equal(round_trunc(-2.4231),-2L)
   expect_equal(round_trunc(100),100L) })
+
+test_that("Can get current COP year",{
+  expect_true(is.numeric(getCurrentCOPYear()))
+  expect_equal(getCurrentCOPYear(),2021)
+})
