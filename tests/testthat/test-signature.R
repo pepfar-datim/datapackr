8c3a21496e0b9775d43cd7d49ecabe19e252ac6d
Jason P. Pickering <jason.p.pickering@gmail.com>
2021-05-20 09:48:53 +0200

    Fix signature test

--- tests/testthat/test-signature.R
+++ tests/testthat/test-signature.R
@@ -1,37 +1,37 @@
 context("test-signature")
 
 
-# test_that("Can generate a signature", {
-#   d <- datapackr:::createKeychainInfo(submission_path = test_sheet('COP20_Data_Pack_Template_vFINAL.xlsx'),
-#                         tool = "Data Pack Template",
-#                         country_uids = NULL,
-#                         cop_year = NULL) 
-#   testthat::expect_setequal(names(d), c("info", "keychain"))
-#   testthat::expect_setequal(
-#      names(d$info),
-#      c(
-#        "datapack_name",
-#        "tool",
-#        "country_uids",
-#        "cop_year",
-#        "schema",
-#        "has_error",
-#        "newSNUxIM",
-#        "has_psnuxim",
-#        "missing_psnuxim_combos",
-#        "missing_DSNUs"
-#      )
-#    )
-#    expect_null(d$info$warning_msg)
-#    expect_false(d$info$has_error)
-#    expect_false(d$info$newSNUxIM)
-# 
-#   
-# } )
-# 
-# test_that("Can get the type and COP year of tool of a COP20 Data Pack",{
-#    
-#    d <- datapackr:::createKeychainInfo(submission_path = test_sheet('COP20_Data_Pack_Template_vFINAL.xlsx'))
-#    expect_equal(d$info$tool, "Data Pack Template")
-#    expect_equal(d$info$cop_year, 2020)
-# })
+test_that("Can generate a signature", {
+  d <- datapackr:::createKeychainInfo(submission_path = test_sheet('COP21_Data_Pack_Template.xlsx'),
+                        tool = "Data Pack",
+                        country_uids = NULL,
+                        cop_year = NULL)
+  testthat::expect_setequal(names(d), c("info", "keychain"))
+  testthat::expect_setequal(
+     names(d$info),
+     c(
+       "datapack_name",
+       "tool",
+       "country_uids",
+       "cop_year",
+       "schema",
+       "has_error",
+       "newSNUxIM",
+       "has_psnuxim",
+       "missing_psnuxim_combos",
+       "missing_DSNUs"
+     )
+   )
+   expect_null(d$info$warning_msg)
+   expect_false(d$info$has_error)
+   expect_false(d$info$newSNUxIM)
+
+
+} )
+
+test_that("Can get the type and COP year of tool of a COP21 Data Pack",{
+
+   d <- datapackr:::createKeychainInfo(submission_path = test_sheet('COP21_Data_Pack_Template.xlsx'))
+   expect_equal(d$info$tool, "Data Pack")
+   expect_equal(d$info$cop_year, 2021)
+})
