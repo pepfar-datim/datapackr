f7d0f3e74ad619bce826a4f5871bb1970b61d052
Jason P. Pickering <jason.p.pickering@gmail.com>
2021-05-20 17:09:43 +0200

    Add tests for checkStructure

---
+++ tests/testthat/test-checkStructure.R
@@ -0,0 +1,28 @@
+context("test-checkStructure")
+
+
+test_that("Can pass a good workbook", {
+  d <- datapackr:::createKeychainInfo(submission_path = test_sheet('COP21_Data_Pack_Template.xlsx'),
+                                      tool = "Data Pack",
+                                      country_uids = NULL,
+                                      cop_year = NULL)
+  expect_silent(foo<-checkStructure(d))
+  expect_equal(NROW(foo$tests$missing_sheets),0L)
+} )
+
+test_that("Can warn on missing sheet" , {
+  
+  
+  template_copy=paste0(tempfile(),".xlsx")
+  file.copy(from = test_sheet("COP21_Data_Pack_Template.xlsx"), to=template_copy)
+  wb = openxlsx::loadWorkbook(template_copy)
+  openxlsx::removeWorksheet(wb,"PMTCT")
+  openxlsx::saveWorkbook(wb,file=template_copy,overwrite = TRUE)
+  d <- datapackr:::createKeychainInfo(submission_path = template_copy,
+                                      tool = "Data Pack",
+                                      country_uids = NULL,
+                                      cop_year = NULL)
+  expect_silent(foo<-checkStructure(d))
+  expect_equal(NROW(foo$tests$missing_sheets),1L)
+  expect_true(grepl("MISSING SHEETS",foo$info$warning_msg)) 
+  })
