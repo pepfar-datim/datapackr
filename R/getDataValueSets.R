d18e48209ea8e4a1d691bedc300c60ee46f9c260
Jason P. Pickering <jason.p.pickering@gmail.com>
2021-05-27 09:11:48 +0200

    Minor refactor of getDataValue set

--- R/getDataValueSets.R
+++ R/getDataValueSets.R
@@ -6,12 +6,10 @@
 #' @param values character vector - values marching the key from keys (e.g. "Abcde123456", "2019Q1"
 #' @param base_url string - base address of instance (text before api/ in URL)
 #' @param api_version - api version to use when calling DHIS2
-#' @param max_attempts - number of times to try for a valid response
 #' @return  tibble with the data requested
 #'
 getDataValueSets <- function(keys, values,  
                             api_version = datapackr::api_version(),
-                            max_attempts = 3,
                             d2_session = dynGet("d2_default_session",
                                                 inherits = TRUE)){
   
@@ -25,14 +23,13 @@ getDataValueSets <- function(keys, values,
   # we can disregard that warning
   #  row col   expected     actual         file
   #  1  -- 11 columns 10 columns literal data
-  data <- datapackcommons::RetryAPI(api_call, "application/csv", max_attempts = max_attempts,
-                                    d2_session = d2_session) %>%   
-    httr::content(., "text") %>% 
-    {suppressWarnings(readr::read_csv(., 
-                                      col_names = TRUE, 
-                                      col_types = readr::cols(.default = "c", 
-                                                              value = "d", 
-                                                              lastupdated = "T")))} %>%
+  
+  data <- httr::GET(api_call,handle = d2_session$handle) %>%
+    httr::content(.,"text") %>% 
+    readr::read_csv(.,col_names = TRUE, 
+                    col_types = readr::cols(.default = "c", 
+                                            value = "d", 
+                                            lastupdated = "T")) %>%
     dplyr::rename(data_element = dataelement, 
                   org_unit = orgunit,
                   category_option_combo = categoryoptioncombo,
--- tests/testthat/test-GetDataValueSet.R
+++ tests/testthat/test-GetDataValueSet.R
@@ -1,13 +1,16 @@
 context("Test GetDataValueSets")
 
+
+#I think this should be using a mock, otherwise, its easy that it is going to fail. 
+
 test_that("GetDataValueSets", {
   datimutils::loginToDATIM(username = "admin", 
                            password = "district",
                            base_url = "https://play.dhis2.org/2.34.4/",
                            d2_session_name = "play")
-  data <- datapackr:::getDataValueSets(c("dataElementGroup", "orgUnit", "startDate", "endDate", "children", "limit"), 
+  expect_warning(data <- datapackr:::getDataValueSets(c("dataElementGroup", "orgUnit", "startDate", "endDate", "children", "limit"), 
                   c("qfxEYY9xAl6", "O6uvpzGd5pu", "2013-01-01", "2090-01-01","true","25"), 
-                  d2_session = play)
+                  d2_session = play))
   testthat::expect_named(data, c("data_element",
                          "period",
                          "org_unit",
@@ -22,6 +25,5 @@ test_that("GetDataValueSets", {
   testthat::expect_equal(NROW(data), 25)
   testthat::expect_error(getDataValueSets(c("limit"), 
                                          c("25"), 
-                                         base, 
-                                         max_attempts = 1))
+                                         base))
   })
