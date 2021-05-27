576ffff74ed804134fa7f38c7d173beb07085125
Jason P. Pickering <jason.p.pickering@gmail.com>
2021-05-27 10:41:45 +0200

    Add test for analytics creation

---
+++ tests/testthat/test-create-analytics.R
@@ -0,0 +1,53 @@
+context("Analytics creation tests") 
+
+
+with_mock_api({
+  test_that("We can create analytics", {
+    datimutils::loginToDATIM(config_path = test_config("test-config.json"))
+    expect_true(exists("d2_default_session"))
+    
+    d <-
+      datapackr:::createKeychainInfo(
+        submission_path = test_sheet('COP21_DP_random_with_psnuxim.xlsx'),
+        tool = "Data Pack",
+        country_uids = NULL,
+        cop_year = NULL
+      )
+    d <-  d %>% unPackSheets(.) %>%
+      separateDataSets(.) %>%
+      unPackSNUxIM(.) %>%
+      packForDATIM(., type = "Undistributed MER") %>%
+      packForDATIM(., type = "SUBNAT_IMPATT") %>%
+      packForDATIM(., type = "PSNUxIM") %>%
+      createAnalytics(.)
+    
+    
+    expect_named(d,
+                 c("keychain", "info", "tests", "data", "datim"),
+                 ignore.order = TRUE)
+    analytics_column_names <-
+      c(
+        'ou','ou_id','country_name','country_uid','snu1','snu1_id','psnu',
+        'psnu_uid','prioritization','mechanism_code','mechanism_desc','partner_id',
+        'partner_desc','funding_agency','fiscal_year','dataelement_id','dataelement_name',
+        'indicator','numerator_denominator','support_type','hts_modality',
+        'categoryoptioncombo_id','categoryoptioncombo_name','age','sex',
+        'key_population','resultstatus_specific','upload_timestamp',
+        'disagg_type','resultstatus_inclusive','top_level',
+        'target_value',
+        'indicator_code'
+      )
+    expect_true(!is.null(d$data$analytics))
+    expect_true(NROW(d$data$analytics) > 0)
+    expect_named(d$data$analytics,analytics_column_names)
+   numeric_columns<-c("fiscal_year","target_value")
+   foo<-sapply(d$data$analytics,typeof)
+   expect_true(all(foo[!(names(d$data$analytics) %in%  numeric_columns)] == "character"))
+   expect_true(all(foo[(names(d$data$analytics) %in%  numeric_columns)] == "double"))
+   expect_true(all(sapply(d$data$analytics$ou_id,is_uidish)))
+   expect_true(all(sapply(d$data$analytics$country_uid,is_uidish)))
+   expect_true(all(sapply(d$data$analytics$snu1_id,is_uidish)))
+   expect_true(all(sapply(d$data$analytics$psnu_uid,is_uidish)))
+   expect_true(all(sapply(d$data$analytics$dataelement_id,is_uidish)))
+  })
+})
