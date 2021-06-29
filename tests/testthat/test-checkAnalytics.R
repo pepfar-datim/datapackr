7d2bf17863ca6a320aa6434141251599cbc35c7a
Sam Garman <36417685+sam-bao@users.noreply.github.com>
2021-06-29 09:13:52 +0200

    Check analytics tests sam (#298)
    * supplement test data for PMTCT_EID coverage by 2 months old < 90% expect message
    
    Add a row that should NOT cause an error.
    
    * test for exclusion of keypop records in VMMC_CIRC Indeterminate Rate
    
    This test should not include cases with a key pop disagg
    
    * add boundary case for PMTCT Known Pos/PMTCT Total <  0.75 expect null
    
    Check that we also do not get an error if ratio = .75
    
    * test boundry for TB Known Pos ratio < 75% expect message expect null
    
    * TB Known Pos ratio > 75% expect message test value closer to boundary
    
    * Test retention < 98% expect message test closer to boundary
    
    * Test retention > 100% expect message test at boundary
    
    * Update test-checkAnalytics.R
    
    * checkAnalytics tests move linkage tests closer to boundaries
    
    * Update test-checkAnalytics.R
    
    Add data for the KP versions of linkage.
    
    * Update test-checkAnalytics.R
    
    Split out the KP tests because the ones from the previous commit confuses the disaggs.
    
    * Update test-checkAnalytics.R
    
    Add linkage test with data for age <01

--- tests/testthat/test-checkAnalytics.R
+++ tests/testthat/test-checkAnalytics.R
@@ -3,7 +3,8 @@ context("test-check-analytics")
 test_that("PMTCT_EID coverage by 2 months old < 90% expect message" , {
   data<-tribble(
     ~psnu, ~psnu_uid, ~age, ~sex, ~key_population,~PMTCT_EID.N.2.T,~PMTCT_EID.N.12.T,
-    "a",   1,         "<1",  "F",  NA,                         1,        100
+    "a",   1,         "<1",  "F",  NA,                         1,        100,
+    "b",   1,         "<1",  "F",  NA,                         90,        10
   )
   
   foo<-analyze_eid_2mo(data)
@@ -75,6 +76,17 @@ test_that("VMMC_CIRC Indeterminate Rate all zeros expect NULL" , {
   
 } )
 
+test_that("VMMC_CIRC Indeterminate Rate all keypop not NA expect NULL" , {
+  data<-tribble(
+    ~psnu, ~psnu_uid, ~age, ~sex, ~key_population,~VMMC_CIRC.Pos.T,~VMMC_CIRC.Neg.T,~VMMC_CIRC.Unk.T,
+    "a",   1,         "<1",  "M",  "PWID",                         0,        0, 1
+  )
+  
+  foo<-analyze_vmmc_indeterminate(data)
+  expect_null(foo)
+  
+} )
+
 test_that("PMTCT Known Pos/PMTCT Total >  0.75 expect message" , {
   data<-tribble(
     ~psnu, ~psnu_uid, ~age, ~sex, ~key_population,~PMTCT_STAT.N.New.Pos.T,~PMTCT_STAT.N.KnownPos.T,~PMTCT_STAT.N.New.Neg.T,
@@ -95,7 +107,8 @@ test_that("PMTCT Known Pos/PMTCT Total <  0.75 expect null" , {
   data<-tribble(
     ~psnu, ~psnu_uid, ~age, ~sex, ~key_population,~PMTCT_STAT.N.New.Pos.T,~PMTCT_STAT.N.KnownPos.T,~PMTCT_STAT.N.New.Neg.T,
     "a",   1,         "<1",  "M",  NA,                         10,        10, 10,
-    "b",  2,          "<1", "M", NA,                         0,         0, 0
+    "b",  2,          "<1", "M", NA,                         0,         0, 0,
+    "c",  3,          "<1", "M", NA,                         25,         150, 25
   )
   
   expect_null(analyze_pmtctknownpos(data))
@@ -116,7 +129,7 @@ test_that("PMTCT Known Pos/PMTCT Total all zeros expect null" , {
 test_that("TB Known Pos ratio > 75% expect message" , {
   data<-tribble(
     ~psnu, ~psnu_uid, ~age, ~sex, ~key_population,~TB_STAT.N.New.Pos.T,~TB_STAT.N.KnownPos.T,~TB_STAT.N.New.Neg.T,
-    "a",   1,         "<1",  "M",  NA,                         10,        100, 10,
+    "a",   1,         "<1",  "M",  NA,                         25,        151, 25,
     "b",  2,          "<1", "M", NA,                         0,         0, 0
   )
   
@@ -124,7 +137,7 @@ test_that("TB Known Pos ratio > 75% expect message" , {
   testthat::expect_equal(class(foo),"list")
   testthat::expect_setequal(names(foo),c("test_results","msg"))
   testthat::expect_equal(NROW(foo$test_results),1)
-  expect_equal(foo$test_results$knownpos_ratio,0.833,tolerance=1e-3)
+  expect_equal(foo$test_results$knownpos_ratio,0.751,tolerance=1e-3)
   
   
 } )
@@ -132,8 +145,8 @@ test_that("TB Known Pos ratio > 75% expect message" , {
 test_that("TB Known Pos ratio < 75% expect message expect null" , {
   data<-tribble(
     ~psnu, ~psnu_uid, ~age, ~sex, ~key_population,~TB_STAT.N.New.Pos.T,~TB_STAT.N.KnownPos.T,~TB_STAT.N.New.Neg.T,
-    "a",   1,         "<1",  "M",  NA,                         10,        10, 10,
-    "b",  2,          "<1", "M", NA,                         0,         0, 0
+    "a",   1,         "<1",  "M",  NA,                         25,        150, 25,
+    "b",  2,          "<1", "M", NA,                         0,         0, 0,
   )
   
   expect_null(analyze_tbknownpos(data))
@@ -154,7 +167,7 @@ test_that("PMTCT Known Pos/PMTCT Total all zeros expect null" , {
 test_that(" Test retention < 98% expect message", {
   data<-tribble(
     ~psnu, ~psnu_uid, ~age, ~sex, ~key_population,~TX_CURR.T,~TX_CURR.T_1,~TX_NEW.T,
-    "a",   1,         "<1",  "F",  NA,                    10,        10, 10,
+    "a",   1,         "<1",  "F",  NA,                    97,        97, 3,
     "b",  2,          "<1", "M", NA,                         0,         0, 0
   )
   
@@ -162,22 +175,22 @@ test_that(" Test retention < 98% expect message", {
   testthat::expect_equal(class(foo),"list")
   testthat::expect_setequal(names(foo),c("test_results","msg"))
   testthat::expect_equal(NROW(foo$test_results),1)
-  expect_equal(foo$test_results$TX.Retention.T,0.5,tolerance=1e-3)
+  expect_equal(foo$test_results$TX.Retention.T,0.97,tolerance=1e-3)
   
 } )
 
 test_that(" Test retention > 100% expect message", {
   data<-tribble(
     ~psnu, ~psnu_uid, ~age, ~sex, ~key_population,~TX_CURR.T,~TX_CURR.T_1,~TX_NEW.T,
-    "a",   1,         "<1",  "F",  NA,                    100,        10, 10,
-    "b",  2,          "<1", "M", NA,                         0,         0, 0
+    "a",   1,         "<1",  "F",  NA,                    101,        10, 90,
+    "b",  2,          "<1", "M", NA,                      100,        10, 90
   )
   
   foo<-analyze_retention(data)
   testthat::expect_equal(class(foo),"list")
   testthat::expect_setequal(names(foo),c("test_results","msg"))
   testthat::expect_equal(NROW(foo$test_results),1)
-  expect_equal(foo$test_results$TX.Retention.T,5,tolerance=1e-3)
+  expect_equal(foo$test_results$TX.Retention.T,1.01,tolerance=1e-3)
   
 } )
 
@@ -206,31 +219,57 @@ test_that(" Test retention all zeros expect NULL", {
 test_that(" Test linkage < 95% expect message", {
   data<-tribble(
     ~psnu, ~psnu_uid, ~age, ~sex, ~key_population,~HTS_INDEX_COM.New.Pos.T,~HTS_INDEX_FAC.New.Pos.T,~TX_NEW.T,~HTS_TST.KP.Pos.T,~TX_NEW.KP.T,
-    "a",   1,         "25-49",  "F",  NA,                    100,          10,                                10,                 0,               0,
-    "b",  2,          "25-49", "M",  NA,                         0,           0,                                0,                 0,               0
+    "a",   1,         "25-49",  "F",  NA,                    95,          5,                                94,                 0,               0,
+    "b",  2,          "25-49", "M",  NA,                         95,           5,                                95,                 0,               0
   )
   
   foo<-analyze_linkage(data)
   testthat::expect_equal(class(foo),"list")
   testthat::expect_setequal(names(foo),c("test_results","msg"))
   testthat::expect_equal(NROW(foo$test_results),1)
-  expect_equal(foo$test_results$HTS_TST.Linkage.T,0.0909,tolerance=1e-3)
+  expect_equal(foo$test_results$HTS_TST.Linkage.T,0.94,tolerance=1e-3)
+} )
+
+test_that(" Test KP linkage < 95% expect message", {
+  data<-tribble(
+    ~psnu, ~psnu_uid, ~age, ~sex, ~key_population,~HTS_INDEX_COM.New.Pos.T,~HTS_INDEX_FAC.New.Pos.T,~TX_NEW.T,~HTS_TST.KP.Pos.T,~TX_NEW.KP.T,
+    "a",   1,         NA,  NA,  "PWID",                    0,          0,                                0,                 100,               94,
+    "b",  2,          NA, NA, "PWID",                         0,           0,                                0,                 100,               95
+  )
   
+  foo<-analyze_linkage(data)
+  testthat::expect_equal(class(foo),"list")
+  testthat::expect_setequal(names(foo),c("test_results","msg"))
+  testthat::expect_equal(NROW(foo$test_results),1)
+  expect_equal(foo$test_results$HTS_TST.KP.Linkage.T,0.94,tolerance=1e-3)
 } )
 
 test_that(" Test linkage > 100% expect message", {
   data<-tribble(
     ~psnu, ~psnu_uid, ~age, ~sex, ~key_population,~HTS_INDEX_COM.New.Pos.T,~HTS_INDEX_FAC.New.Pos.T,~TX_NEW.T,~HTS_TST.KP.Pos.T,~TX_NEW.KP.T,
-    "a",   1,         "25-49",  "F",  NA,                    1,          1,                                10,                 0,               0,
-    "b",  2,          "25-49", "M",  NA,                         0,           0,                                0,                 0,               0
+    "a",   1,         "25-49",  "F",  NA,                    50,          50,                                100,                 0,               0,
+    "b",  2,          "25-49", "M",  NA,                         50,           50,                              101,                 0,               0
   )
   
   foo<-analyze_linkage(data)
   testthat::expect_equal(class(foo),"list")
   testthat::expect_setequal(names(foo),c("test_results","msg"))
   testthat::expect_equal(NROW(foo$test_results),1)
-  expect_equal(foo$test_results$HTS_TST.Linkage.T,5,tolerance=1e-3)
+  expect_equal(foo$test_results$HTS_TST.Linkage.T,1.01,tolerance=1e-3)
+} )
+
+test_that(" Test KP linkage > 100% expect message", {
+  data<-tribble(
+    ~psnu, ~psnu_uid, ~age, ~sex, ~key_population,~HTS_INDEX_COM.New.Pos.T,~HTS_INDEX_FAC.New.Pos.T,~TX_NEW.T,~HTS_TST.KP.Pos.T,~TX_NEW.KP.T,
+    "a",   1,         NA, NA,  "PWID",                    0,          0,                                0,                 100,               100,
+    "b",  2,          NA, NA,  "PWID",                         0,           0,                             0,                 100,               101
+  )
   
+  foo<-analyze_linkage(data)
+  testthat::expect_equal(class(foo),"list")
+  testthat::expect_setequal(names(foo),c("test_results","msg"))
+  testthat::expect_equal(NROW(foo$test_results),1)
+  expect_equal(foo$test_results$HTS_TST.KP.Linkage.T,1.01,tolerance=1e-3)
 } )
 
 test_that(" Test linkage = 98% expect NULL", {
@@ -238,6 +277,17 @@ test_that(" Test linkage = 98% expect NULL", {
     ~psnu, ~psnu_uid, ~age, ~sex, ~key_population,~HTS_INDEX_COM.New.Pos.T,~HTS_INDEX_FAC.New.Pos.T,~TX_NEW.T,~HTS_TST.KP.Pos.T,~TX_NEW.KP.T,
     "a",   1,         "25-49",  "F",  NA,                    20,          20,                                39,                 0,               0,
     "b",  2,          "25-49", "M",  NA,                         0,           0,                                0,                 0,               0
+  )
+  
+  expect_null(analyze_linkage(data))
+  
+} )
+
+test_that(" Test KP linkage = 98% expect NULL", {
+  data<-tribble(
+    ~psnu, ~psnu_uid, ~age, ~sex, ~key_population,~HTS_INDEX_COM.New.Pos.T,~HTS_INDEX_FAC.New.Pos.T,~TX_NEW.T,~HTS_TST.KP.Pos.T,~TX_NEW.KP.T,
+    "a",   1,         NA, NA,  "PWID",                    0,          0,                                0,                 100,               98,
+    "b",  2,          NA, NA,  "PWID",                         0,           0,                             0,                 0,               0
   )
   
   expect_null(analyze_linkage(data))
@@ -253,4 +303,18 @@ test_that(" Test linkage all zeros expect NULL", {
   
   expect_null(analyze_linkage(data))
   
+} )
+
+test_that(" Test linkage with age <1", {
+  data<-tribble(
+    ~psnu, ~psnu_uid, ~age, ~sex, ~key_population,~HTS_INDEX_COM.New.Pos.T,~HTS_INDEX_FAC.New.Pos.T,~TX_NEW.T,~HTS_TST.KP.Pos.T,~TX_NEW.KP.T,
+    "a",   1,         "<01", "M",  NA,                    50,          50,                                101,                 100,               100,
+    "b",  2,          NA, NA,  "PWID",                         0,           0,                             0,                 100,               101
+  )
+  
+  foo<-analyze_linkage(data)
+  testthat::expect_equal(class(foo),"list")
+  testthat::expect_setequal(names(foo),c("test_results","msg"))
+  testthat::expect_equal(NROW(foo$test_results),1)
+  expect_equal(foo$test_results$HTS_TST.KP.Linkage.T,1.01,tolerance=1e-3)
 } )
