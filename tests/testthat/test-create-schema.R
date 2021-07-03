701b763fac0e2cf4a48fa67e01473ae4e2da22a1
Sam Garman <36417685+sam-bao@users.noreply.github.com>
2021-07-03 10:33:18 +0200

    Test compare schema and template (#300)
    * test-create-schema add initial attempt to add compare with schema rda
    
    * finalize test to compare templates with schema
    
    * Update test-create-schema.R
    
    Co-authored-by: Jason Pickering <jason.p.pickering@gmail.com>

--- tests/testthat/test-create-schema.R
+++ tests/testthat/test-create-schema.R
@@ -1,5 +1,9 @@
 context("Create a DataPackSchema")
 
+#Explicity set the locale to avoid any issues related to ordering within objects
+#when comparing them. 
+
+Sys.setlocale(category = "LC_COLLATE", locale = "en_US.UTF-8")
 
 with_mock_api({
   test_that("We can create a datapack schema", {
@@ -47,5 +51,28 @@ with_mock_api({
     expect_type(test_dataset$formula,"character")
     expect_type(test_dataset$FY,"double")
     expect_type(test_dataset$period,"character")
+  })
+  
+  test_that("COP21 template and schema match", {
+    schema <- unPackSchema_datapack(
+      filepath = 
+        system.file("extdata", "COP21_Data_Pack_Template.xlsx", package = "datapackr"),
+      skip = skip_tabs(tool = "Data Pack Template", cop_year = 2021),
+      cop_year = 2021,
+      d2_session = training)
+    
+    expect_identical(schema, cop21_data_pack_schema)
+  })
+
+  test_that("COP20 opu template and schema match", {
+    schema <-
+      datapackr::unPackSchema_datapack(
+        filepath = cop20_opu_datapack_template_path,
+        skip = datapackr::skip_tabs(tool = "OPU Data Pack Template", 
+                                    cop_year = 2020),
+        type = "OPU Data Pack Template",
+        cop_year = 2020,
+        d2_session = training)
+    expect_identical(schema, cop20OPU_data_pack_schema)
   })
 })
