8111187eb948a2209c97cf8a9e7a8a3b2808c816
sam-bao <sgarman@baosystems.com>
2021-07-06 10:52:24 +0200

    Update test-getNumeratorDenominator.R
    modify to use d2_session stub

--- tests/testthat/test-getNumeratorDenominator.R
+++ tests/testthat/test-getNumeratorDenominator.R
@@ -3,9 +3,7 @@ context("test-numerator-denominator")
 
 with_mock_api({
    test_that("We can get a map of numerators/denominators/data elements", {
-    datimutils::loginToDATIM(config_path = test_config("test-config.json"))
-    expect_true(exists("d2_default_session"))
-    n_d<-getNumeratorDenominator()
+    n_d<-getNumeratorDenominator(d2_session = training)
     expect_true(is.data.frame(n_d))
     expect_setequal(names(n_d),c("dataElement","numerator_denominator"))
     expect_setequal(unique(n_d$numerator_denominator),c("Numerator","Denominator"))
@@ -14,9 +12,8 @@ with_mock_api({
 
 with_mock_api({
   test_that("We can filter a map of numerators/denominators/data elements", {
-    datimutils::loginToDATIM(config_path = test_config("test-config.json"))
-    expect_true(exists("d2_default_session"))
-    n_d<-getNumeratorDenominator(dataElements = c("EtEcoXYQLW5"))
+    n_d<-getNumeratorDenominator(dataElements = c("EtEcoXYQLW5"),
+                                 d2_session = training)
     expect_true(is.data.frame(n_d))
     expect_setequal(names(n_d),c("dataElement","numerator_denominator"))
     expect_equal(NROW(n_d),1L)
