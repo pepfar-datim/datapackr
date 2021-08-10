9dcaffed0adce434afe450df406ee727095c883d
sam-bao <sgarman@baosystems.com>
2021-08-10 10:40:06 +0200

    Update generateTestingData.R

--- data-raw/generateTestingData.R
+++ data-raw/generateTestingData.R
@@ -2,7 +2,7 @@
 ## To Do - do a better job with dedupes
 library(magrittr)
 library(datimutils)
-datapackr::loginToDATIM("/users/sam/.secrets/datim.json")
+datapackr::loginToDATIM("/users/sam/.secrets/cop.json")
 base_url <- getOption("baseurl")
 
 country_uid <- datimutils::getOrgUnits("Rwanda", name)
@@ -115,8 +115,7 @@ random_data <- dplyr::inner_join(global_data,
                 value) %>% 
   dplyr::group_by_at(dplyr::vars(-value)) %>% 
   dplyr::summarise(value = sum(value)) %>% 
-  dplyr::ungroup()%>%
-  dplyr::mutate(value = as.character(value)) %>% 
+  dplyr::ungroup()  %>% 
   dplyr::filter(!(dataElement %in% dreams_agyw_data_elements) |
                   (dataElement %in% dreams_agyw_data_elements &
                   orgUnit %in% dreams_snus$id)) %>% 
@@ -125,6 +124,25 @@ random_data <- dplyr::inner_join(global_data,
                      orgUnit %in% dreams_psnus))
 
 
+no_dedupes <- dplyr::group_by_at(random_data,
+                   dplyr::vars(-value,-attributeOptionCombo)) %>% 
+  dplyr::summarise(attributeOptionCombo=paste0(attributeOptionCombo,collapse = ","),
+                   value=sum(value)) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::filter(!(stringr::str_detect(attributeOptionCombo, "00000") |
+                    stringr::str_detect(attributeOptionCombo, "00001")
+                  )
+                )
+
+extra_dedupes <- dplyr::bind_rows(
+  dplyr::mutate(no_dedupes, attributeOptionCombo = "00000", value = 0),
+  dplyr::mutate(no_dedupes, attributeOptionCombo = "00001", value = 0)
+)
+
+random_data <- dplyr::bind_rows(random_data,
+                                extra_dedupes) %>% 
+  dplyr::mutate(value = as.character(value))
+
 dedupes_00000 <- dplyr::filter(random_data,
                                attributeOptionCombo == "00000")
 
@@ -132,6 +150,9 @@ dedupes_00001 <- dplyr::filter(random_data,
                                attributeOptionCombo == "00001")
 
 deletes <- country_data
+
+random_data <- dplyr::filter(random_data,
+                             !(attributeOptionCombo %in% c("00000", "00001")))
 
 prep_json <- function(data){
   
@@ -149,7 +170,15 @@ prep_json <- function(data){
   return(list(pl = pl, raw_file = raw_file))
 }
 
-random_data_json <- prep_json(random_data)
+# create data structure to import data one PSNU at a time
+# cop-test doesn't seem to work well with large imports
+random_data_nested <- dplyr::mutate(random_data, group = orgUnit) %>%  
+  dplyr::group_by(group) %>% 
+  tidyr::nest() %>% 
+  dplyr::mutate(data = purrr::map(data, prep_json)) %>%
+  dplyr::ungroup()
+  
+
 dedupes_00000_json <- prep_json(dedupes_00000)
 dedupes_00001_json <- prep_json(dedupes_00001)
 deletes_json = prep_json(deletes)
@@ -157,7 +186,7 @@ deletes_json = prep_json(deletes)
 url <- paste0("https://cop-test.datim.org/", 
               "api/dataValueSets?importStrategy=DELETE&force=true&preheatCache=true&categoryOptionComboIdScheme=code")
 r <- httr::POST(url, body = deletes_json[["raw_file"]],
-                content_type_json(),
+                httr::content_type_json(),
                 handle = d2_default_session$handle,
                 httr::timeout(600))
 
@@ -166,18 +195,21 @@ httr::content(r)
 
 url <- paste0("https://cop-test.datim.org/", 
               "api/dataValueSets?importStrategy=CREATE_AND_UPDATE&force=true&preheatCache=true&categoryOptionComboIdScheme=code")
-r <- httr::POST(url, body = random_data_json[["raw_file"]],
-                content_type_json(),
-                handle = d2_default_session$handle,
-                httr::timeout(600))
+
+r <- purrr::map(random_data_nested[["data"]], ~ {httr::POST(url, body = .x[["raw_file"]],
+                                                      httr::content_type_json(),
+                                                      handle = d2_default_session$handle)})
+# r <- httr::POST(url, body = random_data_json[["raw_file"]],
+#                 httr::content_type_json(),
+#                 handle = d2_default_session$handle)
 # prin import summary
-httr::content(r)
+purrr::map(r, httr::content)
 
 
 url <- paste0("https://cop-test.datim.org/", 
               "api/dataValueSets?importStrategy=CREATE_AND_UPDATE&force=true&preheatCache=true&categoryOptionComboIdScheme=code")
 r <- httr::POST(url, body = dedupes_00000_json[["raw_file"]],
-                content_type_json(),
+                httr::content_type_json(),
                 handle = d2_default_session$handle,
                 httr::timeout(600))
 
@@ -187,6 +219,6 @@ httr::content(r)
 url <- paste0("https://cop-test.datim.org/", 
               "api/dataValueSets?importStrategy=CREATE_AND_UPDATE&force=true&preheatCache=true&categoryOptionComboIdScheme=code")
 r <- httr::POST(url, body = dedupes_00001_json[["raw_file"]],
-                content_type_json(),
+                httr::content_type_json(),
                 handle = d2_default_session$handle,
                 httr::timeout(600))
