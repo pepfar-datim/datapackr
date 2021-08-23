4eb5d224f03979ce8d383aa8b7c8c8f4dd5840e4
Jason P. Pickering <jason.p.pickering@gmail.com>
2021-08-23 16:36:06 +0200

    Add more tests for package setup

--- tests/testthat/test-package-setup.R
+++ tests/testthat/test-package-setup.R
@@ -46,3 +46,76 @@ test_that("We can pick template file", {
   
 }
 )
+
+
+test_that("We can check datapack paramaters", {
+  test_params<-check_params()
+  expect_type(test_params,"list")
+  #Is the intent that we should return a zero-length list with no arguments?
+  expect_equal(length(test_params),0L)
+  
+  #Test for a valid country UID
+  test_params<-check_params(country_uids = "JTypsdEUNPw")
+  expect_type(test_params,"list")
+  expect_equal(test_params$country_uids,"JTypsdEUNPw")
+  
+  #Test for an invalid country UID
+  test_params<-check_params(country_uids = "foo")
+  expect_type(test_params,"list")
+  expect_equal(length(check_params(country_uids = "foo")$country_uids),0L)
+  
+  #Throw an error if the argument is NULL
+  expect_error(check_params(country_uids = NULL))
+  
+  #Test for a valid COP year
+  test_params<-check_params(cop_year = 2020)
+  expect_type(test_params,"list")
+  expect_equal(test_params$cop_year,2020)
+  
+  #Error on a bogus COP year
+  expect_error(check_params(cop_year = 1999))
+
+  #Can check a valid tool
+  test_params<-check_params(tool = "Data Pack")
+  expect_type(test_params,"list")
+  expect_equal(test_params$tool,"Data Pack")
+
+  #Can error on a bogus tool
+  expect_error(check_params(tool = "Foo Pack"))
+  
+  #Can check a valid season
+  test_params<-check_params(season = "COP")
+  expect_type(test_params,"list")
+  expect_equal(test_params$season,"COP")
+  
+  #Can error on a bogus tool
+  expect_error(check_params(season = "The Long Winter"))
+  
+  #Can check a valid schema
+  test_params <-
+    check_params(
+      schema = datapackr::cop20_data_pack_schema,
+      cop_year = 2020,
+      season = "COP"
+    )
+  expect_type(test_params, "list")
+  expect_setequal(names(test_params), c("schema", "cop_year", "season"))
+  
+  # #Return a message when using an invalid combination of schema/cop_year/season
+  # 
+  # expect_message(
+  #   test_params <-
+  #     check_params(
+  #       schema = datapackr::cop20_data_pack_schema,
+  #       cop_year = 2021,
+  #       season = "COP"
+  #     )
+  # )
+  
+  }
+  
+
+  
+
+)
+
