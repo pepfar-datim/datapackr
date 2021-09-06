38874770a8b0c7143f8a758574e9dc69b09f51ea
sam-bao <sgarman@baosystems.com>
2021-09-06 12:01:33 +0200

    Update update_cached_PSNUs.R
    Add Comment

--- data-raw/update_cached_PSNUs.R
+++ data-raw/update_cached_PSNUs.R
@@ -6,6 +6,10 @@ secrets <- "~/.secrets/datim.json"
 
 datapackr::loginToDATIM(secrets)
 
+# Burkina Faso and Sierra Leone changed PSNU levels from Country to SNU1
+# between COP20 and COP21.
+# we preserver their country level PSNUs for the moment to allow for COP20 OPU
+# Processing
 valid_PSNUs <- getPSNUs(additional_fields = "lastUpdated") %>%
   dplyr::bind_rows(
     dplyr::filter(datapackr::valid_PSNUs,
