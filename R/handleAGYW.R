950b93fb24eeed5b7a876673d335b6d0b901a4db
Fausto Lopez <flopez@baosystems.com>
2021-11-12 11:59:44 -0500

    refactor unpackSheet
    modularized functionality into R files, documented and added to namespace. Altered comment structure.

---
+++ R/handleAGYW.R
@@ -0,0 +1,66 @@
+#' @export
+#' @importFrom magrittr %>% %<>%
+#' @title Handle AGYW data from a Data Pack
+#'
+#' @description Used to process AGYW sheets in a Data Pack
+#' alerts on missing or invalid SNUs.
+#'
+#' @param d Datapackr object.
+#' @param sheet Sheet to check
+#'
+#' @return d
+#'
+
+handleAGYW <- function(d, sheet) {
+  
+  DataPack_DSNUs <- d$data$extract %>%
+    dplyr::select(PSNU, psnu_uid = psnuid) %>%
+    dplyr::distinct() %>%
+    dplyr::mutate(DataPack = 1)
+  
+  DATIM_DSNUs <- datapackr::valid_PSNUs %>%
+    dplyr::filter(country_uid %in% d$info$country_uids) %>%
+    add_dp_psnu(.) %>%
+    dplyr::arrange(dp_psnu) %>%
+    dplyr::filter(!is.na(DREAMS)) %>%
+    dplyr::select(PSNU = dp_psnu, psnu_uid, snu1) %>%
+    dplyr::mutate(DATIM = 1)
+  
+  DSNU_comparison <- DataPack_DSNUs %>%
+    dplyr::full_join(DATIM_DSNUs, by = "psnu_uid")
+  
+  d$tests$DSNU_comparison <- DSNU_comparison
+  attr(d$tests$DSNU_comparison, "test_name") <- "DSNU List Comparison"
+  
+  if (any(is.na(DSNU_comparison$DataPack))) {
+    missing_DSNUs <- DSNU_comparison %>%
+      dplyr::filter(is.na(DataPack))
+    
+    warning_msg <- paste0(
+      "WARNING! In tab ",
+      sheet,
+      ": MISSING DREAMS SNUs found! ->  \n\t* ",
+      paste(missing_DSNUs$PSNU.y, collapse = "\n\t* "),
+      "\n")
+    
+    d$info$messages <- appendMessage(d$info$messages, warning_msg, "WARNING")
+    d$info$missing_DSNUs <- TRUE
+  }
+  
+  if (any(is.na(DSNU_comparison$DATIM))) {
+    invalid_DSNUs <- DSNU_comparison %>%
+      dplyr::filter(is.na(DATIM))
+    
+    warning_msg <- paste0(
+      "WARNING! In tab ",
+      sheet,
+      ": INVALID DREAMS SNUs found! ->  \n\t* ",
+      paste(invalid_DSNUs$PSNU.x, collapse = "\n\t* "),
+      "\n")
+    
+    d$info$messages <- appendMessage(d$info$messages, warning_msg, "WARNING")
+  }
+  
+  return(d)
+  
+}
---
+++ R/handleTX.R
@@ -0,0 +1,37 @@
+#' @export
+#' @importFrom magrittr %>% %<>%
+#' @title Handle TX data from a Data Pack
+#'
+#' @description Used to process TX sheets in a Data Pack (legacy).
+#'
+#' @param d Datapackr object.
+#' @param sheet Sheet to check
+#'
+#' @return d
+#'
+
+handleTX <- function(d, sheet) {
+  d$tests$tx_new_invalid_lt1_sources <- d$data$extract %>%
+    dplyr::select(PSNU, Age, Sex, TX_NEW.N.Age_Sex_HIVStatus.T,
+                  TX_NEW.N.IndexRate, TX_NEW.N.TBRate, TX_NEW.N.PMTCTRate,
+                  TX_NEW.N.PostANC1Rate, TX_NEW.N.EIDRate, TX_NEW.N.VMMCRateNew,
+                  TX_NEW.N.prevDiagnosedRate) %>%
+    dplyr::filter(Age == "<01",
+                  TX_NEW.N.Age_Sex_HIVStatus.T > 0) %>%
+    dplyr::filter_at(dplyr::vars(-PSNU, -Age, -Sex, -TX_NEW.N.EIDRate,
+                                 -TX_NEW.N.Age_Sex_HIVStatus.T),
+                     dplyr::any_vars(. > 0))
+  attr(d$tests$tx_new_invalid_lt1_sources, "test_name") <- "Invalid TX <01 data source"
+  
+  if (NROW(d$tests$tx_new_invalid_lt1_sources) > 0) {
+    warning_msg <-
+      paste0(
+        "WARNING! In tab TX",
+        ": TX_NEW for <01 year olds being targeted through method other than EID.",
+        " MER Guidance recommends all testing for <01 year olds be performed through EID rather than HTS",
+        "\n")
+    
+    d$info$messages <- appendMessage(d$info$messages, warning_msg, "WARNING")
+  }
+  return(d)
+}
