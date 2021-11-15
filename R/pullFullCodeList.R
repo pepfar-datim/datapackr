9ca0e4b0881970af6cab99110198027daf6230de
Scott Jackson <scott.j.jackson@gmail.com>
2021-11-15 10:41:45 -0800

    pullFullCodeList: Update to allow custom dataset specification

--- R/pullFullCodeList.R
+++ R/pullFullCodeList.R
@@ -9,6 +9,9 @@
 #' @param datastream At least one
 #' of "mer_targets", "mer_results", "subnat_targets", "subnat_results" or "impatt".
 #' If left will return a code list for the specified COP Year of all streams
+#' @param datasets Character vector of dataSet IDs to pull code lists for.
+#' @param expanded If TRUE, will add dataset, period, period_dataset, and
+#' targets_results, related categoryOption metadata,  as additional columns
 #' @param d2_session R6 datimutils object which handles authentication with DATIM
 #' @return Combined code list as dataframe.
 #'
@@ -16,40 +19,47 @@ pullFullCodeList <- function(FY = getCurrentCOPYear() + 1,
                              datastream = c("mer_targets", "mer_results",
                                             "subnat_targets", "subnat_results",
                                             "impatt"),
+                             datasets,
+                             expanded = FALSE,
                              d2_session = dynGet("d2_default_session",
-                                                 inherits = TRUE)
-                             ) {
+                                                 inherits = TRUE)) {
 
-  datasets_list <- character(0)
+  datasets <- datasets %missing% NULL
+  datasets_provided <- !is.null(datasets)
 
-  if ("mer_targets" %in% datastream) {
-    datasets_list <- c(datasets_list,
-                       datapackr::getDatasetUids(FY, type = "mer_targets"))
+  if (!datasets_provided) {
+    datasets <- character(0)
+  
+    if ("mer_targets" %in% datastream) {
+      datasets <- c(datasets,
+                    datapackr::getDatasetUids(FY, type = "mer_targets"))
+    }
+    if ("mer_results" %in% datastream) {
+      datasets <- c(datasets,
+                    datapackr::getDatasetUids(FY, type = "mer_results"))
+    }
+    if ("subnat_targets" %in% datastream) {
+      datasets <- c(datasets,
+                    datapackr::getDatasetUids(FY, type = "subnat_targets"))
+    }
+    if ("subnat_results" %in% datastream) {
+      datasets <- c(datasets,
+                    datapackr::getDatasetUids(FY, type = "subnat_results"))
+    }
+    if ("impatt" %in% datastream) {
+      datasets <- c(datasets,
+                    datapackr::getDatasetUids(FY, type = "impatt"))
+    }
+  
   }
-  if ("mer_results" %in% datastream) {
-    datasets_list <- c(datasets_list,
-                       datapackr::getDatasetUids(FY, type = "mer_results"))
-  }
-  if ("subnat_targets" %in% datastream) {
-    datasets_list <- c(datasets_list,
-                       datapackr::getDatasetUids(FY, type = "subnat_targets"))
-  }
-  if ("subnat_results" %in% datastream) {
-    datasets_list <- c(datasets_list,
-                       datapackr::getDatasetUids(FY, type = "subnat_results"))
-  }
-  if ("impatt" %in% datastream) {
-    datasets_list <- c(datasets_list,
-                       datapackr::getDatasetUids(FY, type = "impatt"))
-  }
-
-  datasets_list <- unique(datasets_list)
+  
+  datasets <- unique(datasets)
 
   ds <- data.frame()
 
   fullCodeList <-
     lapply(
-      datasets_list,
+      datasets,
       function(x) {
         cl <- pullDATIMCodeList(x, d2_session = d2_session)
         ds <- rbind(ds, cl)
