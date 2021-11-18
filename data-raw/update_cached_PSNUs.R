aeeadb2f9e819f17826131febdfca2783dc9afee
Jordan <Jordan>
2021-11-18 15:32:23 -0500

    [DP-361] Cleaned up code and made an abundant amount of comments. Note no large code refactoring occured, as I did not want to introduce another schema such as data.table. The returns would be diminishing.
    I do think we should explore parallilization and python for the intensive time/resource consuming sections. I would love to take that on when time permits

--- data-raw/update_cached_PSNUs.R
+++ data-raw/update_cached_PSNUs.R
@@ -6,10 +6,17 @@ secrets <- "~/.secrets/datim.json"
 
 datapackr::loginToDATIM(secrets)
 
+
+# NOTE: Full documentation can be found in data.R
+# The current list can be viewed by running View(valid_PSNUs)
+
+# NOTE:
 # Burkina Faso and Sierra Leone changed PSNU levels from Country to SNU1
 # between COP20 and COP21.
-# we preserver their country level PSNUs for the moment to allow for COP20 OPU
+# We preserve their country level PSNUs for the moment to allow for COP20 OPU
 # Processing
+
+# Fetch PSNU values, filter PSNUs as needed, Stack with default valid_PSNUs list
 valid_PSNUs <- getPSNUs(additional_fields = "lastUpdated") %>%
   dplyr::bind_rows(
     dplyr::filter(datapackr::valid_PSNUs,
@@ -17,10 +24,12 @@ valid_PSNUs <- getPSNUs(additional_fields = "lastUpdated") %>%
                   )
     )
 
+# Comparing default valid_PSNUs list to newly modified list
 compare_diffs <- datapackr::valid_PSNUs %>%
   dplyr::full_join(valid_PSNUs, by = "psnu_uid") %>%
   dplyr::filter(is.na(psnu.x) | is.na(psnu.y))
 
-save(valid_PSNUs, file = "./data/valid_PSNUs.rda", compress = "xz")
-
-
+# Overwriting default list with newly created list
+save(valid_PSNUs,
+     file = "./data/valid_PSNUs.rda",
+     compress = "xz") 
