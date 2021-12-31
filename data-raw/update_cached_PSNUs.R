4219532186e08988ae690d3aabe869bcab4fd950
Scott Jackson <scott.j.jackson@gmail.com>
2021-12-30 18:09:11 -0800

    Update update_cached_PSNUs.R
    Cannot manually maintain Sierra Leone and Burkina Faso country-level PSNUs because these will be invalid in DATIM, and no archive currently kept in valid_PSNUs. Any outstanding COP20 OPUs with these at country-level will need to be reissued at the correct level.

--- data-raw/update_cached_PSNUs.R
+++ data-raw/update_cached_PSNUs.R
@@ -6,33 +6,18 @@ secrets <- "~/.secrets/datim.json"
 
 datimutils::loginToDATIM(secrets)
 
-
 # NOTE: Full documentation can be found in data.R
 # The current list can be viewed by running View(valid_PSNUs)
 
-# NOTE:
-# Burkina Faso and Sierra Leone changed PSNU levels from Country to SNU1
-# between COP20 and COP21.
-# We preserve their country level PSNUs for the moment to allow for COP20 OPU
-# Processing
-
-# Fetch PSNU values, filter PSNUs as needed, Stack with default valid_PSNUs list
-valid_PSNUs <- getPSNUs(additional_fields = "lastUpdated") %>%
-  dplyr::bind_rows(
-    dplyr::filter(datapackr::valid_PSNUs,
-                  psnu %in% c("Sierra Leone", "Burkina Faso")
-                  )
-    )
-
+# Fetch PSNU values
+valid_PSNUs <- getPSNUs(additional_fields = "lastUpdated")
 
 # Comparing default valid_PSNUs list to newly modified list
 compare_diffs <- datapackr::valid_PSNUs %>%
   dplyr::full_join(valid_PSNUs, by = "psnu_uid") %>%
   dplyr::filter(is.na(psnu.x) | is.na(psnu.y))
 
-
 # Overwriting default list with newly created list
 save(valid_PSNUs,
      file = "./data/valid_PSNUs.rda",
-     compress = "xz") 
-
+     compress = "xz")
