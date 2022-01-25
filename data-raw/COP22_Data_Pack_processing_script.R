c7f6a401b60f9987d557d615a3eb8dee1cd1593d
Scott Jackson <scott.j.jackson@gmail.com>
2022-01-24 18:58:52 -0800

    Update COP22_Data_Pack_processing_script.R

--- data-raw/COP22_Data_Pack_processing_script.R
+++ data-raw/COP22_Data_Pack_processing_script.R
@@ -3,14 +3,13 @@ library(magrittr)
 
 datimutils::loginToDATIM("~/.secrets/datim.json")
 
-#model_data_path <- file.choose()
-snuxim_model_data_path <- file.choose()
-output_folder <- "/Users/scott/Google Drive/PEPFAR/COP Targets/COP 22/3) Testing & Deployment/Beta Packs"
-
 # Unpack Submitted Data Pack ####
 d <- unPackTool(cop_year = 2022)
 
+#model_data_path <- file.choose()
 # d <- checkAnalytics(d,
 #                    model_data_path)
 
+snuxim_model_data_path <- file.choose()
+output_folder <- "/Users/scott/Google Drive/PEPFAR/COP Targets/COP 22/3) Testing & Deployment/Beta Packs"
 d <- writePSNUxIM(d, snuxim_model_data_path, output_folder)
