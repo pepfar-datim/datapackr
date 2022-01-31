e872b61dc278a2170706ed7db6740af1dde72f10
Sam Garman <36417685+sam-bao@users.noreply.github.com>
2022-01-31 11:45:14 +0100

    DP-510 (#372)
    * Update datapackVsDatim.R
    
    Change compareData_OpuDatapackVsDatim to NOT compare AGYW data which are not part of OPU data packs.
    
    * linting
    
    * Update datapackVsDatim.R
    
    Change test to ensure attributeOptionCombos are not uids
    
    * Typo
    
    Resolves issue in devtools::check
    
     compareData_DatapackVsDatim: warning in getCOPDataFromDATIM(country_uid
        = d$info$country_uids, cop_year = d$info$cop_year, d2_session =
        d2_session): partial argument match of 'country_uid' to
        'country_uids'
      compareData_DatapackVsDatim: warning in getCOPDataFromDATIM(country_uid
        = d$info$country_uids, cop_year = d$info$cop_year - 1, streams =
        c("subnat_targets"), d2_session = d2_session): partial argument match
        of 'country_uid' to 'country_uids'

--- R/datapackVsDatim.R
+++ R/datapackVsDatim.R
@@ -127,10 +127,10 @@ compareData_DatapackVsDatim <-
 # Get data from DATIM using data value sets
 
     datim_data <- dplyr::bind_rows(
-      getCOPDataFromDATIM(country_uid = d$info$country_uids,
+      getCOPDataFromDATIM(country_uids = d$info$country_uids,
                           cop_year = d$info$cop_year,
                           d2_session = d2_session),
-      getCOPDataFromDATIM(country_uid = d$info$country_uids,
+      getCOPDataFromDATIM(country_uids = d$info$country_uids,
                           cop_year = d$info$cop_year - 1,
                           streams = c("subnat_targets"),
                           d2_session = d2_session)) %>%
@@ -213,11 +213,16 @@ compareData_DatapackVsDatim <-
 compareData_OpuDatapackVsDatim <-
   function(d, d2_session = dynGet("d2_default_session",
                                   inherits = TRUE)) {
+# current assumption is that d$datim$OPU has mech codes but this is planned to change
+# this assertion alerts us if the change is made and we forget to make necessary changes here:
+
+    assertthat::assert_that(
+      !any(datapackr::is_uidish(d$datim$OPU$attributeOptionCombo))
+    )
 
     if (!(d$info$cop_year %in% c(2020, 2021))) {
       stop("Attempting to use compareData_OpuDatapackVsDatim for unsupported COP year")
     }
-
     datapack_data <- d$datim$OPU
 
 # recoding to account for code change in DATIM for the default COC
@@ -282,7 +287,10 @@ compareData_OpuDatapackVsDatim <-
                     orgUnit,
                     categoryOptionCombo,
                     attributeOptionCombo,
-                    datim_value)
+                    datim_value) %>%
+# AGYW data don't have mechs and aren't in OPU data packs
+# exclude them from comparison or any other data without mech
+      dplyr::filter(attributeOptionCombo != "default")
 
 # extract dedupes from import file to handle seperately
     dedupes <- dplyr::filter(datapack_data,
