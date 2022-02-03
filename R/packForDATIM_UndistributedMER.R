cd6b9cc332ccc7079180cd21bbf900607c74fde2
Fausto Lopez <flopez@baosystems.com>
2022-02-03 12:32:11 -0500

    Restructured and cut down functions
    -added new functions to utilities that handle some of the data munging int he packForDATIM tree and standard selections for exports.
    -cut down OPU, undistributedMER and MER in tree down to under 50 lines each
    -removed depracated code from exportSubnatToDatim

--- R/packForDATIM_UndistributedMER.R
+++ R/packForDATIM_UndistributedMER.R
@@ -3,39 +3,14 @@
 #'
 #' @description Packs undistributed MER data from Data Pack for use in analytics (NOT IMPORT).
 #'
-#' @param data Dataframe to pack for DATIM. Any mechanism attribution will be ignored.
-#' @inheritParams datapackr_params
+#' @param d Datapackr object
+#' @param datim_map datim map object for the cop year
+#' @return Modified d object with a DATIM compatible data frame for analytics id d$datim$UndistributedMER
 #'
-#' @return MER data formatted as standard DATIM import file, with all data
-#' allocated against the default mechanism UID.
-#'
-packForDATIM_UndistributedMER <- function(data,
-                                          cop_year) {
+packForDATIM_UndistributedMER <- function(d, datim_map) {
 
-  # Check params
-  params <- check_params(cop_year = cop_year)
-
-  ps <- c("cop_year")
-
-  for (p in ps) {
-    assign(p, purrr::pluck(params, p))
-  }
-
-  rm(params, ps, p)
-
-  # Check dataset ####
-  if (!all(names(data) == c("PSNU", "psnuid", "sheet_name", "indicator_code",
-                            "Age", "Sex", "KeyPop", "value"))) {
-    stop(paste0("Provided dataset should have the following columns: PSNU, ",
-          "psnuid, sheet_name, indicator_code, Age, Sex, KeyPop, value"))
-  }
-
-  # Add dataElement & categoryOptionCombo ####
-  datim_map <- getMapDataPack_DATIM_DEs_COCs(cop_year = cop_year)
-  UndistributedMER <- data %>%
-    dplyr::mutate(support_type = "DSD",
-                  mech_code = default_catOptCombo()) %>%
-    # Special handling for AGYW_PREV
+  # Add dataElement & categoryOptionCombo ----
+  d$datim$UndistributedMER <- d$data$MER %>%
     dplyr::mutate(
       support_type = dplyr::case_when(
         stringr::str_detect(indicator_code, "AGYW_PREV") ~ "No Support Type",
@@ -51,29 +26,15 @@ packForDATIM_UndistributedMER <- function(data,
       ),
       by = c("indicator_code", "Age", "Sex", "KeyPop", "support_type")
     ) %>%
-    tidyr::drop_na(dataelementuid, categoryoptioncombouid) %>%
+    map_datim_join(datim_map) %>%
 
-    # Add period ####
-    dplyr::mutate(
-      period = paste0(FY - 1, "Oct")) %>%
+  # Add PSNU uid ----
+  dplyr::mutate(
+    psnuid = addPsnuid(PSNU)
+  ) %>%
 
-    # Add PSNU uid ####
-    dplyr::mutate(
-      psnuid = stringr::str_extract(PSNU, "(?<=(\\(|\\[))([A-Za-z][A-Za-z0-9]{10})(?=(\\)|\\])$)")
-    ) %>%
-
-    # Select and rename based on DATIM protocol ####
-    dplyr::select(
-      dataElement = dataelementuid,
-      period,
-      orgUnit = psnuid,
-      categoryOptionCombo = categoryoptioncombouid,
-      attributeOptionCombo = mech_code,
-      value) %>%
-  # Aggregate across 50+ age bands ####
-    dplyr::group_by(dplyr::across(c(-value))) %>%
-    dplyr::summarise(value = sum(value)) %>%
-    dplyr::ungroup()
+  # Select and rename based on DATIM protocol ----
+  set_datim_protocol()
 
   return(UndistributedMER)
 
