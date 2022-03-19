1c14bd69dc6253f420d1b4825d75962322775210
Jason Pickering <jason.p.pickering@gmail.com>
2022-03-19 10:41:51 +0100

    Linting

--- R/utilities.R
+++ R/utilities.R
@@ -766,31 +766,29 @@ getMaxCores <- function() {
 #' @export
 #'
 formatSetStrings <- function(vec) {
-  
+
   if (!is.vector(vec)) return(NA_character_)
-  
+
   if (is.list(vec)) {
     warning("Can only accept simple vectors")
     return(NA_character_)
     }
-  
+
   vec <- vec[!is.na(vec)]
-  
-  if( length(vec) == 0) return(NA_character_)
-  
-  if (!all(is.numeric(vec))) 
-  {
+
+  if (length(vec) == 0) return(NA_character_)
+
+  if (!all(is.numeric(vec))) {
      warning("Ensure that all values are numeric")
      return(NA_character_)
   }
-    
 
   vec <- sort(vec)
   groups <- cumsum(c(0, diff(vec) > 1))
   sets <- split(vec, groups)
   set_strings <- sapply(sets, function(x) {
-    ifelse(min(x) == max(x),x,paste0(min(x),":",max(x))) })
-  paste0(set_strings,collapse=",")
+    ifelse(min(x) == max(x), x, paste0(min(x), ":", max(x))) })
+  paste0(set_strings, collapse = ",")
 }
 
 #' @export
