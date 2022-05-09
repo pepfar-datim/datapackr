89cd0ae54ffd3c20866387521eac8287532e1a0a
Christopher Nemarich <55459914+cnemarich@users.noreply.github.com>
2022-05-09 03:11:21 -0400

    DP-682 (#455)
    * Deletes `R/getValidCategoryOptions.R`
    * Deletes `data/valid_category_options.rda`
    * Deletes `data-raw/update_cached_valid_COs.R`
    * Deletes documentation for dataset in `R/data.R`
    * Regenerates documentation

--- data-raw/update_cached_valid_COs.R
+++
@@ -1,9 +0,0 @@
-## Save Valid COs ####
-
-# Point to DATIM login secrets ####
-secrets <- Sys.getenv("SECRETS_FOLDER") %>% paste0(., "test-mer2.json")
-datimutils::loginToDATIM(secrets)
-
-valid_category_options <- getValidCategoryOptions(cop_year = getCurrentCOPYear())
-
-save(valid_category_options, file = "./data/valid_category_options.rda", compress = "xz")
