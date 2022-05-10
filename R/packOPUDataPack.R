c0de220d478f97d3ee1496b518851d82ba3af38d
Jason P. Pickering <jason.p.pickering@gmail.com>
2022-05-10 08:44:50 +0200

    Minor adjustment to packOPUDataPack

--- R/packOPUDataPack.R
+++ R/packOPUDataPack.R
@@ -66,8 +66,7 @@ packOPUDataPack <- function(snuxim_model_data = NULL,
     d$data$UndistributedMER <- d$data$snuxim_model_data %>%
       dplyr::mutate(attributeOptionCombo = default_catOptCombo()) %>%
       dplyr::group_by(dplyr::across(c(-value))) %>%
-      dplyr::summarise(value = sum(value), na.rm = TRUE) %>%
-      dplyr::ungroup() %>%
+      dplyr::summarise(value = sum(value, na.rm = TRUE), .groups = "drop") %>%
       dplyr::filter(value != 0)
   }
 
