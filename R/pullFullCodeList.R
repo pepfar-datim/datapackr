af2833020f687100077728e248673016ce24e13d
Jason P. Pickering <jason.p.pickering@gmail.com>
2022-05-17 10:51:13 +0200

    Linting

--- R/pullFullCodeList.R
+++ R/pullFullCodeList.R
@@ -35,7 +35,7 @@ pullFullCodeList <- function(FY = getCurrentCOPYear() + 1,
   ds <- data.frame()
 
   fullCodeList <-
-    purrr::map_dfr(datasets,pullDATIMCodeList) %>% 
+    purrr::map_dfr(datasets,pullDATIMCodeList) %>%
     dplyr::select(dataelement, dataelementuid, categoryoptioncombo, categoryoptioncombouid) %>%
     dplyr::mutate(FY = FY) %>%
     dplyr::distinct() %>%
