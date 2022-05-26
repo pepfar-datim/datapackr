082342fb0127753e0841894cbc62d9ce4fafb40f
Jason P. Pickering <jason.p.pickering@gmail.com>
2022-05-26 13:07:13 +0200

    Linting

--- tests/testthat/test-getMechanismView.R
+++ tests/testthat/test-getMechanismView.R
@@ -6,9 +6,9 @@ context("Get a mechanism view when logged into DATIM")
     test_that("We can get a mechanism list when logged in", {
       test_mech_list <- datimutils::getSqlView(sql_view_uid = "fgUtV6e9YIX",
                                                d2_session = training)
-      
+
       expect_type(test_mech_list, "list")
-      
+
       mechs_names <- c("mechanism",
                      "code",
                      "uid",
@@ -18,9 +18,9 @@ context("Get a mechanism view when logged into DATIM")
                      "ou",
                      "startdate",
                      "enddate")
-      
+
       expect_true(setequal(mechs_names, names(test_mech_list)))
-      
+
       #Expect error on a faulty dataset UID
       expect_error(datimutils::getSqlView("foo"))
     })
