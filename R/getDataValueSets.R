cf89214a934c44d7fc35dbdab814a462271f20e1
Christopher Nemarich <55459914+cnemarich@users.noreply.github.com>
2022-06-01 17:00:11 -0400

    DP-607: Refactor `getDataValueSets`
    Refactors `getDataValueSets` to utilize `datimutils` instead of `datapackr` API functions. Removes `api_version` as a parameter from `getDataValueSets`. Updates test. Moves mock file to correct folder.

--- R/getDataValueSets.R
+++ R/getDataValueSets.R
@@ -3,40 +3,23 @@
 #'
 #' @description Used to read DHIS 2 data using the data value set endpoint
 #' @param keys character vector - data value set parameter keys (e.g. "dataSet", "period")
-#' @param values character vector - values marching the key from keys (e.g. "Abcde123456", "2019Q1"
-#' @param api_version - api version to use when calling DHIS2
-#' @param d2_session R6 datimutils object which handles authentication with DATIM
+#' @param values character vector - values marching the key from keys (e.g. "Abcde123456", "2019Q1")
+#' @inheritParams datapackr_params
 #' @return  tibble with the data requested
 #'
 getDataValueSets <- function(keys, values,
-                            api_version = datapackr::api_version(),
-                            d2_session = dynGet("d2_default_session",
-                                                inherits = TRUE)) {
+                             d2_session = dynGet("d2_default_session",
+                                                 inherits = TRUE)) {
 
   # concatenate and format the keys and values provided for the api call
   parameters <- stringr::str_c(keys,
                                values,
                                sep = "=",
                                collapse = "&")
-  api_call <- glue::glue("{d2_session$base_url}api/{api_version}/dataValueSets.csv?{parameters}")
-  # note the deleted column comes back empty (missing a comma in fact) which results in a warning
-  # we can disregard that warning
-  #  row col   expected     actual         file
-  #  1  -- 11 columns 10 columns literal data
 
-  data <- httr::GET(api_call, handle = d2_session$handle) %>%
-    httr::content(., "text") %>%
-{ # nolint
-      suppressWarnings(readr::read_csv(
-        .,
-        col_names = TRUE,
-        col_types = readr::cols(
-          .default = "c",
-          value = "d",
-          lastupdated = "T"
-        )
-      ))
-    } %>%
+  data <- datimutils::getMetadata("dataValueSets",
+                                  parameters,
+                                  d2_session = d2_session) %>%
     dplyr::rename(data_element = dataelement,
                   org_unit = orgunit,
                   category_option_combo = categoryoptioncombo,
@@ -44,5 +27,5 @@ getDataValueSets <- function(keys, values,
                   stored_by = storedby,
                   last_updated = lastupdated)
 
-  return(data)
+  data
 }
--- tests/testthat/test-GetDataValueSet.R
+++ tests/testthat/test-GetDataValueSet.R
@@ -12,7 +12,6 @@ test_that("GetDataValueSets", {
   with_mock_api({
   expect_silent(data <- getDataValueSets(c("dataElementGroup", "orgUnit", "startDate", "endDate", "children", "limit"),
                                            c("qfxEYY9xAl6", "O6uvpzGd5pu", "2013-01-01", "2090-01-01", "true", "25"),
-                                           api_version = 36,
                                            d2_session = play2361))
     testthat::expect_named(data, c("data_element",
                                    "period",
