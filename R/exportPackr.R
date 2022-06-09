5c8f33acdb0ae849a9c4d6883e410a7764cad860
Christopher Nemarich <55459914+cnemarich@users.noreply.github.com>
2022-06-09 12:50:39 -0400

    DP-693: Remove `readr` as required package
    Refactors `exportPackr` so that `readr` is no longer a hard requirement for `datapackr`.

--- R/exportPackr.R
+++ R/exportPackr.R
@@ -9,7 +9,6 @@
 #' @param tool File prefix to be applied in output filename, as follows:
 #'   \describe{
 #'     \item{\code{Data Pack}}{Openxlsx Workbook object containing Data Pack.}
-#'     \item{\code{FAST Export}}{Data frame containing FAST export data.}
 #'     \item{\code{SUBNAT IMPATT}}{Data frame containing SUBNAT/IMPATT data.}
 #'     \item{\code{Results Archive}}{List object containing results archive.}
 #'     \item{\code{OPU Data Pack}}{Openxlsx Workbook object containing OPU Data Pack.}
@@ -43,14 +42,14 @@ exportPackr <- function(data, output_folder, tool, datapack_name) {
     openxlsx::saveWorkbook(wb = data, file = output_file_name, overwrite = TRUE)
   }
 
-  else if (tool %in% c("FAST Export", "SUBNAT IMPATT", "Spectrum Example", "DATIM Export File")) {
+  else if (tool %in% c("SUBNAT IMPATT", "Spectrum Example", "DATIM Export File")) {
     if (!any(stringr::str_detect(class(data), "data\\.frame|tbl_df"))) {
       stop("Output type and data do not match!")
     }
 
     output_file_name <- packName(output_folder, tool, datapack_name, extension = ".csv")
 
-    readr::write_csv(data, output_file_name)
+    write.csv(data, output_file_name, row.names = FALSE)
   }
 
   else if (tool %in% c("Results Archive")) {
