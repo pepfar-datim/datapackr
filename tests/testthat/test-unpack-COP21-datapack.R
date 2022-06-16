2511e76b04ce0b1ed536fbd476c24b71e9f765dc
Scott Jackson <scott.j.jackson@gmail.com>
2022-06-16 11:55:02 -0700

    Move UndistributedMER from d$data to d$datim
    Also move prioritizations from d$datim$fy22_prioritizations to d$datim$prioritizations

--- tests/testthat/test-unpack-COP21-datapack.R
+++ tests/testthat/test-unpack-COP21-datapack.R
@@ -72,21 +72,21 @@ with_mock_api({
 
     # Package the undistributed data for DATIM
     d <- packForDATIM(d, type = "Undistributed MER")
-    expect_true(!is.null(d$data$UndistributedMER))
-    expect_true(NROW(d$data$UndistributedMER) > 0)
+    expect_true(!is.null(d$datim$UndistributedMER))
+    expect_true(NROW(d$datim$UndistributedMER) > 0)
     expect_true(all(unlist(
-      lapply(d$data$UndistributedMER$dataElement, is_uidish)
+      lapply(d$datim$UndistributedMER$dataElement, is_uidish)
     )))
     expect_true(all(unlist(
-      lapply(d$data$UndistributedMER$categoryOptionCombo, is_uidish)
+      lapply(d$datim$UndistributedMER$categoryOptionCombo, is_uidish)
     )))
     expect_true(all(unlist(
-      lapply(d$data$UndistributedMER$period, function(x) {
+      lapply(d$datim$UndistributedMER$period, function(x) {
         grepl("^\\d{4}Oct$", x)
       })
     )))
 
-    expect_type(d$data$UndistributedMER$attributeOptionCombo, "character")
-    expect_type(d$data$UndistributedMER$value, "double")
+    expect_type(d$datim$UndistributedMER$attributeOptionCombo, "character")
+    expect_type(d$datim$UndistributedMER$value, "double")
   })
 })
