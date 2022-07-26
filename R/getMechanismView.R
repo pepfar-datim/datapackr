3083629cd542e98159d15807131bd05b78b18539
Christopher Nemarich <55459914+cnemarich@users.noreply.github.com>
2022-07-26 15:48:37 -0400

    Update handling of `getPSNUs` call in `getMechanismView`

--- R/getMechanismView.R
+++ R/getMechanismView.R
@@ -97,12 +97,9 @@ getMechanismView <- function(country_uids = NULL,
 
   # Filter by OU from a vector of country UIDs
   if (!is.null(country_uids)) {
-    ous <- datapackr::getPSNUs(d2_session = d2_session) %>%
-      dplyr::select(ou, ou_id, country_uid) %>%
-      dplyr::distinct() %>%
-      dplyr::filter(country_uid %in% country_uids) %>%
+    ous <- datapackr::getPSNUs(country_uids = country_uids, d2_session = d2_session) %>%
       dplyr::pull(ou) %>%
-      unique(.)
+      unique()
 
     mechs %<>%
       dplyr::filter(ou %in% ous | is.na(ou))
