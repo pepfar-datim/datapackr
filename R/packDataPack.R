b0e77e1dec1b5c04d5b76714eeb7c2a7a603ab7e
Christopher Nemarich <55459914+cnemarich@users.noreply.github.com>
2022-08-02 13:38:37 -0400

    DP-725: Create wrapper function `packTool` (#480)
    * Creates new function `packTool` to serve as a wrapper for `packDataPack` and `packOPUDataPack` and contains common functionality between the two.
    * Updates `check_params` to add check for `output_folder` that checks if folder path is provided and valid.
    * Refactors `packDataPack` and `packOPUDataPack` to work with `packTool`. Now each function only takes in `d` and `d2_session` as parameters. Common functionality has been removed and moved into `packTool`. Each function now checks if appropriate model filepath has been provided and then writes tabs into pre-generated file.
    * Updates `createDataPack` function to add `sane_name` and `operating_unit` to newly created `d` objects.
    * Deprecates `compareTemplateToSchema` after removing its only remaining usage in `packDataPack`.
    * Updates generation scripts
    * Updates documentation

--- R/packDataPack.R
+++ R/packDataPack.R
@@ -9,102 +9,18 @@
 #'
 #' @return Exports a Data Pack to Excel within \code{output_folder}.
 #'
-packDataPack <- function(model_data,
-                         datapack_name,
-                         country_uids,
-                         template_path = NULL,
-                         cop_year = getCurrentCOPYear(),
-                         output_folder = NULL,
-                         results_archive = TRUE,
+packDataPack <- function(d,
                          d2_session = dynGet("d2_default_session",
                                              inherits = TRUE)) {
 
-  interactive_print(datapack_name)
-  interactive_print(country_uids)
-
-  if (is.null(output_folder) || file.access(output_folder, 2) != 0) {
-    stop("Cannot write to output_folder")
-  }
-
-  # Create data train for use across remainder of program
-  d <- list(
-    keychain = list(
-      template_path = template_path,
-      output_folder = output_folder
-    ),
-    info = list(
-      datapack_name = datapack_name,
-      sane_name = getSaneName(datapack_name),
-      country_uids = country_uids,
-      tool = "Data Pack",
-      cop_year =  cop_year,
-      source_user = d2_session$me$userCredentials$username,
-      operating_unit = getOUFromCountryUIDs(country_uids)
-    ),
-    data = list(
-      model_data = model_data
-    )
+  # Checks and reads in Data Pack Model File ####
+  stopifnot(
+    "Model data file could not be read!" = canReadFile(d$info$model_data_path),
+    "Model data is not correct file type! File must have .rds extension." =
+      tools::file_ext(d$info$model_data_path) == "rds"
   )
 
-  # Open schema ####
-  d$info$schema <- pick_schema(cop_year, "Data Pack")
-
-  # Open template ####
-  # Grab correct schema
-  if (is.null(d$keychain$template_path)) {
-    if (cop_year == 2021) {
-      d$info$template_filename <- "COP21_Data_Pack_Template.xlsx"
-    } else if (cop_year == 2022) {
-      d$info$template_filename <- "COP22_Data_Pack_Template.xlsx"
-    }
-
-    d$keychain$template_path <- system.file("extdata",
-                                 d$info$template_filename,
-                                 package = "datapackr",
-                                 mustWork = TRUE)
-  }
-
-  d$keychain$template_path <- handshakeFile(path = d$keychain$template_path,
-                                             tool = "Data Pack Template")
-
-  # Test template against schema ####
-  interactive_print("Checking template against schema and DATIM...")
-  schema <-
-    unPackSchema_datapack(
-      template_path = d$keychain$template,
-      skip = skip_tabs(tool = "Data Pack Template", cop_year = cop_year),
-      cop_year = cop_year)
-
-  if (!identical(d$info$schema, schema)) {
-    stop("Template provided does not match specified schema.")
-  }
-
-  # Place Workbook into play ####
-  if (is.null(d$tool$wb)) {
-    d$tool$wb <- openxlsx::loadWorkbook(d$keychain$template_path)
-  }
-
-  # Set global numeric format ####
-  options("openxlsx.numFmt" = "#,##0")
-
-  # Write Home Sheet info ####
-  d$tool$wb <- writeHomeTab(wb = d$tool$wb,
-                            datapack_name = d$info$datapack_name,
-                            country_uids = d$info$country_uids,
-                            cop_year = cop_year,
-                            tool = "Data Pack")
-
-  # Get PSNU List####
-  d$data$PSNUs <- datapackr::valid_PSNUs %>%
-    dplyr::filter(country_uid %in% country_uids) %>%
-    add_dp_psnu(.) %>%
-    dplyr::arrange(dp_psnu) %>%
-    ## Remove DSNUs
-    dplyr::filter(!is.na(psnu_type)) %>%
-    dplyr::select(PSNU = dp_psnu, psnu_uid, snu1)
-
-  # TODO: Separate PSNUs as parameter for this function, allowing you to include
-  # a list of whatever org units you want. Sites, PSNUs, Countries, whatever.
+  d$data$model_data <- readRDS(d$info$model_data_path)
 
   # Write Main Sheets ####
   d$tool$wb <- packDataPackSheets(wb = d$tool$wb,
@@ -122,13 +38,7 @@ packDataPack <- function(model_data,
 
   # Add Styles ####
   interactive_print("Cleaning up Styles...")
-  ## TODO: Address this in Data Pack?
-  #   ## Add styles to Summary tab
-  # summaryStyle = openxlsx::createStyle(fgFill = "#404040")
-  # openxlsx::addStyle(d$tool$wb, sheet = "Summary",
-  # summaryStyle, cols = 1:2, rows = 1:62, gridExpand = TRUE, stack = TRUE)
 
-    ## Add styles to Spectrum tab ####
   #TODO: See if new openxlsx release addresses this issue
   spectrumStyle1 <- openxlsx::createStyle(fgFill = "#9CBEBD")
   spectrumStyle2 <- openxlsx::createStyle(fgFill = "#FFEB84")
@@ -142,23 +52,10 @@ packDataPack <- function(model_data,
     sheet = "Spectrum",
     spectrumStyle2, cols = 2, rows = 2, gridExpand = TRUE, stack = TRUE)
 
-  # Add validations
+  # Add validations ####
   interactive_print("Adding Validations...")
   #TODO: Adding validations prevents use of openxlsx to add SNU x IM tab
 
-
-  # Save & Export Workbook
-  interactive_print("Saving...")
-  exportPackr(data = d$tool$wb,
-              output_folder = d$keychain$output_folder,
-              tool = d$info$tool,
-              datapack_name = d$info$datapack_name)
-
-  # Save & Export Archive
-  if (results_archive) {
-    exportPackr(data = d,
-                output_folder = d$keychain$output_folder,
-                tool = "Results Archive",
-                datapack_name = d$info$datapack_name)
-  }
+  # Return d object ####
+  d
 }
--- data-raw/COP21_Data_Pack_generation_script.R
+++ data-raw/COP21_Data_Pack_generation_script.R
@@ -1,5 +1,4 @@
 library(datapackr)
-library(magrittr)
 
 # Point to DATIM login secrets ####
 secrets <- Sys.getenv("SECRETS_FOLDER") %>% paste0(., "datim.json")
@@ -8,123 +7,25 @@ datimutils::loginToDATIM(secrets)
 output_folder <- Sys.getenv("OUTPUT_FOLDER") %>% paste0(., "Mock Data Pack/")
 model_data_path <- Sys.getenv("MODEL_DATA_PATH")
 
-model_data <- readRDS(model_data_path)
+# For Generating Individual Data Packs ####
+generation_list <- c("Central America and Brazil")
 
-# COP21_datapacks_countries <- tibble::tribble(
-#   ~datapack_name, ~country_uids,
-#   "Angola","XOivy2uDpMF",                  #1
-#   "Botswana","l1KFEXKI4Dg",                #2
-#   "Burundi","Qh4XMQJhbk8",                 #3
-#   "Cameroon","bQQJe0cC1eD",                #4
-#   "Cote d'Ivoire","ds0ADyc9UCU",           #5
-#   "Democratic Republic of the Congo","ANN4YCOufcP",     #6
-#   "Dominican Republic","NzelIFhEv3C",      #7
-#   "Eswatini","V0qMZH29CtN",                #8
-#   "Ethiopia","IH1kchw86uA",                #9
-#   "Haiti","JTypsdEUNPw",                   #10
-#   "Kenya","HfVjCurKxh2",                   #11
-#   "Lesotho","qllxzIjjurr",                 #12
-#   "Malawi","lZsCb6y0KDX",                  #13
-#   "Mozambique", "h11OyvlPxpJ",             #14
-#   "Namibia","FFVkaV9Zk1S",                 #15
-#   "Nigeria","PqlFzhuPcF1",                 #16
-#   "Rwanda","XtxUYCsDWrR",                  #17
-#   "South Africa", "cDGPF739ZZr",           #18
-#   "South Sudan","WLG0z5NxQs8",             #19
-#   "Tanzania","mdXu6iCbn2G",                #20
-#   "Uganda","FETQ6OmnsKB",                  #21
-#   "Ukraine","ligZVIYs2rL",                 #22
-#   "Vietnam","YM6xn5QxNpY",                 #23
-#   "Zambia","f5RoebaDLMx",                  #24
-#   "Zimbabwe","a71G4Gtcttv",                #25
-#   "Burma","wChmwjpXOw2",                   #26
-#   "Cambodia","XWZK2nop7pM",                #27
-#   "India","skj3e4YSiJY",                   #28
-#   "Indonesia","W73PRZcjFIU",               #29
-#   "Laos","PcXTNoVUrUc",                    #30
-#   "Papua New Guinea","cl7jVQOW3Ks",        #31
-#   "Thailand","Gv5ApcpDrIB",                #32
-#   "Kazakhstan","xVvOdyoS7wi",              #33
-#   "Kyrgyzstan","vm58KTm9wvy",              #34
-#   "Nepal","YlSE5fOVJMa",                   #35
-#   "Tajikistan","ZtoVYbNCnsj",              #36
-#   "Philippines","p1E1K4MWGpa",             #37
-#   "Caribbean Region", c("RKoVudgb05Y",
-#                         "PeOHqAwdtez",
-#                         "WuxG6jzaypt",
-#                         "zhJINyURZ5Y",
-#                         "WSl5y9jxCpC"), #38
-#   "Central America and Brazil", c("joGQFpKiHl9",
-#                             "QKD4CzBG2GM",
-#                             "N7QAPGSaODP",
-#                             "EXVC4bNtv84",
-#                             "w5NMe34EjPN",
-#                             "aUTsSmqqu9O",
-#                             "oK0gC85xx2f"), #39
-#   "Burkina Faso","ZeB2eGmDfGw",            #40
-#   "Ghana","y3zhsvdXlhN",                   #41
-#   "Liberia","kH29I939rDQ",                 #42
-#   "Mali","N3xTKNKu5KM",                    #43
-#   "Senegal","N5GhQWVpVFs",                 #44
-#   "Sierra Leone","ODOymOOWyl0",            #45
-#   "Togo","EIUtrKbw8PQ",                    #46
-#   "Benin","QLimmm7UUKT",                    #47
-#   "Colombia", "yZ0U5wuyirV", #48
-#   "Peru", "Y0PDkv3IkGS", #49
-#   "Venezuela",  "iiPgv10L3fG" #50
-# )
-# 
-# usethis::use_data(COP21_datapacks_countries, overwrite = TRUE, compress = "gzip")
+pick <- datapackr::COP21_datapacks_countries %>%
+  dplyr::filter(datapack_name %in% generation_list)
 
-#Beta Pack Countries list ####
- pick <- datapackr::COP21_datapacks_countries[c(39),]
-
-# Dedupe Testing
-# pick <- datapackr::COP21_datapacks_countries %>%
-#   dplyr::filter(
-#     datapack_name %in% c(
-#       "Colombid",
-#       "Eswatini",
-#       "Namibia",
-#       "Zambia",
-#       "South Sudan",
-#       "Malawi"
-#     )
-#   )
-
-# For individual testing ####
-# pick <- datapackr::COP21_datapacks_countries %>%
-#   dplyr::filter(
-#     !datapack_name %in% c(
-#       "Botswana",
-#       "Cote d'Ivoire",
-#       "Eswatini",
-#       "Ethiopia",
-#       "India",
-#       "Laos",
-#       "Lesotho",
-#       "Malawi",
-#       "Mozambique",
-#       "Rwanda",
-#       "Senegal",
-#       "South Africa",
-#       "Tanzania",
-#       "Uganda",
-#       "Zambia")
-#   )
-# i = 1
-
-# For Production run
+# # For Production run ####
 # pick <- datapackr::COP21_datapacks_countries
 
+# Execution ####
 for (i in 1:NROW(pick)) {
   print(paste0(i," of ",NROW(pick), ": ", pick[[i,1]]))
 
-  packDataPack(model_data = model_data,
-               datapack_name = pick[[i,1]],
-               country_uids = unlist(pick[[i,2]]),
-               template_path = NULL,
-               cop_year = 2021,
-               output_folder = output_folder,
-               results_archive = FALSE)
+  packTool(model_data_path = model_data_path,
+           tool = "Data Pack",
+           datapack_name = pick[[i,1]],
+           country_uids = unlist(pick[[i,2]]),
+           template_path = NULL,
+           cop_year = 2021,
+           output_folder = output_folder,
+           results_archive = FALSE)
 }
