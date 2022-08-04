87ee96384d2fb3a5886a52fd4d217012cac704ff
Jason P. Pickering <jason.p.pickering@gmail.com>
2022-08-04 11:23:59 +0200

    More linting

--- tests/testthat/test-unPackSchema.R
+++ tests/testthat/test-unPackSchema.R
@@ -4,85 +4,85 @@ test_that("We can get sheets to skip", {
 
   tool <- "Data Pack"
   cop_year <- 2022
-  test_schema <- pick_schema(cop_year,tool)
-  this_skip <- getSkipSheets(test_schema,tool,cop_year)
-  expect_named(this_skip,c("package_skip","num","names")) })
+  test_schema <- pick_schema(cop_year, tool)
+  this_skip <- getSkipSheets(test_schema, tool, cop_year)
+  expect_named(this_skip, c("package_skip", "num", "names")) })
 
 
-test_that("We can flag missing sheets which are skipped",{
+test_that("We can flag missing sheets which are skipped", {
   tool <- "Data Pack"
   cop_year <- 2022
-  ref_schema <- pick_schema(cop_year,tool)
+  ref_schema <- pick_schema(cop_year, tool)
   #Simulate deleting the Spectrum tab
   bad_schema <- ref_schema %>%
     dplyr::filter(sheet_name != "Spectrum")
-  test_results <- checkSchema_SkippedSheets(bad_schema,tool,cop_year)
+  test_results <- checkSchema_SkippedSheets(bad_schema, tool, cop_year)
   expect_true(length(test_results) > 0)
-  expect_named(test_results,c("error","data"))
+  expect_named(test_results, c("error", "data"))
 })
 
-test_that("We can pass when all skip sheets are present in the schema",{
+test_that("We can pass when all skip sheets are present in the schema", {
   tool <- "Data Pack"
   cop_year <- 2022
-  ref_schema <- pick_schema(cop_year,tool)
+  ref_schema <- pick_schema(cop_year, tool)
 
-  test_results <- checkSchema_SkippedSheets(ref_schema,tool,cop_year)
+  test_results <- checkSchema_SkippedSheets(ref_schema, tool, cop_year)
   expect_true(length(test_results) == 0)
   expect_null(names(test_results))
 })
 
 
-test_that("We can pass when schema sheets are ordered sequentially",{
+test_that("We can pass when schema sheets are ordered sequentially", {
   tool <- "Data Pack"
   cop_year <- 2022
-  ref_schema <- pick_schema(cop_year,tool)
+  ref_schema <- pick_schema(cop_year, tool)
 
   test_results <- checkSchema_SheetNums(ref_schema)
   expect_true(length(test_results) == 0)
   expect_null(names(test_results))
 })
 
-test_that("We can flag when sheets are not ordered sequentially",{
+test_that("We can flag when sheets are not ordered sequentially", {
   tool <- "Data Pack"
   cop_year <- 2022
-  ref_schema <- pick_schema(cop_year,tool)
+  ref_schema <- pick_schema(cop_year, tool)
   bad_schema <- ref_schema %>%
     dplyr::filter(sheet_num != 2)
 
   test_results <- checkSchema_SheetNums(bad_schema)
 
   expect_true(length(test_results) > 0)
-  expect_named(test_results,c("error","data"), ignore.order = TRUE)
+  expect_named(test_results, c("error", "data"), ignore.order = TRUE)
 })
 
 
-test_that("We can pass when schema names match the package",{
+test_that("We can pass when schema names match the package", {
   tool <- "Data Pack"
   cop_year <- 2022
-  ref_schema <- pick_schema(cop_year,tool)
+  ref_schema <- pick_schema(cop_year, tool)
 
   test_results <- checkSchema_SheetNames(ref_schema, ref_schema)
   expect_true(length(test_results) == 0)
   expect_null(names(test_results))
 })
 
-test_that("We can flag when sheet names do not match the reference schema",{
+test_that("We can flag when sheet names do not match the reference schema", {
   tool <- "Data Pack"
   cop_year <- 2022
-  ref_schema <- pick_schema(cop_year,tool)
+  ref_schema <- pick_schema(cop_year, tool)
   bad_schema <- ref_schema %>%
     dplyr::filter(sheet_num != 2)
 
   test_results <- checkSchema_SheetNames(ref_schema, bad_schema)
 
   expect_true(length(test_results) > 0)
-  expect_named(test_results,c("error","data"), ignore.order = TRUE)
+  expect_named(test_results, c("error", "data"), ignore.order = TRUE)
 })
 
-test_that("We can pass when data sets are valid",{
+test_that("We can pass when data sets are valid", {
   tool <- "Data Pack"
   cop_year <- 2022
-  ref_schema <- pick_schema(cop_year,tool)
+  ref_schema <- pick_schema(cop_year, tool)
 
   test_results <- checkSchema_InvalidDatasets(ref_schema, tool, cop_year)
   expect_true(is.data.frame(test_results))
@@ -93,12 +93,12 @@ test_that("We can pass when data sets are valid",{
 
 })
 
-test_that("We can flag when data sets are invalid",{
+test_that("We can flag when data sets are invalid", {
   tool <- "Data Pack"
   cop_year <- 2022
-  ref_schema <- pick_schema(cop_year,tool)
+  ref_schema <- pick_schema(cop_year, tool)
   bad_schema <- ref_schema %>%
-    dplyr::mutate(dataset = dplyr::case_when( col_type == "reference" ~ "foobar",
+    dplyr::mutate(dataset = dplyr::case_when(col_type == "reference" ~ "foobar",
                                              TRUE ~ dataset))
 
   test_results <- checkSchema_InvalidDatasets(bad_schema, tool, cop_year)
@@ -109,7 +109,7 @@ test_that("We can flag when data sets are invalid",{
                c("sheet_name", "data_structure", "col", "indicator_code", "dataset", "col_type"),
                ignore.order = TRUE)
  # Skipped sheets should have no data set
-  this_skip <- getSkipSheets(ref_schema,tool,cop_year)
+  this_skip <- getSkipSheets(ref_schema, tool, cop_year)
   bad_schema <- ref_schema %>%
     dplyr::mutate(dataset = dplyr::case_when(sheet_name %in% this_skip$names ~ "foobar",
                                    TRUE ~ dataset))
@@ -123,10 +123,10 @@ test_that("We can flag when data sets are invalid",{
                ignore.order = TRUE)
   })
 
-test_that("We can pass valid column types",{
+test_that("We can pass valid column types", {
   tool <- "Data Pack"
   cop_year <- 2022
-  ref_schema <- pick_schema(cop_year,tool)
+  ref_schema <- pick_schema(cop_year, tool)
 
   test_results <- checkSchema_InvalidColType(ref_schema, tool, cop_year)
   expect_true(is.data.frame(test_results))
@@ -137,12 +137,12 @@ test_that("We can pass valid column types",{
 
 })
 
-test_that("We can flag when column types are invalid",{
+test_that("We can flag when column types are invalid", {
   tool <- "Data Pack"
   cop_year <- 2022
-  ref_schema <- pick_schema(cop_year,tool)
+  ref_schema <- pick_schema(cop_year, tool)
   bad_schema <- ref_schema %>%
-    dplyr::mutate(col_type = dplyr::case_when( col_type == "reference" ~ "foobar",
+    dplyr::mutate(col_type = dplyr::case_when(col_type == "reference" ~ "foobar",
                                               TRUE ~ col_type))
 
   test_results <- checkSchema_InvalidColType(bad_schema, tool, cop_year)
@@ -155,10 +155,10 @@ test_that("We can flag when column types are invalid",{
 
 })
 
-test_that("We can pass valid value types",{
+test_that("We can pass valid value types", {
   tool <- "Data Pack"
   cop_year <- 2022
-  ref_schema <- pick_schema(cop_year,tool)
+  ref_schema <- pick_schema(cop_year, tool)
 
   test_results <- checkSchema_InvalidValueType(ref_schema, tool, cop_year)
   expect_true(is.data.frame(test_results))
@@ -169,12 +169,12 @@ test_that("We can pass valid value types",{
 
 })
 
-test_that("We can flag invalid value types",{
+test_that("We can flag invalid value types", {
   tool <- "Data Pack"
   cop_year <- 2022
-  ref_schema <- pick_schema(cop_year,tool)
+  ref_schema <- pick_schema(cop_year, tool)
   bad_schema <- ref_schema %>%
-    dplyr::mutate(value_type = dplyr::case_when( value_type == "integer" ~ "foobar",
+    dplyr::mutate(value_type = dplyr::case_when(value_type == "integer" ~ "foobar",
                                                TRUE ~ value_type))
 
   test_results <- checkSchema_InvalidValueType(bad_schema, tool, cop_year)
@@ -187,28 +187,31 @@ test_that("We can flag invalid value types",{
 
 })
 
-test_that("We can pass valid ages",{
+test_that("We can pass valid ages", {
   tool <- "Data Pack"
   cop_year <- 2022
-  ref_schema <- pick_schema(cop_year,tool)
+  ref_schema <- pick_schema(cop_year, tool)
 
   test_results <- checkSchema_ValidAges(ref_schema)
   expect_true(is.data.frame(test_results))
   expect_true(NROW(test_results) == 0)
   expect_named(test_results,
-               c("sheet_name","col","indicator_code","name","id"),
+               c("sheet_name", "col", "indicator_code", "name", "id"),
                ignore.order = TRUE)
 
 })
 
-test_that("We can flag invalid ages",{
+test_that("We can flag invalid ages", {
   tool <- "Data Pack"
   cop_year <- 2022
-  ref_schema <- pick_schema(cop_year,tool)
+  ref_schema <- pick_schema(cop_year, tool)
 
   modify_age <- function(x) {
-    if (is.null(x)) {return(NULL)}
-    dplyr::mutate(x,name= dplyr::case_when(name == "15-19" ~ "abc123", TRUE ~ name))
+    if (is.null(x)) {
+      return(NULL)
+      }
+
+    dplyr::mutate(x, name = dplyr::case_when(name == "15-19" ~ "abc123", TRUE ~ name))
   }
 
    bad_schema <- ref_schema %>%
@@ -219,12 +222,15 @@ test_that("We can flag invalid ages",{
   expect_true(is.data.frame(test_results))
   expect_true(NROW(test_results) > 0L)
   expect_named(test_results,
-               c("sheet_name","col","indicator_code","name","id"),
+               c("sheet_name", "col", "indicator_code", "name", "id"),
                ignore.order = TRUE)
 
   modify_id <- function(x) {
-    if (is.null(x)) {return(NULL)}
-    dplyr::mutate(x,name= dplyr::case_when(grepl("^tt",id) ~ "abc123", TRUE ~ id))
+    if (is.null(x)) {
+    return(NULL)
+    }
+
+    dplyr::mutate(x, name = dplyr::case_when(grepl("^tt", id) ~ "abc123", TRUE ~ id))
   }
 
   bad_schema <- ref_schema %>%
@@ -235,7 +241,7 @@ test_that("We can flag invalid ages",{
   expect_true(is.data.frame(test_results))
   expect_true(NROW(test_results) > 0L)
   expect_named(test_results,
-               c("sheet_name","col","indicator_code","name","id"),
+               c("sheet_name", "col", "indicator_code", "name", "id"),
                ignore.order = TRUE)
 
 
@@ -243,28 +249,31 @@ test_that("We can flag invalid ages",{
 
 
 
-test_that("We can pass valid sex identifiers",{
+test_that("We can pass valid sex identifiers", {
   tool <- "Data Pack"
   cop_year <- 2022
-  ref_schema <- pick_schema(cop_year,tool)
+  ref_schema <- pick_schema(cop_year, tool)
 
   test_results <- checkSchema_ValidSexes(ref_schema)
   expect_true(is.data.frame(test_results))
   expect_true(NROW(test_results) == 0)
   expect_named(test_results,
-               c("sheet_name","col","indicator_code","name","id"),
+               c("sheet_name", "col", "indicator_code", "name", "id"),
                ignore.order = TRUE)
 
 })
 
-test_that("We can flag invalid sexes",{
+test_that("We can flag invalid sexes", {
   tool <- "Data Pack"
   cop_year <- 2022
-  ref_schema <- pick_schema(cop_year,tool)
+  ref_schema <- pick_schema(cop_year, tool)
 
   modify_males <- function(x) {
-    if (is.null(x)) {return(NULL)}
-    dplyr::mutate(x,name= dplyr::case_when(name == "Male" ~ "Malez", TRUE ~ name))
+    if (is.null(x)) {
+      return(NULL)
+      }
+
+    dplyr::mutate(x, name = dplyr::case_when(name == "Male" ~ "Malez", TRUE ~ name))
   }
 
   bad_schema <- ref_schema %>%
@@ -275,12 +284,16 @@ test_that("We can flag invalid sexes",{
   expect_true(is.data.frame(test_results))
   expect_true(NROW(test_results) > 0L)
   expect_named(test_results,
-               c("sheet_name","col","indicator_code","name","id"),
+               c("sheet_name", "col", "indicator_code", "name", "id"),
                ignore.order = TRUE)
 
   modify_id <- function(x) {
-    if (is.null(x)) {return(NULL)}
-    dplyr::mutate(x,id = dplyr::case_when(name == "Male" ~ "abc123", TRUE ~ id))
+
+    if (is.null(x)) {
+    return(NULL)
+    }
+
+    dplyr::mutate(x, id = dplyr::case_when(name == "Male" ~ "abc123", TRUE ~ id))
   }
 
   bad_schema <- ref_schema %>%
@@ -291,34 +304,37 @@ test_that("We can flag invalid sexes",{
   expect_true(is.data.frame(test_results))
   expect_true(NROW(test_results) > 0L)
   expect_named(test_results,
-               c("sheet_name","col","indicator_code","name","id"),
+               c("sheet_name", "col", "indicator_code", "name", "id"),
                ignore.order = TRUE)
 
 
 })
 
-test_that("We can pass valid KP identifiers",{
+test_that("We can pass valid KP identifiers", {
   tool <- "Data Pack"
   cop_year <- 2022
-  ref_schema <- pick_schema(cop_year,tool)
+  ref_schema <- pick_schema(cop_year, tool)
 
   test_results <- checkSchema_ValidKPs(ref_schema)
   expect_true(is.data.frame(test_results))
   expect_true(NROW(test_results) == 0)
   expect_named(test_results,
-               c("sheet_name","col","indicator_code","name","id"),
+               c("sheet_name", "col", "indicator_code", "name", "id"),
                ignore.order = TRUE)
 
 })
 
-test_that("We can flag KP identifiers",{
+test_that("We can flag KP identifiers", {
   tool <- "Data Pack"
   cop_year <- 2022
-  ref_schema <- pick_schema(cop_year,tool)
+  ref_schema <- pick_schema(cop_year, tool)
 
   modify_pwid <- function(x) {
-    if (is.null(x)) {return(NULL)}
-    dplyr::mutate(x,name = dplyr::case_when(name == "PWID" ~ "DIWP", TRUE ~ name))
+    if (is.null(x)) {
+      return(NULL)
+      }
+
+    dplyr::mutate(x, name = dplyr::case_when(name == "PWID" ~ "DIWP", TRUE ~ name))
   }
 
   bad_schema <- ref_schema %>%
@@ -329,12 +345,14 @@ test_that("We can flag KP identifiers",{
   expect_true(is.data.frame(test_results))
   expect_true(NROW(test_results) > 0L)
   expect_named(test_results,
-               c("sheet_name","col","indicator_code","name","id"),
+               c("sheet_name", "col", "indicator_code", "name", "id"),
                ignore.order = TRUE)
 
   modify_id <- function(x) {
-    if (is.null(x)) {return(NULL)}
-    dplyr::mutate(x,id = dplyr::case_when(name == "PWID" ~ "abc123", TRUE ~ id))
+    if (is.null(x)) {
+    return(NULL)
+    }
+    dplyr::mutate(x, id = dplyr::case_when(name == "PWID" ~ "abc123", TRUE ~ id))
   }
 
   bad_schema <- ref_schema %>%
@@ -345,13 +363,8 @@ test_that("We can flag KP identifiers",{
   expect_true(is.data.frame(test_results))
   expect_true(NROW(test_results) > 0L)
   expect_named(test_results,
-               c("sheet_name","col","indicator_code","name","id"),
+               c("sheet_name", "col", "indicator_code", "name", "id"),
                ignore.order = TRUE)
 
 
 })
-
-
-
-
-
