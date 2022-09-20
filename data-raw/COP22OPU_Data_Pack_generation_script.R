83a2d809fd331c218e71151b40097b755eae169b
sam-bao <sgarman@baosystems.com>
2022-09-20 11:26:45 +0200

    Update COP22OPU_Data_Pack_generation_script.R

--- data-raw/COP22OPU_Data_Pack_generation_script.R
+++ data-raw/COP22OPU_Data_Pack_generation_script.R
@@ -4,19 +4,22 @@ library(datapackr)
 secrets <- Sys.getenv("SECRETS_FOLDER") %>% paste0(., "datim.json")
 datimutils::loginToDATIM(secrets)
 
-output_folder <- Sys.getenv("OUTPUT_FOLDER") %>% paste0(., "COP22 OPUs/")
+output_folder <- Sys.getenv("OUTPUT_FOLDER") %>% paste0(., "Documents/COP22 OPUs/")
 
 # For Generating Individual Data Packs ####
-generation_list <- c("Eswatini")
-
+#generation_list <- c("Eswatini")
+exclude_list <- c("Ukraine", "Venezuela", "Cambodia") # No FY23 targets
 pick <- datapackr::COP21_datapacks_countries %>%
-  dplyr::filter(datapack_name %in% generation_list)
+ # dplyr::filter(datapack_name %in% generation_list) %>%
+  dplyr::filter(!(datapack_name %in% exclude_list)) %>%
+  dplyr::arrange(datapack_name)
 
 # # For Production run ####
 # pick <- datapackr::COP21_datapacks_countries
 
 # Execution ####
 for (i in seq_len(NROW(pick))) {
+  datimutils::loginToDATIM(secrets)
   print(paste0(i, " of ", NROW(pick), ": ", pick[[i, 1]]))
 
   packTool(tool = "OPU Data Pack",
