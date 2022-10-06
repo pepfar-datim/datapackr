77c9ad51bb08b77e2ca565b277bad7be37cc4009
jason.p.pickering <jason.p.pickering@gmail.com>
2022-10-06 08:02:26 +0200

    Test opened OPU

--- tests/testthat/test-packCOP22OPU.R
+++ tests/testthat/test-packCOP22OPU.R
@@ -10,8 +10,10 @@ with_mock_api({
       dplyr::filter(datapack_name %in% generation_list) %>%
       dplyr::arrange(datapack_name)
 
-    output_folder <- tempdir()
-     d <- packTool(tool = "OPU Data Pack",
+    output_folder <- paste0("/tmp/",digest::sha1(Sys.time()))
+    dir.create(output_folder)
+
+    d <- packTool(tool = "OPU Data Pack",
              datapack_name = pick$datapack_name[1],
              country_uids = unlist(pick$country_uids[1]),
              template_path = NULL,
@@ -30,8 +32,30 @@ with_mock_api({
   expect_setequal(names(d_out), c("keychain", "info", "data", "tests", "datim"))
   expect_setequal(names(d_out$datim$OPU), c("dataElement", "period",
                                            "orgUnit", "categoryOptionCombo", "attributeOptionCombo", "value"))
-  #
-  skip("We need to test what was the data in packTool versus unPackTool")
+
+  #Open the generated tool in libreoffice to kick off the formulas
+  #Do not even try and do this on Windows
+  skip_if(Sys.info()["sysname"] == "Windows")
+  lo_path <- system("which libreoffice", intern = TRUE)
+
+  out_dir <- paste0(output_folder,"/out")
+  dir.create(out_dir)
+
+
+  Sys.setenv(LD_LIBRARY_PATH = "/usr/lib/libreoffice/program/")
+  sys_command <- paste0("libreoffice --headless --convert-to xlsx --outdir ", out_dir, " '", d$info$output_file, "'" )
+  system(sys_command)
+
+  out_file <- paste0(out_dir, "/", basename(d$info$output_file))
+
+   #Unpack this tool which has been "opened" in libreoffice
+   d_out <- unPackTool(submission_path = out_file, d2_session = training)
+   expect_identical(d$info$datapack_name, d_out$info$datapack_name)
+   expect_setequal(names(d_out), c("keychain", "info", "data", "tests", "datim"))
+   expect_setequal(names(d_out$datim$OPU), c("dataElement", "period",
+                                             "orgUnit", "categoryOptionCombo", "attributeOptionCombo", "value"))
+
+   unlink(output_folder, recursive = TRUE)
 
   })
 })
