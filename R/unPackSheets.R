47986abed095c0ed4d901eeaa6c36e64c8625d02
jason.p.pickering <jason.p.pickering@gmail.com>
2022-10-12 12:25:39 +0200

    Remove direct header column check

--- R/unPackSheets.R
+++ R/unPackSheets.R
@@ -48,32 +48,14 @@ unPackSheets <- function(d,
   }
 
   # Check if there are any sheets where all columns are missing
-  has_all_header_columns <- checkExistsAllCols(d, sheets = sheets)
-  sheets_with_all_headers <- d$sheets[sheets] %>%
-    purrr::keep(has_all_header_columns) %>%
-    names(.)
-
-  lvl <- "ERROR"
-  if (any(!has_all_header_columns)) {
-    msg <-
-      paste0(
-        lvl, "! The following sheets could not undergo any of our checks as they
-        are missing ALL their column headers. Please correct this error and resubmit: ",
-        paste(sheets[!has_all_header_columns])
-      )
-
-    d$tests$missing_al_columns <- data.frame(sheet_name = sheets[!has_all_header_columns])
-    attr(d$tests$missing_al_columns, "test_name") <- "Missing all columns"
-    d$info$messages <- appendMessage(d$info$messages, msg, lvl)
-
-  }
+  d <- checkExistsIndexCols(d, sheets = sheets)
 
   if (check_sheets) {
-    d <- checkSheetData(d, sheets = sheets_with_all_headers)
+    d <- checkSheetData(d, sheets = sheets)
   }
 
   # Unpack Sheet Data ----
-  targets <- unPackDataPackSheet(d, sheets_with_all_headers)
+  targets <- unPackDataPackSheet(d, sheets)
 
   # Separate Sheet Data ----
   if (separate_datasets) {
