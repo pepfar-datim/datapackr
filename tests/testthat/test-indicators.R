9dd1ebcaaf7aa1b9310961921a7bf5c084646497
jason.p.pickering <jason.p.pickering@gmail.com>
2022-10-19 10:56:00 +0200

    Add test for expression with a missing term

--- tests/testthat/test-indicators.R
+++ tests/testthat/test-indicators.R
@@ -1,4 +1,33 @@
 
+test_that("Can calculate an indicator with missing terms", {
+  ind_uid <- "abc123"
+  de1 <- "aFp9qn4JW5y"
+  coc1 <- "TQPl2RZmz5b"
+  coc2 <- "KLH5fjVMgKG"
+
+  numerator_formula <- paste0(paste0("#{", de1, ".", coc1, "}"), "+", paste0("#{", de1, ".", coc2, "}"))
+
+  inds <- data.frame(id = ind_uid,
+                     name = "Test Indicator 1",
+                     numerator = numerator_formula,
+                     denominator = "1")
+
+  combis <- c(paste0("#{", de1, ".", coc1, "}"))
+  values <- c(10)
+
+  test_values <- evaluateIndicators(combis, values, inds)
+  expect_equal(class(test_values), "data.frame")
+  expect_setequal(names(test_values), c("id", "name", "numerator", "denominator", "value"))
+  expect_equal(NROW(test_values), 1)
+  expect_equal(test_values$value, 10)
+  expect_equal(test_values$numerator, 10)
+  expect_equal(test_values$denominator, 1)
+}
+
+
+)
+
+
 test_that("Can calculate a simple indicator", {
             ind_uid <- "abc123"
             de1 <- "aFp9qn4JW5y"
