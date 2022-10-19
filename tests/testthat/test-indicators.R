fdceeb24f361c292428b04993dcbcced9d3318b6
jason.p.pickering <jason.p.pickering@gmail.com>
2022-10-19 11:14:03 +0200

    Additional tests for indicator calculations

--- tests/testthat/test-indicators.R
+++ tests/testthat/test-indicators.R
@@ -1,4 +1,37 @@
 
+test_that("Can calculate an indicator with totals", {
+  ind_uid <- "abc123"
+  de1 <- "aFp9qn4JW5y"
+  de2 <- "KvzDWWFtHFy"
+  coc1 <- "TQPl2RZmz5b"
+  coc2 <- "KLH5fjVMgKG"
+
+  numerator_formula <- paste0("#{", de1, "}")
+  denominator_formula <- paste0("#{", de1, "}", " + #{", de2, "}")
+
+  inds <- data.frame(id = ind_uid,
+                     name = "Test Indicator 1",
+                     numerator = numerator_formula,
+                     denominator = denominator_formula)
+
+  combis <- c(paste0("#{", de1, ".", coc1, "}"), paste0("#{", de2, ".", coc2, "}"))
+  values <- c(10, 20)
+
+  test_values <- evaluateIndicators(combis, values, inds)
+  expect_equal(class(test_values), "data.frame")
+  expect_setequal(names(test_values), c("id", "name", "numerator", "denominator", "value"))
+  expect_equal(NROW(test_values), 1)
+  expect_equal(test_values$value, 0.33333, tolerance = 0.0001)
+  expect_equal(test_values$numerator, 10)
+  expect_equal(test_values$denominator, 30)
+}
+
+
+)
+
+
+
+
 test_that("Can calculate an indicator with missing terms", {
   ind_uid <- "abc123"
   de1 <- "aFp9qn4JW5y"
