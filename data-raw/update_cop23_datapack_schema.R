9e74eb585976f01468228c918de264018104ee1b
Scott Jackson <scott.j.jackson@gmail.com>
2022-11-16 19:20:31 -0800

    Create update_cop23_datapack_schema.R

---
+++ data-raw/update_cop23_datapack_schema.R
@@ -0,0 +1,28 @@
+## If you've made any edits to the Excel template, rebuild package first to
+## capture these, then run the below.
+
+library(datapackr)
+library(magrittr)
+
+# Point to DATIM login secrets ####
+# secrets <- Sys.getenv("SECRETS_FOLDER") %>% paste0(., "datim.json")
+# datimutils::loginToDATIM(secrets)
+# datimutils::loginToDATIM("~/.secrets/datim.json")
+
+datapack_template_filepath <- system.file("extdata",
+                                          "COP23_Data_Pack_Template.xlsx",
+                                          package = "datapackr",
+                                          mustWork = TRUE)
+cop23_data_pack_schema <-
+  unPackSchema_datapack(
+    template_path = datapack_template_filepath,
+    skip = skip_tabs(tool = "Data Pack Template", cop_year = 2023),
+    cop_year = 2023)
+
+waldo::compare(cop22_data_pack_schema, datapackr::cop22_data_pack_schema)
+
+save(cop22_data_pack_schema,
+     file = "./data/cop22_data_pack_schema.rda",
+     compress = "xz")
+
+## Rebuild package again.
