b58b317e22980f45248bb8b7c58ceb478a401f98
Fausto Lopez <92821116+flopez-bao@users.noreply.github.com>
2022-11-30 13:59:11 -0500

    Expand unit test coverage (#549)
    * test example
    
    - added rds file creation to speed loading of test data
    - added check decimal test
    
    * Update test-unPackingChecks.R
    
    alter test with created data instead of reading rds
    
    * Update test-unPackingChecks.R
    
    simplified test to tribble
    
    * Update test-unPackingChecks.R
    
    - added check decimal values
    - added check negative values
    
    * Update test-unPackingChecks.R
    
    - added tests for all unpacking checks in checkSheetData
    
    * Update test-unPackingChecks.R
    
    remove garbage collection
    
    * Release 5.3.2 (#557)
    
    * Update version & date
    
    * updated to correct version.
    
    * Expand checkExistsIndexCols to check for no data in rows
    
    If no data in rows, or no text in key header cols, sheet should be classified as "empty" and skipped in checking & unpacking
    
    * Skip empty sheets in unPackSheet
    
    * Undo Warning/Error change in checkDupeRows
    
    * Update NEWS.md
    
    * Apply regex | (or) to simplify statement in createAnalytics
    
    * Add check in adorn_import_file whether psnu_prioritizations uses valid PSNUs
    
    Co-authored-by: Jordan <Jordan>
    Co-authored-by: Scott Jackson <scott.j.jackson@gmail.com>
    
    * Update test-unPackingChecks.R
    
    - fix tests failing
    
    * Update test-unPackingChecks.R
    
    linting
    
    * Update test-unPackingChecks.R
    
    - change code for linting purposes
    
    * Update test-unPackingChecks.R
    
    - more infernal linting...
    
    * Change Warning to Error
    
    - missing columns warning changed to error on account of dev branch
    
    * Add unPackDataPack test for COP21
    
    * Minor
    
    * Add missing test in check Analytics
    
    * Add missing tests for message queue
    
    * Create test-unPackSheets.R
    
    - add tests for untested parts of unPackSheets
    
    * add testing for checkColStructure
    
    - added testing for checkColStructure
    - linting
    
    * Update test-checkColStructure.R
    
    - fix issue with failing test
    
    Co-authored-by: Jordan Bales <89547800+JordanBalesBAO@users.noreply.github.com>
    Co-authored-by: Scott Jackson <scott.j.jackson@gmail.com>
    Co-authored-by: Jason Pickering <jason.p.pickering@gmail.com>

---
+++ tests/testthat/test-checkColStructure.R
@@ -0,0 +1,100 @@
+context("test-checkColStructure")
+
+test_that("Can check missing column...", {
+
+  # create minimal schema data
+  d <- list()
+  d$info$messages <- MessageQueue()
+  d$info$cop_year <- 2022
+  d$info$tool <- "Data Pack"
+  d$info$has_psnuxim <- TRUE
+
+  d$info$schema <-
+    tribble(
+      ~indicator_code, ~col, ~sheet_name,
+                 "PSNU",              1, "PSNUxIM",
+       "indicator_code",              2, "PSNUxIM",
+                  "Age",              3, "PSNUxIM",
+                  "Sex",              4, "PSNUxIM",
+               "KeyPop",              5, "PSNUxIM",
+                   "ID",              6, "PSNUxIM"
+    )
+
+  d$data$SNUxIM <-
+    tribble(
+      ~PSNU, ~indicator_code, ~Age, ~Sex, ~KeyPop,
+      NA, NA, NA, NA, NA
+    )
+
+  sheet <- "PSNUxIM"
+  header_row <- headerRow(tool = d$info$tool, cop_year = d$info$cop_year)
+  d <- checkColStructure(d, sheet = sheet)
+
+  # expect missing column ID message
+  testthat::expect_equal(
+    d$info$messages$message,
+    paste0("ERROR! In tab PSNUxIM, MISSING COLUMNS:",
+           " Please ensure no columns have been deleted or renamed",
+           " from the original Data Pack you have received. ->  \n\t* ID\n")
+    )
+
+  # expect level error
+  testthat::expect_equal(d$info$messages$level, "ERROR")
+
+})
+
+
+test_that("Can check duplicate columns and out of order...", {
+
+  # create minimal schema data
+  d <- list()
+  d$info$messages <- MessageQueue()
+  d$info$cop_year <- 2022
+  d$info$tool <- "Data Pack"
+  d$info$has_psnuxim <- TRUE
+
+  d$info$schema <-
+    tribble(
+      ~indicator_code, ~col, ~sheet_name,
+      "PSNU",              1, "PSNUxIM",
+      "indicator_code",              2, "PSNUxIM",
+      "Age",              3, "PSNUxIM",
+      "Sex",              4, "PSNUxIM",
+      "KeyPop",              5, "PSNUxIM",
+      "ID",              6, "PSNUxIM"
+    )
+
+  d$data$SNUxIM <-
+    tribble(
+      ~PSNU, ~indicator_code, ~Age, ~Sex, ~KeyPop, ~ID, ~KeyPop,
+      NA, NA, NA, NA, NA, NA, NA
+    )
+
+  sheet <- "PSNUxIM"
+  header_row <- headerRow(tool = d$info$tool, cop_year = d$info$cop_year)
+  d <- checkColStructure(d, sheet = sheet)
+
+  # expect error on duplicate columns
+  testthat::expect_equal(
+    d$info$messages$message[1],
+    paste0("ERROR! In tab PSNUxIM, DUPLICATE COLUMNS: ",
+           "The following required columns appear twice.",
+           " This must be resolved in your submission in order for processing to continue.",
+           " Please review those columns flagged by this test to determine whether they may",
+           " have been inadvertently duplicated. ->  \n\t* KeyPop\n")
+    )
+
+
+  testthat::expect_equal(d$info$messages$level[1], "ERROR")
+
+  # expect warning on out of order columns
+  testthat::expect_equal(
+    d$info$messages$message[2],
+    paste0("WARNING! In tab PSNUxIM, OUT OF ORDER COLUMNS:",
+           " While it is permitted to rearrange columns within your Data Pack as needed,",
+           " this is not encouraged as it may introduce unintended formula errors.",
+           " Please review these columns to ensure their rearrangement",
+           " has not caused any issues. ->  \n\t* KeyPop\n")
+    )
+  testthat::expect_equal(d$info$messages$level[2], "WARNING")
+})
