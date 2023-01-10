c3ad4d825ad3765bcf9bdcc0c6160206084ff8d4
Scott Jackson <scott.j.jackson@gmail.com>
2023-01-10 09:02:55 -0800

    Update update_cop23_datapack_schema.R

--- data-raw/COP23/update_cop23_datapack_schema.R
+++ data-raw/COP23/update_cop23_datapack_schema.R
@@ -2,12 +2,6 @@
 ## capture these, then run the below.
 
 library(datapackr)
-library(magrittr)
-
-# Point to DATIM login secrets ####
-# secrets <- Sys.getenv("SECRETS_FOLDER") %>% paste0(., "datim.json")
-# datimutils::loginToDATIM(secrets)
-# datimutils::loginToDATIM("~/.secrets/datim.json")
 
 datapack_template_filepath <- system.file("extdata",
                                           "COP23_Data_Pack_Template.xlsx",
@@ -16,10 +10,9 @@ datapack_template_filepath <- system.file("extdata",
 cop23_data_pack_schema <-
   unPackSchema(
     template_path = datapack_template_filepath,
-    skip = skip_tabs(tool = "Data Pack Template", cop_year = 2023),
     cop_year = 2023)
 
-waldo::compare(cop23_data_pack_schema, datapackr::cop23_data_pack_schema)
+waldo::compare(datapackr::cop23_data_pack_schema, cop23_data_pack_schema)
 
 save(cop23_data_pack_schema,
      file = "./data/cop23_data_pack_schema.rda",
