8e7bd83791bdad0db4b822eee7a9522c919d460b
Jason Pickering <jason.p.pickering@gmail.com>
2023-01-19 12:01:43 +0100

    Linting

--- tests/testthat/test-GetDatasetUids.R
+++ tests/testthat/test-GetDatasetUids.R
@@ -6,17 +6,16 @@ test_that("getCOPDatasetUids", {
   expect_error(getCOPDatasetUids(2018))
   expect_error(getCOPDatasetUids(c(2021, 2022)))
   expect_error(getCOPDatasetUids("foo"))
-  expect_error(getCOPDatasetUids("foo",NULL))
+  expect_error(getCOPDatasetUids("foo", NULL))
 
 
 
   expect_true(all(is_uidish(getCOPDatasetUids())))
 
-
   expect_identical(getCOPDatasetUids(), getCOPDatasetUids(getCurrentCOPYear()))
 
   testGetDataSetUIDS <- function(cop_year, datastream) {
-    expect_true(all(sapply(getCOPDatasetUids(cop_year, datastream),is_uidish)))
+    expect_true(all(sapply(getCOPDatasetUids(cop_year, datastream), is_uidish)))
   }
 
   expect_true(testGetDataSetUIDS(2021, "mer_targets"))
@@ -24,4 +23,6 @@ test_that("getCOPDatasetUids", {
   expect_true(testGetDataSetUIDS(2021, "subnat_results"))
   expect_true(testGetDataSetUIDS(2022, "mer_targets"))
   expect_true(testGetDataSetUIDS(2022, c("mer_targets", "subnat_targets", "impatt")))
+
+
 })
