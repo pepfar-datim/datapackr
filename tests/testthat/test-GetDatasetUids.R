ad4fc777c07eebc9704bf91d579f1c812b3c2673
Jason Pickering <jason.p.pickering@gmail.com>
2023-01-20 07:20:18 +0100

    Fix test

--- tests/testthat/test-GetDatasetUids.R
+++ tests/testthat/test-GetDatasetUids.R
@@ -8,21 +8,7 @@ test_that("getCOPDatasetUids", {
   expect_error(getCOPDatasetUids("foo"))
   expect_error(getCOPDatasetUids("foo", NULL))
 
-  expect_true(is.vector(getDatasetUids(cop_year = 2021)))
-  expect_true(all(is_uidish(getDatasetUids(cop_year = 2021))))
-  #expect_identical(getDatasetUids(), getDatasetUids(getCurrentCOPYear()))
-
-  testthat::expect_setequal(getDatasetUids(2021, "mer_targets"), c("YfZot37BbTm",
-                                                                   "cihuwjoY5xP",
-                                                                   "wvnouBMuLuE")
-  )
-  testthat::expect_setequal(getDatasetUids(2021, "mer_results"), c("BHlhyPmRTUY",
-                                                                   "HfhTPdnRWES",
-                                                                   "MGNVwVicMVm"))
-  testthat::expect_setequal(getDatasetUids(2021, "subnat_targets"), "Va7TYyHraRn")
-  testthat::expect_setequal(getDatasetUids(2021, "impatt"), "Zn27xns9Fmx")
-  testthat::expect_setequal(getDatasetUids(2021, "subnat_results"), "IXiORiVFqIv")
-
+  expect_true(all(is_uidish(getCOPDatasetUids(cop_year = 2021))))
   expect_true(all(is_uidish(getCOPDatasetUids())))
 
   expect_identical(getCOPDatasetUids(), getCOPDatasetUids(getCurrentCOPYear()))
