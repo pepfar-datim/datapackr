d55bb24c51ff77c96fec39aa65d74ff2a297be24
Jason Pickering <jason.p.pickering@gmail.com>
2023-01-27 12:19:22 +0100

    Fix issues with getMechanismView

--- tests/testthat/test-getMechanismView.R
+++ tests/testthat/test-getMechanismView.R
@@ -1,27 +1,50 @@
 
-context("Get a mechanism view when logged into DATIM")
+context("Get a mechanism view")
 
   with_mock_api({
-    # This part works.
-    test_that("We can get a mechanism list when logged in", {
-      test_mech_list <- datimutils::getSqlView(sql_view_uid = "fgUtV6e9YIX",
-                                               d2_session = training)
 
-      expect_type(test_mech_list, "list")
+    test_that("We can get a mechanism list ", {
+      mechs <- getMechanismView(d2_session = training)
+      expect_named(mechs, c("mechanism_desc",
+                           "mechanism_code",
+                           "attributeOptionCombo",
+                           "partner_desc",
+                           "partner_id", "agency", "ou", "startdate", "enddate"))
 
-      mechs_names <- c("mechanism",
-                     "code",
-                     "uid",
-                     "partner",
-                     "primeid",
-                     "agency",
-                     "ou",
-                     "startdate",
-                     "enddate")
+      expect_true(NROW(mechs) > 0)
 
-      expect_true(setequal(mechs_names, names(test_mech_list)))
+      expect_false(any(duplicated(mechs$mechanism_desc)))
+      expect_false(any(duplicated(mechs$mechanism_code)))
+      expect_false(any(duplicated(mechs$attributeOptionCombo)))
+      expect_true(all(is_uidish(mechs$attributeOptionCombo)))
 
-      #Expect error on a faulty dataset UID
-      expect_error(datimutils::getSqlView("foo"))
+
+      #Zambia mechs only
+      expect_identical("Zambia", unique(getMechanismView(country_uids = "f5RoebaDLMx",
+                                                        include_dedupe = FALSE,
+                                                        include_default = FALSE,
+                                                        include_MOH = FALSE,
+                                                        d2_session = training)$ou))
+
+      #Should not include dedupe
+      expect_identical(0L, sum(getMechanismView(country_uids = "f5RoebaDLMx",
+                                                include_dedupe = FALSE,
+                                                include_default = FALSE,
+                                                include_MOH = FALSE,
+                                                d2_session = training)$mechanism_code %in% c("00000", "00001")))
+
+      #Should include dedupe
+      expect_identical(2L, sum(getMechanismView(country_uids = "f5RoebaDLMx",
+                                                        include_dedupe = TRUE,
+                                                        include_default = FALSE,
+                                                        include_MOH = FALSE,
+                                                        d2_session = training)$mechanism_code %in% c("00000", "00001")))
+
+      #Bogus country UIDS with no defaults produces no rows
+      expect_warning(getMechanismView(country_uids = "abcdef12345",
+                                      include_default = FALSE,
+                                      d2_session = training))
+      #Bogus cop year gives an error
+      expect_error(getMechanismView(cop_year = 1999, d2_session = training))
     })
   })
