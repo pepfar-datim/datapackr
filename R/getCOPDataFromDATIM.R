6728ec7f8c071a6ffc8caa418f3aa7fb8754272b
Fausto Lopez <flopez@baosystems.com>
2023-02-03 13:13:11 -0500

    return uids in getCOPDataFromDATIM
    - added attributeOptionComboIdScheme to return uids

--- R/getCOPDataFromDATIM.R
+++ R/getCOPDataFromDATIM.R
@@ -43,14 +43,14 @@ getCOPDataFromDATIM <- function(country_uids,
       tibble::tribble(~ key, ~ value,
                       "children", "true",
                       #TODO: We need to migrate this to use UIDs
+                      # added uid return below
                       "categoryOptionComboIdScheme", "code",
+                      "attributeOptionComboIdScheme", "code",
                       "includeDeleted", "false",
                       "period", paste0(cop_year, "Oct")
       )
     )
 
-  # get data from datim using dataValueSets
-  # rename to standard names
   datim_data <-
     tryCatch({
       datimutils::getDataValueSets(parameters$key,
