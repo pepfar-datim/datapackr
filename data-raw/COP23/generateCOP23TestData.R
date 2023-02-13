4c379e66ceab324a7a850fdc7d4f80803e5cdba2
Jason Pickering <jason.p.pickering@gmail.com>
2023-02-13 08:26:17 +0100

    Add script for generating COP23 test data

--- data-raw/COP23/generateCOP23TestData.R
+++ data-raw/COP23/generateCOP23TestData.R
@@ -1,106 +1,152 @@
 require(datapackr)
 require(magrittr)
-
-country <- "Angola"
-#Get Angola OUs
-psnus <- valid_OrgUnits %>%
-  dplyr::filter(country_name == country,
-                is.na(DREAMS)) %>%
-  dplyr::select(orgUnit = uid)
-
-datasets_to_pull <- tibble::tribble(
-  ~dataset_uid, ~dataset_name, ~FY, ~targets_results, ~datastream, ~org_unit,
-  "dA9C5bL44NX", "FY24 MER Targets", 2024, "targets", "mer", "psnu",
-  "cihuwjoY5xP", "FY24 MER DOD Targets", 2024, "targets", "mer", "_mil",
-  "vpDd67HlZcT", "FY24 DREAMS Targets", 2024, "targets", "dreams", "dsnu",
-  "kWKJQYP1uT7", "FY24 IMPATT", 2024, "targets", "impatt", "psnu",
-  "CxMsvlKepvE", "FY23 IMPATT", 2023, "targets", "impatt", "psnu",
-  "bKSmkDP5YTc", "FY24 SUBNAT Targets", 2024, "targets", "subnat", "psnu",
-  "J4tdiDEi08O", "FY23 SUBNAT Targets", 2023, "targets", "subnat", "psnu",
-  "IXiORiVFqIv", "FY22 SUBNAT Results", 2022, "results", "subnat", "psnu")
-
-ds <- data.frame()
-
-fullCodeList <-
-  lapply(
-    datasets_to_pull$dataset_uid,
-    function(x) {
-      cl <- datimutils::getSqlView(sql_view_uid = "DotdxKrNZxG",
-                                   variable_keys = "dataSets",
-                                   variable_values = x) %>%
-        dplyr::mutate(dataset_uid = x)
-      ds <- rbind(ds, cl)
-    }) %>%
-  do.call(rbind, .) %>%
-  dplyr::left_join(
-    dplyr::select(datasets_to_pull, -org_unit),
-    by = c("dataset_uid" = "dataset_uid"))
-
-#A few mechanisms
-#18437, 81002
-mechs <- data.frame(attributeOptionCombo = c("Nv8D9fP0IzS", "eouPVGemcrh"))
-
-des_cocs_mer <- fullCodeList %>%
-  dplyr::filter(datastream == "mer") %>%
-  dplyr::select(dataelementuid, categoryoptioncombouid) %>%
-  dplyr::distinct() %>%
-  dplyr::full_join(mechs, by = character())
-
-des_cocs_impatt <-  fullCodeList %>%
-  dplyr::filter(datastream == "impatt") %>%
-  dplyr::select(dataelementuid, categoryoptioncombouid) %>%
-  dplyr::distinct() %>%
-  dplyr::mutate(attributeOptionCombo = default_catOptCombo())
-
-
-
-des_cocs <- rbind(des_cocs_mer, des_cocs_impatt)
-
-#Mock a d object
-d <- list()
-d$info$cop_year <- 2023
-d$info$country_uids <- COP21_datapacks_countries %>%
-  dplyr::filter(datapack_name == country) %>%
-  dplyr::pull(country_uids)
-d$info$tool <- "Target Setting Tool"
-d$info$operating_unit$ou <- country
-d$info$datapack_name <- country
-d$info$operating_unit$ou_id <- d$info$country_uids
-d$info$has_error <- FALSE
-d$info$sane_name <- country
-d$info$approval_status <- "UNAPPROVED"
-d$info$source_user <- "littlebobbytables"
-
+require(dplyr)
 
 set.seed(5883959)
-cop23_sample_data <- psnus %>%
-  dplyr::full_join(des_cocs, by = character()) %>%
-  dplyr::arrange(orgUnit, dataelementuid, categoryoptioncombouid, attributeOptionCombo) %>%
-  dplyr::mutate(period = "2023Oct", value = sample(0:200, dplyr::n(), replace = TRUE)) %>%
-  dplyr::select(dataElement = dataelementuid,
-                period,
-                orgUnit,
-                categoryOptionCombo = categoryoptioncombouid,
-                attributeOptionCombo,
-                value)
 
-cop23_y2_sample_data <- cop23_sample_data %>%
-  dplyr::filter(attributeOptionCombo != datapackr::default_catOptCombo()) %>% #Remove IMPATT data
-  dplyr::group_by(dataElement, categoryOptionCombo) %>%
-  dplyr::summarise(value = sum(round(value * 1.25)), .groups = "drop") %>%
-  dplyr::mutate(orgUnit = d$info$country_uids[[1]],
-                period = "2024Oct",
-                attributeOptionCombo = datapackr::default_catOptCombo()) %>%
-  dplyr::select(dataElement,
-                period,
-                orgUnit,
-                categoryOptionCombo,
-                attributeOptionCombo,
-                value)
+getSampleDataCodeList <- function() {
+  datasets_to_pull <- tibble::tribble(
+    ~dataset_uid, ~dataset_name, ~FY, ~targets_results, ~datastream, ~org_unit,
+    "dA9C5bL44NX", "FY24 MER Targets", 2024, "targets", "mer", "psnu",
+    "cihuwjoY5xP", "FY24 MER DOD Targets", 2024, "targets", "mer", "_mil",
+    "vpDd67HlZcT", "FY24 DREAMS Targets", 2024, "targets", "dreams", "dsnu",
+    "kWKJQYP1uT7", "FY24 IMPATT", 2024, "targets", "impatt", "psnu",
+    "CxMsvlKepvE", "FY23 IMPATT", 2023, "targets", "impatt", "psnu",
+    "bKSmkDP5YTc", "FY24 SUBNAT Targets", 2024, "targets", "subnat", "psnu",
+    "J4tdiDEi08O", "FY23 SUBNAT Targets", 2023, "targets", "subnat", "psnu",
+    "IXiORiVFqIv", "FY22 SUBNAT Results", 2022, "results", "subnat", "psnu")
+
+  ds <- data.frame()
+
+    lapply(
+      datasets_to_pull$dataset_uid,
+      function(x) {
+        cl <- datimutils::getSqlView(sql_view_uid = "DotdxKrNZxG",
+                                     variable_keys = "dataSets",
+                                     variable_values = x) %>%
+          dplyr::mutate(dataset_uid = x)
+        ds <- rbind(ds, cl)
+      }) %>%
+    do.call(rbind, .) %>%
+    dplyr::left_join(
+      dplyr::select(datasets_to_pull, -org_unit),
+      by = c("dataset_uid" = "dataset_uid"))
+}
+
+generateSampleCOP23Data <- function(x, fullCodeList) {
+  country_uids <- COP21_datapacks_countries %>%
+    dplyr::filter(datapack_name == x$ou) %>%
+    dplyr::pull(country_uids) %>%
+    unlist(.)
+
+  psnus <- valid_OrgUnits %>%
+    dplyr::filter(country_uid %in% country_uids,
+                  is.na(DREAMS)) %>%
+    dplyr::select(orgUnit = uid)
 
 
-d$datim$cop23_sample_data <- cop23_sample_data
-d$datim$cop23_year2_sample_data <- cop23_y2_sample_data
+
+  #A few mechanisms
+  mechs <- getMechanismView(country_uids = country_uids, cop_year = "2022") %>%
+    dplyr::filter(mechanism_code %in% x$mechs) %>%
+    dplyr::select(attributeOptionCombo)
+
+  des_cocs_mer <- fullCodeList %>%
+    dplyr::filter(datastream == "mer",
+                  targets_results == "targets") %>%
+    dplyr::select(dataelementuid, categoryoptioncombouid) %>%
+    dplyr::distinct() %>%
+    dplyr::full_join(mechs, by = character())
+
+  #Subnat, DREAMS, Impatt
+  des_cocs_default <-  fullCodeList %>%
+    dplyr::filter(datastream %in% c("subnat", "dreams", "impatt"))  %>%
+    dplyr::select(dataelementuid, categoryoptioncombouid) %>%
+    dplyr::distinct() %>%
+    dplyr::mutate(attributeOptionCombo = default_catOptCombo())
+
+  ratio_des <- fullCodeList %>%
+    dplyr::filter(datastream == "impatt") %>%
+    dplyr::filter(grepl("_PREV", dataelement)) %>%
+    dplyr::pull(dataelementuid) %>%
+    unique()
+
+  des_cocs_numeric <- rbind(des_cocs_mer, des_cocs_default) %>%
+   dplyr::filter(!(dataelementuid %in% percentage_des))
+
+  des_cocs_ratio <- des_cocs_default %>%
+    dplyr::filter(dataelementuid %in% percentage_des)
+
+  #Mock a d object
+  d <- list()
+  d$info$cop_year <- 2023
+  d$info$country_uids <- country_uids
+  d$info$tool <- "Target Setting Tool"
+  d$info$operating_unit$ou <- x$ou
+  d$info$datapack_name <- x$ou
+  d$info$operating_unit$ou_id <- d$info$country_uids
+  d$info$has_error <- FALSE
+  d$info$sane_name <- getSaneName(x$ou)
+  d$info$approval_status <- "UNAPPROVED"
+  d$info$source_user <- "littlebobbytables"
+
+
+
+  d$datim$cop23_sample_data <- psnus %>%
+    dplyr::full_join(des_cocs_numeric, by = character()) %>%
+    dplyr::arrange(orgUnit, dataelementuid, categoryoptioncombouid, attributeOptionCombo) %>%
+    dplyr::mutate(period = "2023Oct", value = sample(0:200, dplyr::n(), replace = TRUE)) %>%
+    dplyr::select(dataElement = dataelementuid,
+                  period,
+                  orgUnit,
+                  categoryOptionCombo = categoryoptioncombouid,
+                  attributeOptionCombo,
+                  value)
+
+  #Generate ratio values
+  d$datim$cop23_prev_data <- psnus %>%
+    dplyr::full_join(des_cocs_ratio, by = character()) %>%
+    dplyr::arrange(orgUnit, dataelementuid, categoryoptioncombouid, attributeOptionCombo) %>%
+    dplyr::mutate(period = "2023Oct", value = runif(dplyr::n())) %>%
+    dplyr::select(dataElement = dataelementuid,
+                  period,
+                  orgUnit,
+                  categoryOptionCombo = categoryoptioncombouid,
+                  attributeOptionCombo,
+                  value)
+
+
+
+  if (length(d$info$country_uids) == 1) {
+
+    d$datim$cop23_year2_sample_data  <- d$datim$cop23_sample_data %>%
+      dplyr::filter(attributeOptionCombo != datapackr::default_catOptCombo()) %>% #Remove IMPATT data
+      dplyr::group_by(dataElement, categoryOptionCombo) %>%
+      dplyr::summarise(value = sum(round(value * 1.25)), .groups = "drop") %>%
+      dplyr::mutate(
+        orgUnit = d$info$country_uids,
+        period = "2024Oct",
+        attributeOptionCombo = datapackr::default_catOptCombo()
+      ) %>%
+      dplyr::select(dataElement,
+                    period,
+                    orgUnit,
+                    categoryOptionCombo,
+                    attributeOptionCombo,
+                    value)
+  } else {
+        d$datim$cop23_year2_sample_data <- NULL
+  }
+
+
+  sendTimeStampLogToS3(d)
+  sendDATIMExportToS3(d)
+  if (!is.null(d$datim$cop23_sample_data)) {
+    sendYear2ExportToS3(d)
+  }
+
+
+}
 
 createS3BucketTags <- function(d) {
   d$info$country_uids <- paste(d$info$country_uids, sep = "", collapse = "_")
@@ -228,56 +274,78 @@ sendDATIMExportToS3 <- function(d) {
 }
 
 sendYear2ExportToS3 <- function(d, custom_object_name = NULL) {
-  #Write the flatpacked output
-  tmp <- tempfile()
 
-  datim_export <- d$datim$cop23_year2_sample_data %>%
-    mutate(across(everything(), as.character))
+  if (!is.null(d$datim$cop23_year2_sample_data)) {
+    #Write the flatpacked output
+    tmp <- tempfile()
 
-  #Need better error checking here.
-  write.table(
-    datim_export,
-    file = tmp,
-    quote = FALSE,
-    sep = "|",
-    row.names = FALSE,
-    na = "",
-    fileEncoding = "UTF-8"
-  )
+    datim_export <- d$datim$cop23_year2_sample_data  %>%
+      dplyr::bind_rows(d$datim$cop23_prev_data) %>%
+      mutate(across(everything(), as.character))
 
-  # Load the file as a raw binary
-  read_file <- file(tmp, "rb")
-  raw_file <- readBin(read_file, "raw", n = file.size(tmp))
-  close(read_file)
+    #Need better error checking here.
+    write.table(
+      datim_export,
+      file = tmp,
+      quote = FALSE,
+      sep = "|",
+      row.names = FALSE,
+      na = "",
+      fileEncoding = "UTF-8"
+    )
 
-  object_tags <- createS3BucketTags(d)
+    # Load the file as a raw binary
+    read_file <- file(tmp, "rb")
+    raw_file <- readBin(read_file, "raw", n = file.size(tmp))
+    close(read_file)
 
-  object_name <- paste0("datim_export/", gsub("^20", "cop", d$info$cop_year),
-                        ifelse(d$info$cop_year == 2021, "_opu", ""),
-                        "/", d$info$sane_name, "_Y2.csv")
+    object_tags <- createS3BucketTags(d)
 
-  s3 <- paws::s3()
+    object_name <- paste0("datim_export/", gsub("^20", "cop", d$info$cop_year),
+                          ifelse(d$info$cop_year == 2021, "_opu", ""),
+                          "/", d$info$sane_name, "_Y2.csv")
 
-  r <- tryCatch({
-    foo <- s3$put_object(Bucket = Sys.getenv("AWS_S3_BUCKET"),
-                         Body = raw_file,
-                         Key = object_name,
-                         Tagging = object_tags,
-                         ContentType = "text/csv")
-    message("DATIM Export sent to S3", name = "datapack")
-    TRUE
-  },
-  error = function(err) {
-    message("DATIM Export could not be sent to S3", name = "datapack")
-    message(err, name = "datapack")
-    FALSE
-  })
+    s3 <- paws::s3()
 
-  unlink(tmp)
+    r <- tryCatch({
+      foo <- s3$put_object(Bucket = Sys.getenv("AWS_S3_BUCKET"),
+                           Body = raw_file,
+                           Key = object_name,
+                           Tagging = object_tags,
+                           ContentType = "text/csv")
+      message("DATIM Export sent to S3", name = "datapack")
+      TRUE
+    },
+    error = function(err) {
+      message("DATIM Export could not be sent to S3", name = "datapack")
+      message(err, name = "datapack")
+      FALSE
+    })
 
-  return(r)
+    unlink(tmp)
+
+    return(r)
+  }
+
 }
 
-sendTimeStampLogToS3(d)
-sendDATIMExportToS3(d)
-sendYear2ExportToS3(d)
+
+ous <- list(
+  list(
+    ou =  "Angola",
+    mechs = c("81002", "18437")
+  ),
+  list(
+    ou = "Caribbean Region",
+    mechs = c("100081", "100129", "12567")
+  ),
+  list(ou = "Cameroon",
+       mechs = c("85436", "81586")
+  )
+)
+
+secrets <- Sys.getenv("SECRETS_FOLDER") %>% paste0(., "datim.json")
+datimutils::loginToDATIM(secrets)
+
+fullCodeList <- getSampleDataCodeList()
+lapply(ous, function(x) generateSampleCOP23Data(x, fullCodeList = fullCodeList))
