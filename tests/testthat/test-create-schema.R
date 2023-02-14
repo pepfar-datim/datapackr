50a3493fe5cc1d6a5d02d1e310d51b2ef9bcdfb0
Jason Pickering <jason.p.pickering@gmail.com>
2023-02-14 10:34:13 +0100

    Add test to ensure we can detect schema differences

--- tests/testthat/test-create-schema.R
+++ tests/testthat/test-create-schema.R
@@ -238,3 +238,63 @@ with_mock_api({
   })
 
 })
+
+
+
+with_mock_api({
+  test_that("Can detect when template and schema do not match", {
+    template_file <- test_sheet("COP23_Data_Pack_Template_v1.xlsx")
+    expect_true(file.exists(template_file))
+    test_dataset <-   unPackSchema(
+      template_path = template_file,
+      skip = skip_tabs(tool = "Data Pack Template", cop_year = 2023),
+      cop_year = 2023)
+    expect_type(test_dataset, "list")
+    expect_named(
+      test_dataset,
+      c(
+        "sheet_num",
+        "sheet_name",
+        "data_structure",
+        "col",
+        "indicator_code",
+        "dataset",
+        "col_type",
+        "value_type",
+        "dataelement_dsd",
+        "dataelement_ta",
+        "categoryoption_specified",
+        "valid_ages",
+        "valid_sexes",
+        "valid_kps",
+        "formula",
+        "FY",
+        "period"
+      ),
+      ignore.order = TRUE
+    )
+    expect_type(test_dataset$sheet_num, "integer")
+    expect_type(test_dataset$sheet_name, "character")
+    expect_type(test_dataset$data_structure, "character")
+    expect_type(test_dataset$col, "integer")
+    expect_type(test_dataset$indicator_code, "character")
+    expect_type(test_dataset$dataset, "character")
+    expect_type(test_dataset$col_type, "character")
+    expect_type(test_dataset$value_type, "character")
+    expect_type(test_dataset$dataelement_dsd, "character")
+    expect_type(test_dataset$dataelement_ta, "character")
+    expect_type(test_dataset$categoryoption_specified, "character")
+    expect_type(test_dataset$valid_ages, "list")
+    expect_type(test_dataset$valid_sexes, "list")
+    expect_type(test_dataset$valid_kps, "list")
+    expect_type(test_dataset$formula, "character")
+    expect_type(test_dataset$FY, "double")
+    expect_type(test_dataset$period, "character")
+    load(getRDA("cop23_data_pack_schema"))
+    expect_false(identical(test_dataset, cop23_data_pack_schema))
+
+
+  })
+
+})
+
