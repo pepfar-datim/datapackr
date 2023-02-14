c50de3b217b8d7c7861d67a00e72726704930025
Jason Pickering <jason.p.pickering@gmail.com>
2023-02-14 10:02:32 +0100

    Test build fails

--- tests/testthat/test-build.R
+++ tests/testthat/test-build.R
@@ -1,6 +1,6 @@
 context("Test the build")
 
 test_that("We can fail the build", {
-  skip("Skip failing the build unless we need to test it.")
+  #skip("Skip failing the build unless we need to test it.")
   expect_equal(2, 1)
 })
