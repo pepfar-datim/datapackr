443f7e39527d9d52f6cf8de5e412b5276dbdb7c6
Jason Pickering <jason.p.pickering@gmail.com>
2023-02-14 07:13:58 +0100

    More CI tweaks

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -10,7 +10,7 @@ onCI <- isTRUE(as.logical(Sys.getenv("CI")))
 getTemplate <- function(path) {
   ifelse(
     onCI,
-    rprojroot::find_package_root_file(path, path="~/project"),
+    path= paste("/root/datapackr/inst/extdata/", path),
     rprojroot::find_package_root_file(path))
 }
 
