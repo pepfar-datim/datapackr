5a644349b2ab71462dbc72a8785713d2091a5b7e
Jason Pickering <jason.p.pickering@gmail.com>
2023-02-14 07:28:56 +0100

    Syntax fix

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -10,7 +10,7 @@ onCI <- isTRUE(as.logical(Sys.getenv("CI")))
 getTemplate <- function(path) {
   ifelse(
     onCI,
-    path= paste("/root/project/datapackr/inst/extdata/", path),
+    paste0("/root/project/datapackr/inst/extdata/", path),
     rprojroot::find_package_root_file(path))
 }
 
