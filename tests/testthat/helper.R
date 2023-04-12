0b8a1867b0eb1a32d1bc9c9fee52300448933846
jason-p-pickering <jason.p.pickering@gmail.com>
2023-04-12 06:39:18 +0200

    Alter some tests so that devtools::test can work
    Add tests for updating PSNUxIM values

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -6,19 +6,59 @@ test_config <- function(fname) {
 
 onCI <- isTRUE(as.logical(Sys.getenv("CI")))
 
+#' Title
+#'
+#' @param path
+#' @description This helper function eases the process to find files
+#' which are needed during testing. Thsese template files however
+#' will be located in different places, depending on the testing
+#' infrastructure.
+#'
+#' If we are on the CI server, we know the path, so we will just use that.
+#' Otherwise, if the test is run from within Rstudio or the command line
+#' the actual location of the template files may be different.
+#'
+#' The function devtools::test() is also used during covr::report(),
+#' so this test route must work as well.
+#'
+#' @return Full file path to the file
+#'
 getTemplate <- function(path) {
-  ifelse(
-    onCI,
-    paste0("/root/project/inst/extdata/", path),
-    rprojroot::find_package_root_file(paste0("inst/extdata/", path)))
+
+  if (onCI) {
+    return(paste0("/root/project/inst/extdata/", path))
+  }
+
+
+  first <- rprojroot::find_package_root_file("inst/extdata", path)
+  second <- rprojroot::find_package_root_file("extdata", path)
+
+  if (file.exists(first)) {
+    return(first)
+  } else if (file.exists(second)) {
+    return(second)
+  } else {
+    stop("No template could be found")
+  }
+
+
 }
 
 getRDA <- function(object_name) {
-  ifelse(
-    onCI,
-    paste0("/root/project/data/", object_name, ".rda"),
-    rprojroot::find_package_root_file(paste0("data/", object_name, ".rda")))
 
+  if (onCI) {
+    return(paste0("/root/project/data/", object_name, ".rda"))
+  }
+
+  first <- rprojroot::find_package_root_file("data", path)
+  second <- paste0(getwd(), "/", path)
+  if (file.exists(first)) {
+    return(first)
+  } else if (file.exists(second)) {
+    return(second)
+  } else {
+    stop("No template could be found")
+  }
 }
 
 # login object stubs sufficient for use in mocked api calls
