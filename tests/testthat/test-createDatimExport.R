76a1aa50ae2d077b61783330d4d3453a8efb9bd6
jason-p-pickering <jason.p.pickering@gmail.com>
2023-04-26 08:24:52 +0200

    Fix tests

--- tests/testthat/test-createDatimExport.R
+++ tests/testthat/test-createDatimExport.R
@@ -2,23 +2,30 @@ context("Create a DATIM export object")
 
 test_that("Can export COP22 data", {
 
-  expect_warning(d <- loadDataPack(submission_path = test_sheet("COP22_DataPack_unPackingChecks.xlsx"),
-                                   tool = "Data Pack",
-                                   country_uids = NULL,
-                                   cop_year = NULL,
-                                   load_wb = TRUE,
-                                   load_sheets = TRUE,
-                                   d2_session = training))
+
+  d <-
+    loadDataPack(
+      submission_path = test_sheet("COP23_sample_DataPack_Malawi.xlsx"),
+      tool = "Data Pack",
+      country_uids = NULL,
+      cop_year = NULL,
+      load_sheets = TRUE,
+      d2_session = training)
+
+  d %<>%
+    unPackSheets(., check_sheets = FALSE) %>%
+    packForDATIM(., type = "Undistributed MER") %>%
+    packForDATIM(., type = "SUBNAT_IMPATT")
 
   datim_export <- createDATIMExport(d)
-
   expect_true(all(sapply(datim_export, class) == "character"))
-  skip("No idea why this is failing")
+
   expect_named(
     datim_export,
     c(
       "dataElement",
       "period",
+      "orgUnit",
       "categoryOptionCombo",
       "attributeOptionCombo",
       "value"
