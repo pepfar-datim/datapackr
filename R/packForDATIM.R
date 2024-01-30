f85bdb24348b999fde4f435878ac1ce2509990ac
Jordan <Jordan>
2024-01-30 08:36:54 -0500

    linting

--- R/packForDATIM.R
+++ R/packForDATIM.R
@@ -122,7 +122,7 @@ packForDATIM <- function(d, type = NULL) {
   # DP-1195: Keep data from past cop year and current cop year.
   if (type == "SUBNAT_IMPATT") {
     current_period <- paste0(d$info$cop_year, "Oct")
-    previous_year_period <- paste0(d$info$cop_year - 1 , "Oct")
+    previous_year_period <- paste0(d$info$cop_year - 1, "Oct")
     pop_data <- c("MktYDp33kd6", "KssDaTsGWnS", "iwSejvD8cXl", "lJtpR5byqps",
                  "nF19GOjcnoD", "xghQXueYJxu","P2XNbiNnIqV","zoKiMGRucOY")
     if (d$info$cop_year <= 2023) {
