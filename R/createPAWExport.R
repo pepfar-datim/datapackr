ea435b85a18d64945974512eee53109add7998ab
Fausto Lopez <92821116+flopez-bao@users.noreply.github.com>
2024-02-15 15:43:15 -0500

    altered createDATIMExport and created createPAWExport (#795)
    * altered createDATIMExport and created createPAWExport
    
    - altered createDATIMExport and created createPAWExport
    - added pertinent tests
    - edited packForDATIM function documentation
    
    * - removed cop22 ifelse for data pack exports, added new function
    
    - removed cop22 ifelse for data pack exports
    - added createDATAExport as concept for handling createDATIMExport and createPAWExport in one function
    -added tests
    
    * revert to removing pop data for both 2023 and 2024
    
    - revert to removing pop data for both 2023 and 2024
    
    * drop createDATAExport function
    
    - drop createDATAExport function
    
    * separate 2023  from createDatimExport for testing
    
    - separate 2023 from createDatimExport for testing
    
    * bunch 2023 and 2024
    
    - bunch 2023 and 2024 in createDatimExport

---
+++ R/createPAWExport.R
@@ -0,0 +1,35 @@
+#' @export
+#' @title createPAWExport
+#'
+#' @param d
+#'
+#' @return A data frame in standardized DHIS2 export format for PAW
+#' @export
+#'
+createPAWExport <- function(d) {
+
+  if (d$info$tool == "Data Pack") {
+
+    if (d$info$cop_year %in% c(2023, 2024)) {
+      if (d$info$has_psnuxim) {
+        paw_export <- dplyr::bind_rows(d$datim$subnat_impatt,
+                                         d$datim$prioritizations,
+                                         d$datim$OPU)
+      } else {
+        paw_export <- dplyr::bind_rows(d$datim$subnat_impatt,
+                                         d$datim$prioritizations,
+                                         d$datim$UndistributedMER)
+      }
+    }
+  }
+
+  if (d$info$tool %in% c("OPU Data Pack", "PSNUxIM")) {
+    paw_export <-  d$datim$OPU
+  }
+
+  paw_export %>%
+    dplyr::mutate(value = as.character(value)) %>%
+    #Filter any non-dedupe zeros
+    dplyr::filter(!(value == "0" & !grepl("^0000[01]", attributeOptionCombo)))
+
+}
