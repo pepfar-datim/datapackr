f141a88605817e134a54229bdbc6cbbdaac0996f
Jordan <Jordan>
2024-05-29 15:06:43 -0400

    Fixed workboodExtraction. Didn't want to mix with createdatimexport.

--- tests/testthat/test-workbookExtraction.R
+++ tests/testthat/test-workbookExtraction.R
@@ -1,7 +1,7 @@
 context("test-loadDataPack")
 
-test_that("Can list contents of COP24 template", {
-  d  <-   datapackr::createKeychainInfo(submission_path = test_sheet("COP24_Data_Pack_Template_minimal.xlsx"),
+test_that("Can list contents of COP23 template", {
+  d  <-   datapackr::createKeychainInfo(submission_path = test_sheet("COP23_sample_DataPack_Malawi.xlsx"),
                                         tool = "Data Pack",
                                         country_uids = NULL,
                                         cop_year = NULL)
@@ -13,13 +13,13 @@ test_that("Can list contents of COP24 template", {
 })
 
 
-test_that("Can load a COP24 Data Pack.", {
+test_that("Can load a COP23 Data Pack.", {
   d  <-
     datapackr::loadDataPack(
-      submission_path = test_sheet("COP24_Data_Pack_Template_minimal.xlsx"),
+      submission_path = test_sheet("COP23_sample_DataPack_Malawi.xlsx"),
       tool = "Data Pack",
       country_uids = NULL,
-      cop_year = 2024,
+      cop_year = 2023,
       load_wb = TRUE,
       load_sheets = TRUE,
       d2_session = training)
@@ -27,20 +27,18 @@ test_that("Can load a COP24 Data Pack.", {
   expect_named(d, c("keychain", "info", "tool", "sheets"))
   expect_true(file.exists(d$keychain$submission_path))
 
-  skip <- skip_tabs(tool = "Data Pack", cop_year = 2024)$unpack
+  skip <- skip_tabs(tool = "Data Pack", cop_year = 2023)$unpack
   all_sheets <- unique(cop24_data_pack_schema$sheet_name)
   targets_sheets <- all_sheets[!all_sheets %in% c(skip, "PSNUxIM")]
-  # print(names(d$sheets))
-  # print(names(targets_sheets))
-  # expect_named(d$sheets, targets_sheets) #Did we drop a sheet since 2022? Year 2 maybe?
+  expect_named(d$sheets, targets_sheets)
 
   expect_true("wb" %in% names(d$tool))
 
   expect_true(d$info$tool == "Data Pack")
-  expect_true(d$info$cop_year == 2024)
+  expect_true(d$info$cop_year == 2023)
   expect_true(d$info$has_error == FALSE)
-  expect_identical(d$info$schema, datapackr::cop24_data_pack_schema)
-  expect_true(d$info$datapack_name == "Lesotho")
+  expect_identical(d$info$schema, datapackr::cop23_data_pack_schema)
+  expect_true(d$info$datapack_name == "Malawi")
   expect_true(d$info$needs_psnuxim == FALSE)
   expect_true(d$info$newSNUxIM == FALSE)
   expect_true(d$info$has_psnuxim == FALSE)
