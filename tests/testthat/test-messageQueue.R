d3ab1b04a9b80acf092e097cc9408692feff29c4
Jason Pickering <jason.p.pickering@gmail.com>
2024-11-08 14:24:45 +0100

    Improve message queue tests

--- tests/testthat/test-messageQueue.R
+++ tests/testthat/test-messageQueue.R
@@ -64,6 +64,37 @@ test_that("Can error on if messages are not a vector", {
   messages_list <- list(message = message, level = level)
   expect_error(appendMessage(foo, messages_list, level))
 
+  messages_list <- list(message = message, level = list(level = level))
+  expect_error(appendMessage(foo, messages_list, level))
+
+  messages_list <- list(message = message, level = level, tool = list(tool = "DataPack"))
+  expect_error(appendMessage(foo, messages_list, level))
+
+  foo <- MessageQueue()
+  message <- c("a", "b", "c")
+  level <- c("ERROR", "ERROR", "ERROR", "WARNING")
+  expect_error(appendMessage(foo, message, level), "Messages and warnings must be of the same length")
+
+})
+
+test_that("Can warn on an empty message or warning", {
+
+  foo <- MessageQueue()
+  message <- c("INFO: Hello", "")
+  level <- c("INFO", "WARNING")
+
+  expect_warning(foo <- appendMessage(foo, message, level))
+  expect_equal(foo$message, "INFO: Hello")
+  expect_equal(foo$level, "INFO")
+
+  foo <- MessageQueue()
+  message <- c("INFO: Hello", "WARNING: Goodbye")
+  level <- c("INFO", "")
+
+  expect_warning(foo <- appendMessage(foo, message, level))
+  expect_setequal(foo$message, message)
+  expect_setequal(foo$level, c("INFO", "UNKNOWN"))
+
 })
 
 test_that("Can add a tool (optional)", {
@@ -76,4 +107,16 @@ test_that("Can add a tool (optional)", {
   foo <- appendMessage(foo, message, level, tool)
   expect_setequal(names(foo), c("message", "level", "tool"))
   expect_equal(foo$tool, c("Tool1", "UNKNOWN"))
+})
+
+test_that("printMessages.MessageQueue works as expected", {
+  mockery::stub(printMessages.MessageQueue, "interactive", TRUE)
+  foo <- MessageQueue()
+  message <- c("INFO: Hello", "WARNING: Goodbye")
+  level <- c("INFO", "WARNING")
+  tool <- c("Tool1", NA_character_)
+  foo <- appendMessage(foo, message, level, tool)
+  output <- capture.output(printMessages.MessageQueue(foo))
+  expect_true(any(grepl("VALIDATION ISSUES:", output)))
+  expect_true(any(grepl("WARNING!: Problematic", output)))
 })
